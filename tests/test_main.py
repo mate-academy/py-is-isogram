f21393e23b66dc32d456ddd11f1e93cc1fe2095c
Nazar-kob <kobrinnazar05@mail.com>
2022-02-03 16:32:07 +0200

    Solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,17 @@
-# write your code here
+from app.main import is_isogram
+
+
+def test_when_string_is_empty():
+    assert is_isogram("") is True
+
+
+def test_string_with_upper_letters():
+    assert is_isogram("Adam") is False
+
+
+def test_string_with_double_letters():
+    assert is_isogram("look") is False
+
+
+def test_without_double_letters():
+    assert is_isogram("playgrounds") is True
