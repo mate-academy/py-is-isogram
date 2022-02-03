ae1727b03b929d9c0c864a9a70171bf1db821af3
Dmitry Ivanov <karward94@gmail.com>
2022-02-03 22:23:17 +0200

    Fix PEP 8: E712

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,19 +1,15 @@
 from app.main import is_isogram
 
 
-# 1. PEP 8: E712 comparison to True should be 'if cond is True:' or 'if cond:'
-# 2. PEP 8: E712 comparison to False should be 'if cond is False:'
-#    or 'if not cond:'
-
 def test_empty_string():
-    assert is_isogram("") == 1
+    assert is_isogram("") is True
 
 
 def test_case_insensitive():
-    assert is_isogram("LKJFSDLIlsdfeojf") == 0
-    assert is_isogram("QWERTyuiop") == 1
+    assert is_isogram("LKJFSDLIlsdfeojf") is False
+    assert is_isogram("QWERTyuiop") is True
 
 
 def test_core_functionality():
-    assert is_isogram("sldkfjrjfdmo") == 0
-    assert is_isogram("asdfghjk09865") == 1
+    assert is_isogram("sldkfjrjfdmo") is False
+    assert is_isogram("asdfghjk09865") is True
