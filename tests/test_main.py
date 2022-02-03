715af14751d10c76ef5b7bdd49deb31c3e5b926f
Oleksandr Zhukov <alexanderzhukov88@gmail.com>
2022-02-03 19:50:45 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,18 +2,12 @@ from app.main import is_isogram
 
 
 def test_empty_string():
-    goal = is_isogram("")
-
-    assert goal is True
+    assert is_isogram("") is True
 
 
 def test_with_uppercase_letters():
-    goal = is_isogram("PlaygRound")
-
-    assert goal is True
+    assert is_isogram("PlaygRound") is True
 
 
 def test_with_symbls_spaces_and_digits():
-    goal = is_isogram("abcdefg7. $")
-
-    assert goal is True
+    assert is_isogram("abcdefg7. $") is True
