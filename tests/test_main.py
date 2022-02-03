4244b5cef396749075c745062451e68031244b89
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-02-03 15:41:02 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,24 @@
-# write your code here
+from app.main import is_isogram
+
+def test_empty_string():
+    assert is_isogram("") is True
+
+
+def test_upper_case_letter():
+    assert is_isogram("Adam") is False
+
+
+def test_isogram_true():
+    assert is_isogram("playgrounds") is True
+
+
+def test_repeating_consecutive_letters():
+    assert is_isogram("look") is False
+
+
+def test_repeating_non_consecutive_letters():
+    assert is_isogram("loko") is False
+
+
+def test_apostrophe():
+    assert is_isogram("lk'l") is False
