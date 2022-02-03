c3d0b45d7c6b6fe9bbd31fd8fe0cdbb452e79163
TJgkOsVP <sasha06052001@gmail.com>
2022-02-03 16:31:12 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,18 @@
-# write your code here
+from app.main import is_isogram
+
+
+def test_empty_string():
+    assert is_isogram('')
+
+
+def test_mix_upper_with_lower():
+    assert not is_isogram('Adam')
+
+
+def test_string_without_repeating():
+    assert is_isogram('playgrounds')
+
+
+def test_all_words_repeat():
+    assert not is_isogram("bbbBBBBbb")
+
