54b1876fd9181af9f6c2683a6ff4c4ebf1cca18d
Kalyta Valerii <deptovka@gmail.com>
2022-02-03 17:44:19 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,22 @@
-# write your code here
+import pytest
+from app.main import is_isogram
+
+
+@pytest.mark.parametrize(
+    "word, boolean_value",
+    [
+        ('pythonist', False),
+        ('must', True),
+        ('be', True),
+        ('lazy', True),
+        ('alwAys', False),
+        ('anD', True),
+        ('EvERywhEre', False),
+    ],
+)
+def test_word_variations_is_isogram(word, boolean_value):
+    assert is_isogram(word) == boolean_value
+
+
+def test_if_string_is_empty():
+    assert is_isogram('') is True
