298f9221f868528a01842cc1642a8fc629129d2a
Vlad Tsvigun <vl.tsv@outlook.com>
2022-02-03 16:35:39 +0200

    Solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,15 @@
-# write your code here
+from app.main import is_isogram
+
+
+def test_empty_string_is_isogram():
+    assert is_isogram("") is True
+
+
+def test_mixed_letters():
+    assert is_isogram("AsdfgTkl") is True
+    assert is_isogram("KjhgFsrT") is True
+
+
+def test_repeating_letters():
+    assert is_isogram("QwWpP") is False
+    assert is_isogram("PopopPO") is False
