a37c9caf45d85e9b0f1053ccbaf88a1fce21480d
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-02-03 17:14:58 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,14 @@
-# write your code here
+from app.main import is_isogram
+
+
+def test_for_empty_string():
+    assert is_isogram("") is True
+
+
+def test_case_insensitivity():
+    assert is_isogram("pReTtY") == is_isogram("PrEtTy")
+
+
+def test_repeated_letters():
+    assert is_isogram("surrender") is False
+    assert is_isogram("ihavealotrepeatedletter") is False
