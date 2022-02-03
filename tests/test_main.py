5e6b672e123ff554f3025c9538c6a5da816c7257
alix makarova <alix.makarova@gmail.com>
2022-02-03 18:56:46 +0200

    Solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,28 @@
-# write your code here
+from app.main import is_isogram
+
+
+def test_empty_string():
+    assert is_isogram("")
+
+
+def test_true():
+    values = ["unCopyrIghtAbLe", "aMbiDextrOuslY", "plaYGrounDs"]
+    actual = []
+    for word in values:
+        actual.append(is_isogram(word))
+
+    assert all(actual)
+
+
+def test_false():
+    values = ["loOk", "Adam", "DOcToRwHO"]
+    actual = []
+    for word in values:
+        actual.append(is_isogram(word))
+
+    assert not all(actual)
+
+
+def test_symbols():
+    assert not is_isogram('**8+%+9865)')
+    assert is_isogram('*8%+965)')
