9724a273a56e7ccd927746c2e37ec240efcf20e2
Rybak Dmytro <codingclaud@gmail.com>
2022-02-03 15:29:44 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,15 @@
-# write your code here
+from app.main import is_isogram
+
+
+def test_if_string_is_empty():
+    assert is_isogram("") is True
+
+
+def test_if_string_contains_repeating_letters():
+    assert is_isogram("loOk") is False
+    assert is_isogram("Adam") is False
+
+
+def test_if_string_does_not_contain_repeating_letters():
+    assert is_isogram("playgrounds") is True
+    assert is_isogram("Dmytro") is True
