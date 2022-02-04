397136f0ed21b1a72c60f24722ff9ac718d416d6
Alex Gergel <alexgergel@gmail.com>
2022-02-05 00:13:40 +0200

    Solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,6 +7,6 @@ def test_empty_string_and_result_type_bool():
 
 def test_for_upper_case_letters_and_isograms():
     assert is_isogram("Apex") == True
-    assert is_isogram("Full") == False
-    assert is_isogram("banana") == False
+    assert is_isogram("FulL") == False
+    assert is_isogram("bAnaNA") == False
     assert is_isogram("dermatoglyphics") == True
