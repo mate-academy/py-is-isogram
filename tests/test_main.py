e657bdb21a07884e63eb354f5ac80f4076154502
polinatytarenko <p.tytarenko.20@gmail.com>
2022-02-05 23:51:33 +0200

    Solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,17 @@
-# write your code here
+from app.main import is_isogram
+
+
+def test_is_isogram_returns_true_when_string_is_empty():
+    assert is_isogram("") is True
+
+
+def test_is_isogram_returns_true_when_no_repeating_letters():
+    assert is_isogram("playgrounds") is True
+
+
+def test_is_isogram_returns_false_when_repeating_letters_occur():
+    assert is_isogram("childhood") is False
+
+
+def test_is_isogram_case_insensitive():
+    assert is_isogram("Choice") is False
