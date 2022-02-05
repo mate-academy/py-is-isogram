a72e3bc160f958668e3d2502c8cbb17de48c1ba2
Nazar-kob <kobrinnazar05@mail.com>
2022-02-05 13:08:12 +0200

    I added a new test case

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,13 +5,17 @@ def test_when_string_is_empty():
     assert is_isogram("") is True
 
 
-def test_string_with_upper_letters():
+def test_string_with_double_and_upper_letters():
     assert is_isogram("Adam") is False
 
 
-def test_string_with_double_letters():
+def test_string_with_double_and_lower_letters():
     assert is_isogram("look") is False
 
 
 def test_without_double_letters():
-    assert is_isogram("playgrounds") is True
+    assert is_isogram("PlAygrOunDs") is True
+
+
+def test_all_lower_not_repeated():
+    assert is_isogram('qwertyuiop') is True
