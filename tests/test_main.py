92b1d226b8cf0c04e7fc0a10cb14b515241e04ec
alix makarova <alix.makarova@gmail.com>
2022-02-07 19:20:50 +0200

    Used mark.parametrize

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+import pytest
 from app.main import is_isogram
 
 
@@ -5,22 +6,28 @@ def test_empty_string():
     assert is_isogram("")
 
 
-def test_is_isogram_should_be_equal_true():
-    values = ["unCopyrIghtAbLe", "aMbiDextrOuslY", "plaYGrounDs"]
-    actual = []
-    for word in values:
-        actual.append(is_isogram(word))
+@pytest.mark.parametrize(
+    "word",
+    [
+        ("unCopyrIghtAbLe"),
+        ("aMbiDextrOuslY"),
+        ("plaYGrounDs")
+    ]
+)
+def test_no_repeated_letters(word):
+    assert is_isogram(word)
 
-    assert all(actual)
 
-
-def test_is_isogram_should_be_equal_false():
-    values = ["loOk", "Adam", "DOcToRwHO"]
-    actual = []
-    for word in values:
-        actual.append(is_isogram(word))
-
-    assert not all(actual)
+@pytest.mark.parametrize(
+    "word",
+    [
+        ("loOk"),
+        ("Adam"),
+        ("DOcToRwHO")
+    ]
+)
+def test_with_repeated_letters(word):
+    assert not is_isogram(word)
 
 
 def test_when_word_contains_symbols():
