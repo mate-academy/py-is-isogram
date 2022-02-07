e142a6b19dc7f182abb44229383253af5b98e25b
Denys Lipin <lipin.transstream@gmail.com>
2022-02-07 13:13:16 +0200

    'Solution2'

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,9 +9,13 @@ def test_should_return_true_for_isogram_word_with_big_and_small_letters():
     assert is_isogram("PlayGrounds") is True
 
 
-def test_should_return_false_for_different_repeating_letters():
+def test_should_return_false_for_lowercase_repeating_letters():
     assert is_isogram("opportunity") is False
 
 
-def test_should_return_false_for_repeating_big_and_small_letters():
+def test_should_return_false_for_uppercase_repeating_letters():
+    assert is_isogram("SUMMER") is False
+
+
+def test_should_return_false_for_repeating_sensitive_letters():
     assert is_isogram("Test") is False
