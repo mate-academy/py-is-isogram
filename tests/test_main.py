99f657ff0d1f1e9cf96fadecf2a6f166e5a0b4fb
Artur Shavula <arthuromgnh@gmail.com>
2022-02-07 17:36:26 +0200

    Deleted random in tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,3 @@
-from string import ascii_lowercase
-from random import randint, sample
-
 from app.main import is_isogram
 
 
@@ -9,26 +6,11 @@ def test_empty_string():
 
 
 def test_isogram_string():
-    tested_string_list = sample(
-        ascii_lowercase,
-        randint(1, len(ascii_lowercase))
-    )
-    tmp = ''
-    for letter in tested_string_list:
-        if randint(0, 1):
-            tmp += letter.upper()
-        else:
-            tmp += letter.lower()
-    tested_string = tmp
+    tested_string = 'abc'
     assert is_isogram(tested_string)
 
 
 def test_non_isogram_string():
-    tested_string = ''.join(sample(
-        ascii_lowercase,
-        randint(2, len(ascii_lowercase))
-    ))
-
-    tested_string += ''.join(sample(tested_string, 2))
+    tested_string = 'abccbc'
 
     assert not is_isogram(tested_string)
