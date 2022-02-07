8bd8746b65bb2f3e5af1f5b5fa18f71228071778
Kalyta Valerii <deptovka@gmail.com>
2022-02-07 13:41:56 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,13 +5,13 @@ from app.main import is_isogram
 @pytest.mark.parametrize(
     "word, boolean_value",
     [
-        ('pythonist', False),
-        ('must', True),
-        ('be', True),
-        ('lazy', True),
-        ('alwAys', False),
-        ('anD', True),
-        ('EvERywhEre', False),
+        pytest.param('pythonist', False, id="Not an isogram"),
+        pytest.param('must', True, id="True isogram"),
+        pytest.param('be', True, id="True isogram"),
+        pytest.param('lazy', True, id="True isogram"),
+        pytest.param('alwAys', False, id="Not an isogram with different registers"),
+        pytest.param('anD', True, id="True isogram with different registers"),
+        pytest.param('EvERywhEre', False, id="Not an isogram with different registers"),
     ],
 )
 def test_word_variations_is_isogram(word, boolean_value):
