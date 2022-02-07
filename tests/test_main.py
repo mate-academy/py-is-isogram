78cf75798c8f6437e0213869a81c77747b36e30f
Alex Gergel <alexgergel@gmail.com>
2022-02-08 00:40:38 +0200

    Solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,11 +2,19 @@ from app.main import is_isogram
 
 
 def test_empty_string_and_result_type_bool():
-    assert is_isogram("") == True
+    assert is_isogram("") is True
 
 
-def test_for_upper_case_letters_and_isograms():
-    assert is_isogram("Apex") == True
-    assert is_isogram("FulL") == False
-    assert is_isogram("bAnaNA") == False
-    assert is_isogram("dermatoglyphics") == True
+def test_repeated_insensitive_letters():
+    assert is_isogram("Apex") is True
+    assert is_isogram("dermatoglyphics") is True
+
+
+def test_no_repeated_letters():
+    assert is_isogram("uncopyrightable") is True
+    assert is_isogram("copyrightable") is True
+
+
+def test_repeated_letters():
+    assert is_isogram("ololo") is False
+    assert is_isogram("foo") is False
