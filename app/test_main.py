da0796971a0b4d665e49bb9406592a36fdfec6cd
Oleksii Proshchenko <80070761+MasterpieceElbow@users.noreply.github.com>
2022-05-31 13:21:44 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_for_empty_string():
+    assert is_isogram("") is True
+
+
+def test_no_repeated_letters():
+    assert is_isogram("iampython") is True
+
+
+def test_case_insensitivity():
+    assert is_isogram("pReTtY") is False
+
+
+def test_equal_non_consecutive_letters():
+    assert is_isogram("abcdefa") is False
