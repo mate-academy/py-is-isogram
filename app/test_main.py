a1f0ef9b3e9b055911bca747191c7ca74cbfdeb9
Yurnerroo <yura.zdanevych@gmail.com>
2022-06-02 22:06:35 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_isogram_word():
+    assert is_isogram("isogram") is True
+
+
+def test_non_isogram_word():
+    assert is_isogram("door") is False
+
+
+def test_empty_string():
+    assert is_isogram("") is True
+
+
+def test_random_long_word():
+    assert is_isogram("asdyuiqweop123,/.[") is True
+
+
+def test_uppercase_word():
+    assert is_isogram("Arabic") is False
