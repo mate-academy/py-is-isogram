b35a8b4504dfa94b5f683a01bcf05e54ec34b3b4
Serhii Cheshko <cheshko.sergey@gmail.com>
2022-06-02 13:13:56 +0300

    'is_isogram'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    def test_no_letters(self):
+        assert is_isogram("") is True
+
+    def test_one_letter(self):
+        assert is_isogram("a") is True
+        assert is_isogram("A") is True
+
+    def test_uppercase(self):
+        assert is_isogram("Aa") is False
+        assert is_isogram("AA") is False
+
+    def test_different_words(self):
+        assert is_isogram('playgrounds') is True
+        assert is_isogram('look') is False
