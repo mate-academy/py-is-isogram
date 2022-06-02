79a367738628cb26a0e5bc9c95f0f43386a0e748
Dasha Protsko <protscodasha@icloud.com>
2022-06-02 23:11:57 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_isogram_word():
+    assert is_isogram('playgrounds') is True
+
+
+def test_word_with_repeating_same_case():
+    assert is_isogram('look') is False
+
+
+def test_word_with_repeating_different_case():
+    assert is_isogram('Adam') is False
+
+
+def test_empty_string():
+    assert is_isogram('') is True
+
