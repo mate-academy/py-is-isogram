23fe75555f86fff20abb4f19655442a90eea2cef
Max Sukhaniuk <maksym.sukhaniuk@gmail.com>
2022-06-02 15:09:24 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,23 @@
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    def test_string_is_empty_should_be_true(self):
+        goals = is_isogram("")
+        assert goals is True
+
+    def test_isogram_should_be_true(self):
+        goals = is_isogram("playgrounds")
+        assert goals is True
+
+    def test_not_isogram_should_be_false(self):
+        goals = is_isogram("look")
+        assert goals is False
+
+    def test_not_isogram_string_with_uppercase(self):
+        goals = is_isogram("Adam")
+        assert goals is False
+
+    def test_isogram_with_uppercase(self):
+        goals = is_isogram("AdOlF")
+        assert goals is True
