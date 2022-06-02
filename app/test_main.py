3a67e9ac5f92ee943de957240d9e2c41cb35a77a
Daria <darizhur.vol2@gmail.com>
2022-06-02 17:25:43 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_word_is_empty():
+    assert is_isogram('') is True
+
+
+def test_word_is_correct():
+    assert is_isogram('playgrounds') is True
+
+
+def test_letters_should_no_repeating():
+    assert is_isogram('look') is False
+
+
+def test_lower_or_upper_case():
+    assert is_isogram('Adam') is False
