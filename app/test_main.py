fed8d41189c2bd82fc85d00684b22b4b47799526
Magus6784 <100879423+Magus6784@users.noreply.github.com>
2022-06-02 18:36:52 +0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -10,14 +10,6 @@ class TestIsIsogram:
         goals = is_isogram("playgrounds")
         assert goals is True
 
-    def test_not_isogram_should_be_false(self):
-        goals = is_isogram("look")
-        assert goals is False
-
-    def test_not_isogram_string_with_uppercase(self):
-        goals = is_isogram("Adam")
-        assert goals is False
-
     def test_isogram_with_uppercase(self):
         goals = is_isogram("AdOlF")
         assert goals is True
