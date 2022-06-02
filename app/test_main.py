9e2d3a10e8b3d6e11931fe9df39f201996ea727d
Flashmobber <ufinaid@gmail.com>
2022-06-02 19:02:56 +0300

    Just done......

--- app/test_main.py
+++ app/test_main.py
@@ -9,7 +9,8 @@ from app.main import is_isogram
                           pytest.param("book", False,
                                        id="test repeating letters"),
                           pytest.param("Adam", False,
-                                       id="test case-insensitive repeating letters"),
+                                       id="test case-insensitive "
+                                          "repeating letters"),
                           pytest.param("", True, id="test empty string")])
 def test_is_isogram(word: str, result: bool):
     assert is_isogram(word) == result
