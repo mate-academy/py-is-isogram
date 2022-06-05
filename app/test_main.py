d6c694100d9006416edd2dc53f344a919804a339
ILIAS <nossudak@gmail.com>
2022-06-05 21:10:12 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_with_empty_string():
+    assert is_isogram("") is True
+
+
+def test_with_real_isogram():
+    assert is_isogram("playgrounds") is True
+
+
+def test_isogram_with_mixed_case():
+    assert is_isogram("PLAYgrounds") is True
+
+
+def test_isogram_with_all_capital():
+    assert is_isogram("PLAYGROUND") is True
+
+
+def test_with_consecutive():
+    assert is_isogram("APple") is False
+
+
+def test_with_non_consecutive():
+    assert is_isogram("AdaM") is False
+
