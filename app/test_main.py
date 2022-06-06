79fd7093189720bcd9df9669761c60f4734b9374
Mariia Movchan <mariia.movchan.s@gmail.com>
2022-06-06 10:42:14 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string():
+    assert is_isogram("") is True
+
+
+def test_case_insensitive():
+    assert is_isogram('Adam') is False
+
+
+def test_string_with_no_repeating_letters():
+    assert is_isogram('playgrounds') is True
+
+
+def test_string_with_repeating_letter():
+    assert is_isogram('look') is False
