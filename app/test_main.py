6700443aabc443c9bbefd6f715c2da3fb0344b16
Vasyl Hnatiuk <vasylhnatiuk1@gmail.com>
2022-06-06 14:43:46 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+@pytest.mark.parametrize(
+    "word, excpected",
+    [
+        pytest.param(
+            "playgrounds",
+            True,
+            id="playgrounds is isogram"),
+        pytest.param(
+            "look",
+            False,
+            id="'look' is not isogram"),
+        pytest.param(
+            "Adam",
+            False,
+            id="'Adam' is not isogram"),
+        pytest.param(
+            '',
+            True,
+            id="Empty string is True"),
+    ]
+)
+def test_get_coin_combination(word, excpected):
+    assert is_isogram(word) == excpected
