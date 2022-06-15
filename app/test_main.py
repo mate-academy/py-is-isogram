e3e85e1d94fef63650a9d8c19036fa368b0c86f3
Anton Lasko <anton.lasko1993@gmail.com>
2022-06-15 09:26:49 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string():
+    given_word = is_isogram("")
+    assert given_word is True
+
+
+def test_long_word_without_duplicates_of_letters():
+    given_word = is_isogram("playgrounds")
+    assert given_word is True
+
+
+def test_short_word_with_double_o():
+    given_word = is_isogram("look")
+    assert given_word is False
+
+
+def test_short_word_with_uppercase_and_lowercase():
+    given_word = is_isogram("Adam")
+    assert given_word is False
