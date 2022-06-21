1238be72ab168d2399165c968e27a836b90d6fae
Andreii Korotenko <korotenko.qndroid@gmail.com>
2022-06-21 21:32:28 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_should_return_true_when_string_is_isogram():
+    assert is_isogram("playgrounds") is True
+
+
+def test_should_return_true_when_string_is_empty():
+    assert is_isogram("") is True
+
+
+def test_should_return_false_when_letters_in_different_cases():
+    assert is_isogram("lOok") is False
+
+
+def test_should_return_false_if_the_letters_are_not_next_to_each_other():
+    assert is_isogram("Adam") is False
