2746295acb1650c81e6c28323375db48644df12f
Dmytro Hushchyk <D.hushchyk@gmail.com>
2022-07-20 14:37:42 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string():
+    assert is_isogram("") is True
+
+
+def test_is_not_isogram_with_upper_chars():
+    assert is_isogram("DYnamoKyiv") is False
+
+
+def test_non_consecutive_is_not_isogram():
+    assert is_isogram("abcdefa") is False
