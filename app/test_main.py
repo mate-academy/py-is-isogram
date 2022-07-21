e2b488d682d3fdea8347e3bf922d6122e80a34f9
Roman <coderc770@gmail.com>
2022-07-21 13:13:56 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -7,15 +7,15 @@ def test_empty_string_is_isogram():
 
 
 def test_isogram_is_case_insensitive():
-    word = "Member"
+    word = "Adam"
     assert not is_isogram(word)
 
 
 def test_consecutive_letters_are_not_isogram():
-    word = "Book"
+    word = "look"
     assert not is_isogram(word)
 
 
 def test_non_consecutive_letters_are_not_isogram():
-    word = "playgrounds"
+    word = "asa"
     assert not is_isogram(word)
