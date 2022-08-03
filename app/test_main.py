c5803ad892b7998bee58e64d9d75faab99b445e6
Inna Kushnir <innakushnir63@gmail.com>
2022-08-03 21:34:26 +0300

    solutions

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_if_empty_string():
+    assert is_isogram("") is True
+
+
+def test_if_identical_letters_next_to_each_other():
+    assert is_isogram("Annet") is False
+
+
+def test_if_identical_letters_separately():
+    assert is_isogram("Anka") is False
+
+
+def test_if_no_identical_letters():
+    assert is_isogram("President") is True
