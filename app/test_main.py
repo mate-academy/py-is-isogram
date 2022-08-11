3dc79e4ca8b4e6b597decb370c3f6b17fb5ed495
stoveprofi <stoveprofi@gmail.com>
2022-08-11 17:18:34 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,11 @@
 from app.main import is_isogram
 
 
-def test_should_return_true_when_value_have_upper_case_is_isogram():
+def test_should_return_true_when_value_isogram():
     assert is_isogram('pLaygrounds') is True
 
 
-def test_should_return_true_when_value_empty_line_is_isogram():
+def test_return_true_value_empty_line_isogram():
     assert is_isogram('') is True
 
 
@@ -13,5 +13,5 @@ def test_non_consecutive_letters_are_not_isogram():
     assert is_isogram('Aa') is False
 
 
-def test_string_with_different_cases_of_the_same_letter_is_not_isogram():
+def test_different_cases_same_letter_not_isogram():
     assert is_isogram('LdlofokfK') is False
