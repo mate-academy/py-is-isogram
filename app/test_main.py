3763ac45f348e96d21fff1fb858986dbf93b693e
Diliaver Yusupov <diliaver795@gmail.com>
2022-08-21 23:20:42 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,19 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string_is_isogram():
+    goals = is_isogram('')
+
+    assert goals is True
+
+
+def test_non_consecutive_letters_are_not_isogram():
+    goals = is_isogram('acba')
+
+    assert goals is False
+
+
+def test_isogram_is_case_insensitive():
+    goals = is_isogram('Adam')
+
+    assert goals is False
