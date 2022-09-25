94371c851fe7b9ab3bcd7f881d14b142ee0de8ed
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2022-09-25 20:13:45 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,29 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "word, result",
+        [
+            pytest.param(
+                "", True,
+                id="test should return true if the word is an empty line"
+            ),
+            pytest.param(
+                "look", False,
+                id="test should return false if the word is not an isogram"
+            ),
+            pytest.param(
+                "Adam", False,
+                id="test should return false if the same letters "
+                   "have a different registers"
+            ),
+            pytest.param(
+                "playgrounds", True,
+                id="test should return true if the word is an isogram"
+            )
+        ]
+    )
+    def test_is_isogram(self, word, result):
+        assert is_isogram(word) == result
