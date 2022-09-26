005efd9c70f05275aefc907c866e2c302e69e1b4
Oleksandr Panasiuk <provizor.307@gmail.com>
2022-09-26 11:50:03 +0300

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -9,16 +9,8 @@ def test_should_return_true_when_string_have_no_repeat_letter():
     assert is_isogram('playgrounds') is True
 
 
-def test_should_return_false_when_string_have_two_and_more_spaces():
-    assert is_isogram('pl aygrounds ') is False
-
-
 def test_should_return_false_when_string_have_two_and_more_same_letter_and_different_case():
     assert is_isogram('Adam') is False
-
-
-def test_should_return_false_when_string_have_different_case():
-    assert is_isogram('Adem') is True
 
 
 def test_should_return_false_when_string_have_two_and_more_same_letter():
