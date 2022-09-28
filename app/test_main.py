fe73209a1342f6272bc9147335b6266d99f25c83
Kateryna Kilyk <katekilik@gmail.com>
2022-09-28 16:17:38 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,27 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        pytest.param(
+            "", True,
+            id="Empty string is an isogram"
+        ),
+        pytest.param(
+            "Adam", False,
+            id="String with different cases of the same letter is not an isogram"
+        ),
+        pytest.param(
+            "playgrounds", True,
+            id="Should return True when each letter occurs once"
+        ),
+        pytest.param(
+            "look", False,
+            id="Should return False when letter occurs more than once"
+        )
+    ]
+)
+def test_is_isogram(word: str, expected: bool) -> None:
+    assert is_isogram(word) == expected
