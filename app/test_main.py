a14fbe2d1f94f5c9b4de2ccd06e6e121905c6198
Yaroslav Stetsko <yar.stetsko@gmail.com>
2022-10-04 20:28:24 +0300

    Removed unneeded class

--- app/test_main.py
+++ app/test_main.py
@@ -3,33 +3,30 @@ import pytest
 from app.main import is_isogram
 
 
-class TestIsIsogram:
-    @pytest.mark.parametrize(
-        "isogram, check",
-        [
-            pytest.param(
-                "",
-                True,
-                id="should return true for empty string"
-            ),
-            pytest.param(
-                "aa",
-                False,
-                id="should return false for consecutive repeats"
-            ),
-            pytest.param(
-                "cascade",
-                False,
-                id="should return false if letters are not consecutive"
-            ),
-            pytest.param(
-                "Aaron",
-                False,
-                id="should return false if cases are different"
-            ),
-        ]
-    )
-    def test_is_isogram(self,
-                        isogram,
-                        check):
-        assert is_isogram(isogram) is check
+@pytest.mark.parametrize(
+    "isogram, check",
+    [
+        pytest.param(
+            "",
+            True,
+            id="should return true for empty string"
+        ),
+        pytest.param(
+            "aa",
+            False,
+            id="should return false for consecutive repeats"
+        ),
+        pytest.param(
+            "cascade",
+            False,
+            id="should return false if letters are not consecutive"
+        ),
+        pytest.param(
+            "Aaron",
+            False,
+            id="should return false if cases are different"
+        ),
+    ]
+)
+def test_is_isogram(isogram, check):
+    assert is_isogram(isogram) is check
