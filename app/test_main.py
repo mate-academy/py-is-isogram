87a139dd50268ed42418942f0c54fc588982fbeb
Sergey <servizah@gmail.com>
2022-10-05 21:30:27 +0300

    flake8 recomendations checked

--- app/test_main.py
+++ app/test_main.py
@@ -2,16 +2,16 @@ from app.main import is_isogram
 
 
 def test_if_string_empty() -> None:
-    assert is_isogram('') is True
+    assert is_isogram("") is True
 
 
 def test_with_repeating_consecutive_letters() -> None:
-    assert is_isogram('look') is False
+    assert is_isogram("look") is False
 
 
 def test_with_repeating_in_different_cases() -> None:
-    assert is_isogram('Adam') is False
+    assert is_isogram("Adam") is False
 
 
 def test_with_non_repeating_letters() -> None:
-    assert is_isogram('playgrounds') is True
+    assert is_isogram("playgrounds") is True
