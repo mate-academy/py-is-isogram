5a2414d8bccd25470efca2ea962cec013d1d3f23
Nikyxa <kolisova.veronika81@gmail.com>
2022-10-05 21:01:37 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,17 @@
 from app.main import is_isogram
 
 
-def test_empty_string():
+def test_empty_string() -> None:
     assert is_isogram("") is True
 
 
-def test_word_should_be_isogram():
+def test_word_should_be_isogram() -> None:
     assert is_isogram("playgrounds") is True
 
 
-def test_word_should_not_be_isogram():
+def test_word_should_not_be_isogram() -> None:
     assert is_isogram("look") is False
 
 
-def test_word_lover_and_uppercase():
+def test_word_lover_and_uppercase() -> None:
     assert is_isogram("Adam") is False
