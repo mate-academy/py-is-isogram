dbd5b2146e11a8c38b941c35622c90ee28b34cde
Dima Ovcharenko <kukuku381@gmail.com>
2022-10-05 13:08:06 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,19 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_word():
+    assert is_isogram("") is True
+
+
+def test_repeating_letters():
+    assert is_isogram("look") is False
+
+
+def test_no_repeating_letters():
+    assert is_isogram("playgrounds") is True
+
+
+def test_case_sensitivity():
+    assert is_isogram("Adam") is False
+
+
