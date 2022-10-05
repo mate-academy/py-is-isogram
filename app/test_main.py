e348ff1dc349fb8f51db159b32b7c6a0b3f5e589
Katrin Lazarenko <katrin903@gmail.com>
2022-10-05 14:57:28 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,29 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+class TestIsIsogram:
+
+    @pytest.mark.parametrize(
+        "word,result",
+        [
+            pytest.param("playgrounds", True,
+                         id="Should return True if playgrounds"
+                         ),
+            pytest.param("look", False,
+                         id="Should return False if look"
+                         ),
+            pytest.param("Adam", False,
+                         id="Should return False if Adam"
+                         ),
+            pytest.param("", True,
+                         id="Should return True if empty string"
+                         )
+        ]
+    )
+    def test_is_isogram(self, word: str, result: bool) -> None:
+        assert is_isogram(word) == result
+
+
+if __name__ == "__main__":
+    pytest.main()
