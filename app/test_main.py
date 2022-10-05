7e474eaea6e5660a6fed10fe0d2b6f12bc843ec2
Mowka7 <98344661+Mowka7@users.noreply.github.com>
2022-10-05 15:21:20 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,15 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        pytest.param("look", False,),
+        pytest.param("Adam", False,),
+        pytest.param("playgrounds", True),
+        pytest.param("", True,)
+    ]
+)
+def test_is_isogram(word: str, expected: bool) -> None:
+    assert is_isogram(word) == expected
