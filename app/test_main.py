17cf22578383ba50a75a33bc43dca1d74a8e6543
Vitalii Shevchuk <shevch.nl@gmail.com>
2022-10-05 23:51:30 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,17 @@
 from app.main import is_isogram
 
 
-def test_empty_string():
+def test_empty_string() -> None:
     assert is_isogram("") is True
 
 
-def test_return_expected_result_when_word_isogram():
+def test_return_expected_result_when_word_isogram() -> None:
     assert is_isogram("playgrounds") is True
 
 
-def test_return_expected_result_when_word_not_isogram():
+def test_return_expected_result_when_word_not_isogram() -> None:
     assert is_isogram("look") is False
 
 
-def test_check_case():
+def test_check_case() -> None:
     assert is_isogram("Adam") is False
