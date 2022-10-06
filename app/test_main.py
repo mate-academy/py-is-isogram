d7c401f075f25142da227e51f33898e2fb83fd02
Oleksandr Kolomoiets <0985305543@ukr.net>
2022-10-06 10:37:12 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string() -> None:
+    word = is_isogram("")
+
+    assert word is True
+
+
+def test_consecutive_same_letters() -> None:
+    word = is_isogram("loOk")
+
+    assert word is False
+
+
+def test_non_consecutive_same_letters() -> None:
+    word = is_isogram("Adam")
+
+    assert word is False
+
+
+def test_isogram_word() -> None:
+    word = is_isogram("playGrounds")
+
+    assert word is True
