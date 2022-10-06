cc4abb20766b25ec147582123ca991431e7d832a
Mariia Movchan <mariia.movchan.m@gmail.com>
2022-10-06 11:02:38 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,36 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+class TestRepeatingLetters:
+    @pytest.mark.parametrize(
+        "word, expected_result",
+        [
+            pytest.param(
+                "",
+                True,
+                id="should return True if string is empty"
+            ),
+            pytest.param(
+                "Adam",
+                False,
+                id="should return False if letters in different case"
+            ),
+            pytest.param(
+                "look",
+                False,
+                id="test for word contains repeating letters"
+            ),
+            pytest.param(
+                "playgrounds",
+                True,
+                id="test for word contains non-repeating letters"
+            ),
+        ]
+    )
+    def test_convert_to_1_penny(
+            self,
+            word: str,
+            expected_result: bool
+    ) -> None:
+        assert is_isogram(word) == expected_result
