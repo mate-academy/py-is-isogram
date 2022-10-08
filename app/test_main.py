e33d53d8a3406e62021ff187651f43c8da7e19ab
Bohdan2001007 <bogdan1991007@gmail.com>
2022-10-08 23:52:09 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,16 +2,16 @@ from app.main import is_isogram
 
 
 def test_first() -> None:
-    assert is_isogram('playgrounds') is True
+    assert is_isogram("playgrounds") is True
 
 
 def test_second() -> None:
-    assert is_isogram('look') is False
+    assert is_isogram("look") is False
 
 
 def test_third() -> None:
-    assert is_isogram('Adam') is False
+    assert is_isogram("Adam") is False
 
 
 def test_fourth() -> None:
-    assert is_isogram('') is True
+    assert is_isogram("") is True
