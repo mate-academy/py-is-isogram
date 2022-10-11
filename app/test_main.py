45b2107b0f7f0f7690844ff9e6584a4cb91c7249
IvanGLS <gladiolus0707@gmail.com>
2022-10-11 15:37:49 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_nothing() -> None:
+    assert is_isogram("") is True
+
+
+def test_has_not_repeating_letters() -> None:
+    assert is_isogram("playing") is True
+
+
+def test_with_repeating_letters() -> None:
+    assert is_isogram("kavabanga") is False
+
+
+def test_with_different_cases() -> None:
+    assert is_isogram("goOd") is False
+
+
+def test_with_consecutive_letters() -> None:
+    assert is_isogram("good") is False
