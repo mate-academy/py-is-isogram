99fb3891b68b33215035bbadd7b63d69a9101d8e
Vlad Hrytsai <sunrises2929@gmail.com>
2022-10-12 19:53:29 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string():
+    assert is_isogram("") is True
+
+
+def test_string_diff_case():
+    assert is_isogram("look") is False
+
+
+def test_big_letters():
+    assert is_isogram("Adam") is False
