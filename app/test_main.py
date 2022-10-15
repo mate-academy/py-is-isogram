7d23724ef267fdea3afcd0dbfb886e26e62dfdaf
Your Name <y>
2022-10-15 16:24:39 +0300

    Fixed

--- app/test_main.py
+++ app/test_main.py
@@ -8,6 +8,7 @@ class TestIsogram:
     @pytest.mark.parametrize("world, returned", [
         pytest.param("playgrounds", True, id="True isogram test"),
         pytest.param("look", False, id="False isogram test"),
+        pytest.param("Adam", False, id="Capitalize string test"),
         pytest.param("", True, id="Empty string test")
     ])
     def test_is_isogram(self, world: str, returned: bool) -> None:
