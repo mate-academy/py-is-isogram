0ec2d3464f412ff0293773a2d2c7bac4e3105cbd
Your Name <y>
2022-10-15 16:18:37 +0300

    flake8 app error fixed

--- app/test_main.py
+++ app/test_main.py
@@ -6,8 +6,8 @@ from app.main import is_isogram
 class TestIsogram:
 
     @pytest.mark.parametrize("world, returned", [
-        pytest.param('playgrounds', True, id="True isogram test"),
-        pytest.param('look', False, id="False isogram test"),
+        pytest.param("playgrounds", True, id="True isogram test"),
+        pytest.param("look", False, id="False isogram test"),
         pytest.param("", True, id="Empty string test")
     ])
     def test_is_isogram(self, world: str, returned: bool) -> None:
