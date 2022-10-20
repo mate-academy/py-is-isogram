61dfc3b9afa17149b0f49099c97039ccef50b825
Nataliia Petrushak <87134904+Amirammi@users.noreply.github.com>
2022-10-20 12:47:52 +0300

    Fixed flake8 issues

--- app/test_main.py
+++ app/test_main.py
@@ -5,11 +5,14 @@ import pytest
 @pytest.mark.parametrize(
     "string, expected",
     [
-        pytest.param("playgrounds", True, id="should return 'True' for isogram"),
-        pytest.param("look", False, id="should return 'False' for not isogram"),
+        pytest.param("playgrounds", True, 
+                     id="should return 'True' for isogram"),
+        pytest.param("look", False, 
+                     id="should return 'False' for not isogram"),
         pytest.param("Adam", False, id="should be case-insensitive"),
         pytest.param("", True, id="empty string should be isogram")
     ]
 )
-def test_is_isogram_results_are_correct(string: str, expected: bool) -> None:
+def test_is_isogram_results_are_correct(string: str,
+                                        expected: bool) -> None:
     assert is_isogram(string) == expected
