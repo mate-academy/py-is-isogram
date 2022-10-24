721558853a02d4fbe211fd226a5dab5a1223200e
Taras Zelinskyi <taras.sv89@gmail.com>
2022-10-25 00:48:01 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_function_returns_bool() -> None:
+    assert isinstance(is_isogram(""), bool)
+
+
+def test_empty_string_is_isogram() -> None:
+    assert is_isogram("") is True
+
+
+def test_uppercase_words() -> None:
+    assert is_isogram("HeLlo") is False
+
+
+def test_repeating_letters() -> None:
+    assert is_isogram('look') is False
+
+
+def test_non_consecutive_repeating_letters() -> None:
+    assert is_isogram("Taras") is False
+
+
+def test_non_repeating_letters() -> None:
+    assert is_isogram('playgrounds') is True
