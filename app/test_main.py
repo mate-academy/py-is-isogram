60ede5ee04edd216467f3d88fe62fc5ad76c3c90
Mykola Zakharov <kolyazakharov94@gmail.com>
2022-10-30 11:17:52 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_should_retern_true_when_input_is_empty_string() -> None:
+    assert is_isogram("") is True
+
+
+def test_should_retern_true_when_input_is_isogram() -> None:
+    assert is_isogram("qwerty") is True
+
+
+def test_should_retern_false_when_input_is_not_isogram() -> None:
+    assert is_isogram("abba") is False
+
+
+def test_return_false_when_letter_case_insensitive() -> None:
+    assert is_isogram("Adam") is False
