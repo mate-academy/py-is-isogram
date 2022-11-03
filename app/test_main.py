65cbd917953da4f590a42c399417c63955747e8d
Taras Martyniuk <bythewaters800@gmail.com>
2022-11-03 16:41:40 +0100

    Sol

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,res",
+    [
+        ("", True),
+        ("Adam", False),
+        ("look", False),
+        ("Playgrounds", True),
+        ("WRTOdont", False)
+    ]
+)
+def test_if_str_isogram_or_not(word: str,
+                               res: bool) -> None:
+
+    isogram = is_isogram(word)
+
+    assert isogram == res
