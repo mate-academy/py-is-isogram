151a9b7576d5df86b7372f506276dee7e1e49fa2
vani@linux <winfkill@gmail.com>
2022-11-20 22:20:00 +0200

    DONe344

--- app/test_main.py
+++ app/test_main.py
@@ -1,7 +1,7 @@
 from app.main import is_isogram
 
 
-def test_check_is_isogram_str(s='') -> None:
+def test_check_is_isogram_str() -> None:
     assert is_isogram("playgrounds") is True
     assert is_isogram("look") is False
     assert is_isogram("Adam") is False
