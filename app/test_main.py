bf8fba4aaafd87b206275fc87dc08fe5f7745bf9
Roman Kuryliak <romahakl@gmail.com>
2022-12-05 15:23:19 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,37 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,is_isogram_result",
+    [
+        pytest.param(
+            "word",
+            True,
+            id="word 'word' is isogram"
+        ),
+        pytest.param(
+            "",
+            True,
+            id="empty string is isogram"
+        ),
+        pytest.param(
+            "moment",
+            False,
+            id="repeating non-consecutive letters not allowed"
+        ),
+        pytest.param(
+            "Moment",
+            False,
+            id="case of letters doesnt matter"
+        ),
+        pytest.param(
+            "cool",
+            False,
+            id="repeating consecutive letters not allowed"
+        )
+    ]
+)
+def test_is_isogram_work_correct(word: str, is_isogram_result: bool) -> None:
+    assert is_isogram(word) == is_isogram_result
