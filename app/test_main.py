12dac4ef7e0c7bd53376ed20652685c91c8181aa
soobig666 <darkvader.ukr.net@gmail.com>
2022-12-05 23:11:11 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -16,20 +16,20 @@ from app.main import is_isogram
             id="should return 'True' if word is isogram"
         ),
         pytest.param(
-             "moment",
-             False,
-             id="repeating non-consecutive letters not allowed"
-         ),
+            "moment",
+            False,
+            id="repeating non-consecutive letters not allowed"
+        ),
         pytest.param(
-             "Moment",
-             False,
-             id="case of letters doesnt matter"
-         ),
+            "Moment",
+            False,
+            id="case of letters doesnt matter"
+        ),
         pytest.param(
-             "cool",
-             False,
-             id="repeating consecutive letters not allowed"
-         )
+            "cool",
+            False,
+            id="repeating consecutive letters not allowed"
+        )
     ]
 )
 def test_for_right_work_func(
