7610f7d34b44a558ffb85172d28d9834872f1ddd
soobig666 <darkvader.ukr.net@gmail.com>
2022-12-05 20:49:59 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,40 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "check_word,bool_result",
+    [
+        pytest.param(
+            "",
+            True,
+            id="should return 'True' if value is empty string"
+        ),
+        pytest.param(
+            "playgrounds",
+            True,
+            id="should return 'True' if word is isogram"
+        ),
+        pytest.param(
+            "look",
+            False,
+            id="should return 'False' if word is not isogram"
+        ),
+        pytest.param(
+            "Iphone",
+            True,
+            id="function should be case-insensitive"
+        ),
+        pytest.param(
+            "abcd",
+            False,
+            id="not only consecutive letters are not an isogram."
+        )
+    ]
+)
+def test_for_right_work_func(
+        check_word: str,
+        bool_result: bool
+) -> None:
+
+    assert is_isogram(check_word) == bool_result
