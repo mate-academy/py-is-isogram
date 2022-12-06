1e5932984f8472572f33760b138394c35ae2e90e
Dmytro Bleichyk <dima.badiuk@gmail.com>
2022-12-06 20:07:06 +0200

    Double quotes added

--- app/test_main.py
+++ app/test_main.py
@@ -2,16 +2,16 @@ from app.main import is_isogram
 
 
 def test_should_return_true_when_word_is_empty() -> None:
-    assert is_isogram('') is True
+    assert is_isogram("") is True
 
 
-def test_should_return_false_when_word_with_repeat_but_different_case() -> None:
-    assert is_isogram('Anya') is False
+def test_return_false_when_word_with_repeat_but_different_case() -> None:
+    assert is_isogram("Anya") is False
 
 
 def test_should_return_true_when_word_is_long_without_repeats() -> None:
-    assert is_isogram('playgrounds') is True
+    assert is_isogram("playgrounds") is True
 
 
 def test_should_return_false_when_letters_are_consecutive() -> None:
-    assert is_isogram('book') is False
+    assert is_isogram("book") is False
