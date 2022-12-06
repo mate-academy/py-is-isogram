24258d489e90ccbfae6b5fffb4b476ba5d2b9498
Dmytro Bleichyk <dima.badiuk@gmail.com>
2022-12-06 20:02:44 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_should_return_true_when_word_is_empty() -> None:
+    assert is_isogram('') is True
+
+
+def test_should_return_false_when_word_with_repeat_but_different_case() -> None:
+    assert is_isogram('Anya') is False
+
+
+def test_should_return_true_when_word_is_long_without_repeats() -> None:
+    assert is_isogram('playgrounds') is True
+
+
+def test_should_return_false_when_letters_are_consecutive() -> None:
+    assert is_isogram('book') is False
