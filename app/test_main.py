a7e41340eb0f9143b374685b02a1ce384d771b0f
Derkach Oleksandr <derkach03553@gmail.com>
2022-12-22 12:41:04 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,10 +5,10 @@ import pytest
 @pytest.mark.parametrize(
     "actual, expected",
     [
-        (is_isogram('playgrounds'), True),
-        (is_isogram('look'), False),
-        (is_isogram('Adam'), False),
-        (is_isogram(''), True)
+        (is_isogram("playgrounds"), True),
+        (is_isogram("look"), False),
+        (is_isogram("Adam"), False),
+        (is_isogram(""), True)
     ]
 )
 def test_is_isogram(actual: bool, expected: bool) -> None:
