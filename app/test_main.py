d9e14f9c8002891312c798ef8729d17ecfccda74
Derkach Oleksandr <derkach03553@gmail.com>
2022-12-22 12:39:45 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,15 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "actual, expected",
+    [
+        (is_isogram('playgrounds'), True),
+        (is_isogram('look'), False),
+        (is_isogram('Adam'), False),
+        (is_isogram(''), True)
+    ]
+)
+def test_is_isogram(actual: bool, expected: bool) -> None:
+    assert actual == expected
