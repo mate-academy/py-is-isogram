191273655f47c07dd5fd027b2a1074c5395ca77f
Haishuk Pavlo <xomas@ukr.net>
2022-12-26 01:40:14 +0200

    done

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,14 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_for_true_case() -> None:
+    assert is_isogram('playgrounds') is True
+
+
+def test_for_false_case() -> None:
+    assert is_isogram('look') is False
+    assert is_isogram('Adam') is False
+
+
+def test_for_empty_string() -> None:
+    assert is_isogram('') is True
