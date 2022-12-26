f4916ba970cc529003fd3afb8f414f00f7f75f24
Oleksandr Rozsokha <oleksandrozsokha@gmail.com>
2022-12-26 15:33:41 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        pytest.param("", True, id="When word is given empty string"),
+        pytest.param("playgrounds", True, id="When word is isogram"),
+        pytest.param("look", False, id="When word is not isogram"),
+        pytest.param(
+            "Ozone", False, id="When word has same upper and lowercase letters"
+        ),
+    ]
+)
+def test_func_should_return_the_right_result(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
