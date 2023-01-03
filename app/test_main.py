7bec5de14054b0f383ec5f0650aa3a7afd781cb6
Vitalii Panochko <vpapplic@gmail.com>
2023-01-03 14:05:17 +0200

    tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,23 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+def test_should_raise_error() -> None:
+    with pytest.raises(AttributeError):
+        is_isogram(15)
+
+
+def test_data_is_empty() -> None:
+    assert is_isogram("") is True
+
+
+def test_with_repeating_letters() -> None:
+    assert is_isogram("look") is False
+
+
+def test_should_be_case_insensitive() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_true_value() -> None:
+    assert is_isogram("playgrounds") is True
