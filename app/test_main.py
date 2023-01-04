e5b657bcde2e2adcae19447c1056fdaf9591dde6
Artem Kurmanchuk <bosskurmanchuk143@gmail.com>
2023-01-04 18:46:13 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,23 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+def test_invalid_input_throws() -> None:
+    with pytest.raises(TypeError):
+        is_isogram(1)
+
+
+def test_empty_string() -> None:
+    assert is_isogram("") is True
+
+
+def different_case_of_letters() -> None:
+    assert is_isogram("isIsogram") is False
+
+
+def test_is_isogram() -> None:
+    assert is_isogram("isogram") is True
+
+
+def test_different_cases_of_the_same_letter() -> None:
+    assert is_isogram("moOse") is False
