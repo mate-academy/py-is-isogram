37410e95ad543feade14868fcb0f2744ecf6c236
Marharyta Sovpenko <margaretsovpenko@gmail.com>
2023-01-13 23:37:24 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,40 @@
+import pytest
 from app.main import is_isogram
+from typing import Any
 
-# write your code here
+
+class TestIsIsogram:
+
+    @pytest.mark.parametrize(
+        "word,expected_result",
+        [
+            pytest.param(
+                "playgrounds",
+                True,
+                id="should return True when no repeating letters"
+            ),
+            pytest.param(
+                "look",
+                False,
+                id="should return False for repeating letters "
+            ),
+            pytest.param(
+                "Adam",
+                False,
+                id="should count both uppercase and lowercase letters"
+            ),
+            pytest.param(
+                "",
+                True,
+                id="should return True for an empty string"
+            )
+        ]
+    )
+    def test_define_isogram_correctly(
+            self,
+            word: str,
+            expected_result: bool
+    ) -> None:
+        assert is_isogram(word) == expected_result
+
+
