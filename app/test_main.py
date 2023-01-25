47aac88628df34a9565db31264566154c6111263
Alina Boichenko <ilub1108@gmail.com>
2023-01-25 13:38:21 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "word,expected_bool",
+        [
+            ("playgrounds", True),
+            ("look", False),
+            ("Adam", False),
+            ("", True)
+        ]
+    )
+    def test_modify_classes_correctly(
+            self,
+            word: str,
+            expected_bool: bool
+    ) -> None:
+
+        assert is_isogram(word) == expected_bool
