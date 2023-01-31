55db414bd3e1b969324a52d42d81ba06f2f12b55
Vitalii Khmura <vitalikkhmura13@gmail.comw>
2023-01-31 22:23:50 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,40 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "initial_word, result",
+    [
+        pytest.param(
+            "playground ", True,
+            id="should return 'True' when  letter in word not repeated"
+        ),
+
+        pytest.param(
+            "", True,
+            id="Should return 'True' when number of letter 1 or 0"
+        ),
+        pytest.param(
+            "look", False,
+            id="Should return 'False' when letter in word repeated"
+        ),
+        pytest.param(
+            "Adam", False,
+            id="Should return 'False' when letter in word repeated"
+        )
+    ]
+)
+def test_corectly_correct_result(initial_word: str, result: bool) -> None:
+    assert is_isogram(initial_word) == result
+
+
+@pytest.mark.parametrize(
+    "initial_word",
+    [
+        pytest.param(1, id="type of 'word' should be str"),
+        pytest.param([2], id="type of 'word' should be str")
+    ]
+)
+def test_raised_attribute_error(initial_word: str) -> None:
+    with pytest.raises(AttributeError):
+        is_isogram(initial_word)
