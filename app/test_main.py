c38e56573c3cb00bd4b7d4650a2581ef7bc643e8
Taras Savchyn <savik1992@gmail.com>
2023-01-31 20:06:43 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,9 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_main_empty_line() -> None:
+    assert is_isogram("") is True
+
+
+def test_consecutive_letters_not_an_isogram() -> None:
+    assert is_isogram("zxcvasdfZ") is False
