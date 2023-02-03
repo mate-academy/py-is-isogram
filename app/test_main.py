df0d116ff03a0098847d87e4415cd7f7aa1fc531
sergk <srx92@protonmail.com>
2023-02-03 20:50:54 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -10,9 +10,12 @@ from app.main import is_isogram
         pytest.param("look", False),
         pytest.param("Adam", False),
         pytest.param("", True)
-     ]
+    ]
 )
-def test_if_string_is_isogram(inputted_str: str, expected_result: bool) -> None:
+def test_if_string_is_isogram(
+        inputted_str: str,
+        expected_result: bool
+) -> None:
     assert (
         is_isogram(inputted_str) == expected_result
     )
