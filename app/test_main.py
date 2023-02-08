287e53ef104218f37db4cf682102da5c24c34f62
VladyslavOladko <vladguitar03@gmail.com>
2023-02-08 20:40:43 +0300

    fixed v2

--- app/test_main.py
+++ app/test_main.py
@@ -1,36 +1,29 @@
 from app.main import is_isogram
 
 
-def test_empty_string() -> None:
+def test_check_for_empty_str() -> None:
     word = ""
-    actually_result = is_isogram(word)
-    expected = True
-    assert actually_result is expected
+    expected = is_isogram(word)
+
+    assert expected is True
 
 
-def test_amaterasu_word() -> None:
-    word = "amaterasu"
-    actually_result = is_isogram(word)
-    expected = False
-    assert actually_result is expected
+def test_check_for_look_str() -> None:
+    word = "look"
+    expected = is_isogram(word)
+
+    assert expected is False
 
 
-def test_isanagi_word() -> None:
-    word = "isanagi"
-    actually_result = is_isogram(word)
-    expected = False
-    assert actually_result is expected
+def test_check_for_adam_str() -> None:
+    word = "Adam"
+    expected = is_isogram(word)
+
+    assert expected is False
 
 
-def test_word_with_pascal_case() -> None:
-    word = "Kotoamatsukami"
-    actually_result = is_isogram(word)
-    expected = False
-    assert actually_result is expected
+def test_check_for_playground_str() -> None:
+    word = "playground"
+    expected = is_isogram(word)
 
-
-def test_kamui_word() -> None:
-    word = "kamui"
-    actually_result = is_isogram(word)
-    expected = True
-    assert actually_result is expected
+    assert expected is True
