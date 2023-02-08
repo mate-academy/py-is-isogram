4a6e3dd4882e36e8c44793bb7a3504e88c2ba4f2
VladyslavOladko <vladguitar03@gmail.com>
2023-02-08 20:31:52 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,29 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_amaterasu_word() -> None:
+    word = "amaterasu"
+    actually_result = is_isogram(word)
+    expected = False
+    assert actually_result is expected
+
+
+def test_isanagi_word() -> None:
+    word = "isanagi"
+    actually_result = is_isogram(word)
+    expected = False
+    assert actually_result is expected
+
+
+def test_kotoamatsukami_word() -> None:
+    word = "kotoamatsukami"
+    actually_result = is_isogram(word)
+    expected = False
+    assert actually_result is expected
+
+
+def test_kamui_word() -> None:
+    word = "kamui"
+    actually_result = is_isogram(word)
+    expected = True
+    assert actually_result is expected
