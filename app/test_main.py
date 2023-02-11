4464b5c8022e60ce8f6dabb667b81a9b7327b138
Andriy <andriykrav@gmail.com>
2023-02-11 16:05:29 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_different_case_letter_are_not_isogram() -> None:
+    assert is_isogram("Mm") is False
+
+
+def test_empty_string_is_an_isogram() -> None:
+    assert is_isogram("") is True
+
+
+def test_not_only_consecutive_letters_are_not_an_isogram() -> None:
+    assert is_isogram("aaaaa") is False
+
+
+def test_not_only_non_consecutive_letters_are_not_an_isogram() -> None:
+    assert is_isogram("abababab") is False
