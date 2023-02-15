413073c3d233fa0ecb0d7161dcfaec6a94c46029
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-15 14:55:58 +0200

    1 commit

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,20 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+def test_normal_true() -> None:
+    assert is_isogram("AbCDEfG") is True
+
+
+def test_normal_false() -> None:
+    assert is_isogram("AbC") is False
+
+
+def test_empty_string() -> None:
+    assert is_isogram("") is True
+
+
+def test_attribute_error() -> None:
+    with pytest.raises(AttributeError):
+        is_isogram(1)
