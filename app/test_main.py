542e3c82ebd09a30814b92a39f5685f88dd6431f
bohdanlysuchenko <lysachenko111@ukr.net>
2023-02-16 14:19:54 +0200

    Changes

--- app/test_main.py
+++ app/test_main.py
@@ -6,9 +6,9 @@ from app.main import is_isogram
 @pytest.mark.parametrize(
     "word,result_bool",
     [
-        ('playgrounds', True),
-        ('look', False),
-        ('Adam', False),
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
         ("", True),
     ]
 )
@@ -16,6 +16,6 @@ def test_correct_work(word: str, result_bool: bool) -> None:
     assert is_isogram(word) == result_bool
 
 
-def test_doesnt_work_with_another_type():
+def test_doesnt_work_with_another_type() -> None:
     with pytest.raises(AttributeError):
         is_isogram(12)
