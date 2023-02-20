847e266fc3f7c0760382ecfdddedaee784c567d7
Vlad Zabolotnyi <ftftnvm2@gmail.com>
2023-02-20 16:57:03 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,46 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "input_value,expected_result",
+    [
+        pytest.param(
+            "playgrounds",
+            True,
+            id="should return True if letters don't repeat"
+        ),
+        pytest.param(
+            "look",
+            False,
+            id="should return False if letters are repeating"
+        ),
+        pytest.param(
+            "Adam",
+            False,
+            id="should return False with upper-case repeated letters"
+        ),
+        pytest.param(
+            "AdamaAAmDFdf",
+            False,
+            id="should return False with upper-case repeated letters"
+        ),
+
+        pytest.param(
+            "",
+            True,
+            id="should return True is input is empty string"
+        ),
+    ]
+)
+def test_is_isogram_with_different_inputs(
+        input_value: str,
+        expected_result: bool
+) -> None:
+    assert is_isogram(input_value) == expected_result
+
+
+def test_is_isogram_raises_errors() -> None:
+    with pytest.raises(AttributeError):
+        is_isogram(123)
