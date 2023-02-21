901e8cf9faeaca2bf0dea6207dbdd444a969175e
kuznetsb <kuznetsbog@gmail.com>
2023-02-21 22:47:19 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,43 @@
+import pytest
+
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsogramCheck:
+    @pytest.mark.parametrize(
+        "string, result",
+        [
+            pytest.param(
+                "",
+                True,
+                id="Should return True if empty string"
+            ),
+            pytest.param(
+                "playgrounds",
+                True,
+                id="Should return True if string is isogram"
+            ),
+            pytest.param(
+                "letter",
+                False,
+                id="Should return False if letters are consecutive"
+            ),
+            pytest.param(
+                "Test",
+                False,
+                id="Should be case-insensitive"
+            ),
+            pytest.param(
+                "grogu",
+                False,
+                id="Should return False if letters are non-consecutive"
+            )
+        ]
+    )
+    def is_func_works_correctly(
+            self,
+            string,
+            result
+    ) -> None:
+        assert is_isogram(string) is result
