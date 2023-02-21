b8af529c7b473ef5685edcb78c392ce32d79d980
oksana-feshchenko <foksanv@gmail.com>
2023-02-21 17:24:19 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,22 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_with_empty_string_true():
+    assert is_isogram("") is True
+
+
+def test_with_repeating_letter_together_false():
+    assert is_isogram("look") is False
+
+
+def test_with_repeating_letter_not_together_false():
+    assert is_isogram("adam") is False
+
+
+def test_with_different_cases_false():
+    assert is_isogram("Adam") is False
+
+
+def test_with_no_repeating_true():
+    assert is_isogram("playgrounds") is True
+
