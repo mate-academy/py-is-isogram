6fc67c3e89a9101312ea8794d1ed6a97684ec215
oksana-feshchenko <foksanv@gmail.com>
2023-02-21 18:00:36 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,22 +1,26 @@
+import pytest
 from app.main import is_isogram
 
 
-def test_with_empty_string_true():
-    assert is_isogram("") is True
-
-
-def test_with_repeating_letter_together_false():
-    assert is_isogram("look") is False
-
-
-def test_with_repeating_letter_not_together_false():
-    assert is_isogram("adam") is False
-
-
-def test_with_different_cases_false():
-    assert is_isogram("Adam") is False
-
-
-def test_with_no_repeating_true():
-    assert is_isogram("playgrounds") is True
-
+@pytest.mark.parametrize(
+    "string, expected",
+    [
+        pytest.param(
+            "", True, id="test_with_empty_string_true"
+        ),
+        pytest.param(
+            "look", False, id="test_with_repeating_letter_together_false"
+        ),
+        pytest.param(
+            "adam", False, id="test_with_repeating_letter_not_together_false"
+        ),
+        pytest.param(
+            "Adam", False, id="test_with_different_cases_false"
+        ),
+        pytest.param(
+            "playgrounds", True, id="test_with_no_repeating_true"
+        )
+    ]
+)
+def test_is_isogram(string: str, expected: bool) -> bool:
+    assert is_isogram(string) is expected
