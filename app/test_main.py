750848b471687b27dea381ceb0ff76a8461a1520
da Da <danildacenko@gmail.com>
2023-02-22 14:37:19 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
 from app.main import is_isogram
 
-# write your code here
+import pytest
+
+
+@pytest.mark.parametrize("word, expected_result", [
+    pytest.param("isogram", True, id="simple isogram"),
+    pytest.param("Alphabet", False, id="mixed case"),
+    pytest.param("hello", False, id="not isogram"),
+    pytest.param("aba", False, id="duplicate letters"),
+    pytest.param("", True, id="empty string"),
+    pytest.param(" ", True, id="single space"),
+    pytest.param("two words", False, id="multiple words"),
+])
+def test_is_isogram(word: str, expected_result: bool) -> None:
+    assert is_isogram(word) == expected_result
