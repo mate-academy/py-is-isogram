078bea77c66e16c1686442aca0f2bc5cfaf5f4cd
Anna Boichenko <ann.boychenko199@gmail.com>
2023-02-22 17:57:50 +0200

    'solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_should_detect_regular_words() -> None:
+    assert is_isogram("regular") is False
+
+
+def test_should_return_true_when_word_is_isogram() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_empty_string_is_isogram() -> None:
+    assert is_isogram("") is True
+
+
+def test_should_be_case_insensitive() -> None:
+    assert is_isogram("JaCk") is True
+
+
+def test_different_cases_of_same_letter_is_not_isogram():
+    assert is_isogram("Ggt") is False
