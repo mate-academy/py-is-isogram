218f4b19aa2ce026f6fb65d3211a9d788f7c1532
Anatolii Pozniak <a13101990a@gmail.com>
2023-02-22 19:51:04 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
+import pytest
+
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "input_string,expected_result",
+        [
+            ("", True),
+            ("look", False),
+            ("Adam", False),
+            ("playground", True)
+        ],
+        ids=[
+            "test empty string",
+            "test consecutive string",
+            "test non-consecutive string",
+            "test an isogram string"
+        ]
+    )
+    def test_empty_string(self,
+                          input_string: str,
+                          expected_result: bool) -> None:
+        assert is_isogram(input_string) == expected_result
