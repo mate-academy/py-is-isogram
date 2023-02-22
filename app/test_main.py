f6ee36b93bdf50e89647673730c2fe07ff51299f
Vitalii Shulepa <vitaliy.shulep@gmail.com>
2023-02-22 13:49:11 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_when_string_is_empty() -> None:
+    assert is_isogram("") is True
+
+
+def test_when_not_isogram_including_capital_letter() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_when_isogram() -> None:
+    assert is_isogram("name") is True
+
+
+def test_with_ordered_letters() -> None:
+    assert is_isogram("zoo") is False
