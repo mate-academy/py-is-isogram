490ccdadb6d868d4797c0ca28a6d429b640d8b1c
Oksana Dushna <oksana.dushna.1996@gmail.com>
2023-02-22 16:35:30 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,10 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string() -> None:
+    assert is_isogram("") is True
+
+
+def test_similar_letters_case() -> None:
+    assert is_isogram("Adam") is False
+    assert is_isogram("aac") is False
