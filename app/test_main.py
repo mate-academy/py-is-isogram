3e7a7d0954e24363dc3a54787aab160723f7fa1e
Mykola Savchyn <kola.savchyn@gmail.com>
2023-02-22 14:03:19 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,21 @@
 from app.main import is_isogram
 
 
-def test_empty_string():
+def test_empty_string() -> None:
     assert is_isogram("") is True
 
 
-def test_same_letters_same_case():
+def test_same_letters_same_case() -> None:
     assert is_isogram("wood") is False
 
 
-def test_same_letters_different_case():
+def test_same_letters_different_case() -> None:
     assert is_isogram("Adam") is False
 
 
-def test_no_repeated_letters_same_case():
+def test_no_repeated_letters_same_case() -> None:
     assert is_isogram("mate") is True
 
 
-def test_no_repeated_letters_different_case():
+def test_no_repeated_letters_different_case() -> None:
     assert is_isogram("Mate") is True
