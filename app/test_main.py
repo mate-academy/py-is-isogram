3b6935af80102d91c6c35ee78d9064aeb5d0ff93
Ruslan <noezyboy1@gmail.com>
2023-02-22 12:44:56 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,16 +1,33 @@
 from app.main import is_isogram
 
-def test_is_isogram_empty_string():
-    assert is_isogram('') == True
 
-def test_is_isogram_case_insensitive():
-    assert is_isogram('Playgrounds') == True
+def test_empty_string_is_isogram() -> bool:
+    assert is_isogram("") is True
 
-def test_is_isogram_true():
-    assert is_isogram('abcdefg') == True
 
-def test_is_isogram_false():
-    assert is_isogram('look') == False
+def test_isogram_word_is_isogram() -> bool:
+    assert is_isogram("playgrounds") is True
 
-def test_is_isogram_false_with_repeated_letters():
-    assert is_isogram('Adam') == False
+
+def test_non_isogram_word_is_isogram() -> bool:
+    assert is_isogram("look") is False
+
+
+def test_case_insensitivity_is_isogram() -> bool:
+    assert is_isogram("Adam") is False
+    assert is_isogram("aDam") is False
+    assert is_isogram("ADAM") is False
+
+
+def test_with_punctuation_is_isogram() -> bool:
+    assert is_isogram("hello,world!") is False
+
+
+def test_non_consecutive_letters_are_not_isogram() -> bool:
+    assert is_isogram("abcdefghijklmnopqrstuvwxyz") is True
+    assert is_isogram("notices") is False
+
+
+def test_consecutive_letters_are_not_isogram() -> bool:
+    assert is_isogram("aabbccddeeffgghhii") is False
+    assert is_isogram("aaabbbcccddd") is False
