250d2c0eef8a9357a438d11c7ec83c4b00c2c45d
Sashkoooo <o.denysenko@hotmail.com>
2023-02-24 21:48:13 +0200

    Solution v1.0

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,expected_output",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+    ],
+    ids=[
+        "test check if function returns True",
+        "test check if function returns False",
+        "test check if function case-insensitive",
+        "test check empty string",
+    ]
+)
+def test_is_isogram(
+        word: str,
+        expected_output: bool
+) -> None:
+    assert is_isogram(word) == expected_output
