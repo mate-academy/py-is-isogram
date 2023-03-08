a81298cad621f031b1fae6fc7a59af9611f24e31
Aleksandr <lotrikys@gmail.com>
2023-03-08 13:41:02 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -9,5 +9,13 @@ def test_empty_string_is_isogram() -> None:
     assert is_isogram('') is True
 
 
-def test_word_isnt_isogram() -> None:
+def test_word_is_not_isogram() -> None:
     assert is_isogram('request') is False
+
+
+def test_word_with_different_letters_cases_is_not_isogram() -> None:
+    assert is_isogram('rEquest') is False
+
+
+def test_work_with_same_consecutive_letters_is_not_isogram() -> None:
+    assert is_isogram('look') is False
