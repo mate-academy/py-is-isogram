07452695060d3debacd0628708b93473c9469778
Aleksandr <lotrikys@gmail.com>
2023-03-08 13:37:14 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,4 +2,12 @@ from app.main import is_isogram
 
 
 def test_word_is_isogram() -> None:
-    assert is_isogram('fork') == True
+    assert is_isogram('fork') is True
+
+
+def test_empty_string_is_isogram() -> None:
+    assert is_isogram('') is True
+
+
+def test_word_isnt_isogram() -> None:
+    assert is_isogram('request') is False
