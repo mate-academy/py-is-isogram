dbb4850e6d8de9e76d999f759cf5426b50810bbb
Anastasiia <nastyashch1601@gmail.com>
2023-03-15 16:36:01 +0000

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        pytest.param("playgrounds", True, id="'playgrounds' is isogram"),
+        pytest.param("look", False, id="'look' contains 2 'o'"),
+        pytest.param("Adam", False, id="'Adam' contains 2 'a'"),
+        pytest.param("", True, id="empty string is isogram"),
+    ]
+)
+def test_when_age_changes(
+    word: str,
+    result: bool
+) -> None:
+    assert (is_isogram(word) == result)
