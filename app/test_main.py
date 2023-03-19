622a670db8ced78f69ea378d421634ef2655d9ff
Vladimir Titarenko <tytarenkova@gmail.com>
2023-03-19 04:16:48 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,19 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,bool_value",
+    [
+        ('playgrounds', True),
+        ("look", False),
+        ("Adam", False),
+        ("", True)
+    ]
+)
+def test_should_return_correct_boolean_value(
+        word: str, bool_value: bool
+) -> None:
+    assert (is_isogram(word) is bool_value
+            ), f"should return {bool_value} when 'word' is {word}"
