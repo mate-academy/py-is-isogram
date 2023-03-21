d4fe1cdd983beb621d2b16fa600f5800f6c99290
Kirill Melanich <kirillmelanich@gmail.com>
2023-03-21 14:12:04 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,39 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "word, expected_result",
+        [
+            ("alegro", True),
+            ("allegro", False),
+            ("1234567890", True),
+            ("", True),
+            ("abcdefghijklmnopqrstuvwxyz", True),
+            ("ABCDEFGHIJKLMNOPQRSTUVWXYZ.", True),
+            ("AlBert", True),
+            ("DomInIKA", False),
+            ("aLlegro", False),
+        ],
+        ids=[
+            "Simple isogram, should return 'True'",
+            "Simple non isogram, should return 'False'",
+            "Digits only, no duplicates, should return 'True'",
+            "Empty string, should return 'True'",
+            "Big lowercase text, should return 'True'",
+            "Big uppercase text, should return 'False'",
+            "Case sensitive simple isogram, should return 'True'",
+            "Case sensitive simple non isogram, should return 'False'",
+            "String with different cases of the same letter is not an isogram"
+        ]
+    )
+    def test_if_word_is_isogram(
+            self,
+            word: str,
+            expected_result: bool
+    ) -> None:
+        assert is_isogram(word) == expected_result
+
+
