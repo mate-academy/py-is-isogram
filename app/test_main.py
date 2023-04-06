e4c5fde511cdedba5d88d7e79dd823d52df93aed
Iryna Ishchenko <irynaira@gmail.com>
2023-04-06 18:01:57 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,34 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,expected_result",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+    ]
+)
+def test_if_result_has_right_format(word: str, expected_result: bool) -> None:
+    result = is_isogram(word)
+    assert (
+        isinstance(result, bool)
+    ), "result should be boolean"
+
+
+@pytest.mark.parametrize(
+    "word,expected_result",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+    ]
+)
+def test_if_cat_has_years(word: str, expected_result: bool) -> None:
+    assert (
+        is_isogram(word) == expected_result
+    ), f"{word} should be {expected_result}"
+
