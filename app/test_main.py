f0181f0d4ad203706775b4681867fd4dbbb57f51
Evgen Shepelenko <ShepelenkoEV@gmail.com>
2023-04-11 16:36:21 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,24 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True)
+    ]
+)
+def tests_checks_all_edged_situations(
+        word: str,
+        result: bool
+) -> None:
+    assert is_isogram(word) == result
+
+
+def test_incorrect_type_of_data() -> None:
+    with pytest.raises(AttributeError):
+        is_isogram(5)
