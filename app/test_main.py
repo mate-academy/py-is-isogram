7203ffa66a2550bfea0ff70e26f900ccc96fac64
Vitalii Shkrobatskyi <vetali5700@gmail.com>
2023-04-18 21:47:59 +0300

    flake8 problem solved

--- app/test_main.py
+++ app/test_main.py
@@ -2,8 +2,8 @@ from app.main import is_isogram
 
 
 def test_should_return_true() -> None:
-    assert is_isogram('playgrounds') == is_isogram('') is True
+    assert is_isogram("playgrounds") == is_isogram("") is True
 
 
 def test_should_return_false() -> None:
-    assert is_isogram('look') == is_isogram('Adam') is False
+    assert is_isogram("look") == is_isogram("Adam") is False
