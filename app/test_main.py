0facb126923383e7b95551a3548a01342ed921c8
Vlad Tetruk <vladteteruk01@gmail.com>
2023-05-07 15:35:27 +0300

    Add tests for is_isogram function

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        pytest.param("", True, id="test empty string"),
+        pytest.param("text", False, id="test not isogram"),
+        pytest.param("playgrounds", True, id="test isogram"),
+        pytest.param("Mm", False, id="test caps")
+    ]
+)
+def test_isogram(word: str, result: bool) -> None:
+    assert (is_isogram(word) == result
+            ), f"{word} is isogram == {result}"
