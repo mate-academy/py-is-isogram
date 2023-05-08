3aa4d2fa36f533482d74d4443faeb5cf2fcbb79c
Alim Selemetov <alimselemetow@gmail.com>
2023-05-08 19:43:34 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,54 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "accept_str, result",
+        [
+            pytest.param(
+                "playgrounds",
+                True,
+                id="check if exist repeated words"
+            ),
+            pytest.param(
+                "look",
+                False,
+                id="check if exist repeated words"
+            ),
+            pytest.param(
+                "Adam",
+                False,
+                id="check if exist repeated words"
+            ),
+            pytest.param(
+                "",
+                True,
+                id="Empty str is isogram"
+            )
+        ]
+
+    )
+    def test_str_is_isogram(
+            self,
+            accept_str: str,
+            result: bool
+    ) -> None:
+        assert is_isogram(accept_str) == result
+
+    @pytest.mark.parametrize(
+        "value, expected_error",
+        [
+            pytest.param(
+                123445,
+                AttributeError,
+                id="value should be str not int"
+            )
+        ]
+    )
+    def test_is_value_str(
+            self,
+            value: str, expected_error: AttributeError
+    ) -> None:
+        with pytest.raises(expected_error):
+            is_isogram(value)
