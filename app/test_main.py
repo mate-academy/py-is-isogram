f01d6691986b709cd670ac5121db8137bfdbdd90
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-05-09 07:29:21 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -14,5 +14,5 @@ from app.main import is_isogram
 )
 def test_is_isogram(word: str,
                     expected: bool) -> None:
-    error_message = f"Function should return {expected} when {word}"
+    error_message = f"Function should return '{expected}' when '{word}'"
     assert is_isogram(word) == expected, error_message
