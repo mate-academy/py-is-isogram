ae69891937610ec1392e774656c814da80048672
Oleh Oryshchuk <olehoryshshuk@gmail.com>
2023-05-09 07:28:29 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    ("word", "expected"),
+    [
+        ("playgrounds", True),
+        ("", True),
+        ("aDAm", False),
+        ("floor", False)
+    ]
+)
+def test_is_isogram(word: str,
+                    expected: bool) -> None:
+    error_message = f"Function should return {expected} when {word}"
+    assert is_isogram(word) == expected, error_message
