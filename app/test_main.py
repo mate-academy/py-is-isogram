0bd8a100e18d192fbbe1c740dfdb1c99c5a73977
Bohdan11Dii <bodyabrovdiy@gmail.com>
2023-05-22 18:30:45 +0300

    Create pytest for Is isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_the_empty_string_is_an_isogram() -> None:
+    assert is_isogram("") is True
+
+def test_can_be_is_isogram() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_the_function_should_be_case_insensitive() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_consecutive_letters_are_not_isogrm() -> None:
+    assert is_isogram("look") is False
