6bd8cb97956009b563405260ae756bb47b7c7e29
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-05-26 10:57:00 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,16 +2,16 @@ from app.main import is_isogram
 
 
 def test_() -> None:
-    assert is_isogram('playgrounds') is True
+    assert is_isogram("playgrounds") is True
 
 
 def test_should_return_false_simply() -> None:
-    assert is_isogram('look') is False
+    assert is_isogram("look") is False
 
 
 def test_should_return_false_when_uppercase() -> None:
-    assert is_isogram('Adam') is False
+    assert is_isogram("Adam") is False
 
 
 def test_should_return_true_when_empty() -> None:
-    assert is_isogram('') is True
+    assert is_isogram("") is True
