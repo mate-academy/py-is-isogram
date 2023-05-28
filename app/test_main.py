8b324b6a6ba72b84caff9eeb1f5e6940885141a0
Andrii Kuzmenkov <kuzmenkov.andr@gmail.com>
2023-05-28 13:00:12 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
+import pytest
+
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "word,expected_result",
+        [
+            pytest.param(
+                "", True, id="Empty string should return True"
+            ),
+            pytest.param(
+                "gEometry", False, id="Should be case-insensitive"
+            ),
+            pytest.param(
+                "playgrounds", True, id="Should return correct result"
+            ),
+            pytest.param(
+                "zoom", False, id="Consecutive letters should be count as non-isogram"
+            )
+        ]
+    )
+    def test_should_return_correct_result(self, word, expected_result):
+        assert is_isogram(word) == expected_result
