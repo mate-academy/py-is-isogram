da9b23dbfc1b0e1e782c5b84503d42188b53f3ea
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-06-08 14:59:08 +0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_should_return_false_simply() -> None:
+    assert is_isogram("look") is False
+
+
+def test_should_return_false_when_uppercase() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_should_return_true_when_empty() -> None:
+    assert is_isogram("") is True
