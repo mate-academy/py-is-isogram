60e3c4616bd08d2e07ccc96f0da890988bf68ced
kovaliskoveronika <veronika.kovalisko.knm.2020@lpnu.ua>
2023-06-14 15:59:16 +0300

    My tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,27 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize("value,result",
+                         [("playgrounds", True),
+                          ["look", False]])
+def test_usual_input(value: str, result: bool) -> None:
+    assert is_isogram(value) is result
+
+
+def test_empty_line() -> None:
+    assert is_isogram("") is True
+
+
+def test_input_upper_letters() -> None:
+    assert is_isogram("Adam") is False
+
+
+@pytest.mark.parametrize("value",
+                         [1, 2.3, {}, [], ()])
+def test_correct_errors(value: any) -> None:
+    try:
+        is_isogram(value)
+    except (AttributeError, TypeError):
+        pass
