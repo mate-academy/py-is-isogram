df3feea9f9f1416079cf5136ec563062b1a6eeaf
Erik Agayan <erik.agayan1@gmail.com>
2023-06-14 18:23:44 +0300

    Solution #1

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_return_true_if_all_letters_unique() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_return_false_if_letter_not_unique() -> None:
+    assert is_isogram("look") is False
+
+
+def test_return_false_if_ignoring_letter_case() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_return_true_if_string_is_empty() -> None:
+    assert is_isogram("") is True
