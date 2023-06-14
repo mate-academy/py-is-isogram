5c6c262207e21981650ca08b46a63fa03e6bf9ca
Tarasidze <dtar84@ukr.net>
2023-06-14 17:06:18 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
+import pytest
+
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "income_string, exp_result",
+    [
+        pytest.param("", True, id="empty string should return True"),
+        pytest.param("Adam", False, id="letter case and isogram"),
+        pytest.param("look", False, id="in case when str isn't isogram")
+    ]
+)
+def test_is_isogram(income_string: str, exp_result: bool) -> None:
+    assert is_isogram(income_string) == exp_result
