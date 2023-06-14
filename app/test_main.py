8a2948343987ad4725c2dbf79392a5696d565464
Iryna Melnyk <melnyk7iryna@gmail.com>
2023-06-14 12:51:50 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,24 @@
 from app.main import is_isogram
 
-# write your code here
+import pytest
+
+
+@pytest.mark.parametrize(
+    "word, expected_result",
+    [
+        ("", True),
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("MaMa", False),
+    ],
+    ids=[
+        "test_empty_word",
+        "test_isogram_word",
+        "test_word_in_lower_case",
+        "test_word_with_uppercase_character",
+        "test_word_with_repeating_syllable"
+    ]
+)
+def test_is_isogram(word, expected_result):
+    assert is_isogram(word) == expected_result
