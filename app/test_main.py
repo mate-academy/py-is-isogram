4c9551cec3a47eeb002ab67837b138dfa5626385
Eugene Polunin <altahoma@gmail.com>
2023-06-14 18:04:55 +0300

    Implemented TestIsIsogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,28 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "word, expected_result",
+        [
+            pytest.param("", True, id="return True for empty string"),
+            pytest.param("a", True, id="return True for one letter"),
+            pytest.param(
+                "playgrounds", True, id="return True for long word"
+            ),
+            pytest.param(
+                "look", False, id="return False if letters duplicated"
+            ),
+            pytest.param(
+                "Adam",
+                False,
+                id="return False if letters duplicated and different cases",
+            ),
+        ],
+    )
+    def test_check_word_correctly(
+        self, word: str, expected_result: bool
+    ) -> None:
+        assert is_isogram(word) == expected_result
