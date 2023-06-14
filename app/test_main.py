a926bec7759154f653cf7829e31f7c9a9a4ec450
DmytroTsehelnyk <dmytrotsehelnyk@yahoo.com>
2023-06-14 13:21:02 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,31 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "phrase, expected_result",
+        [
+            ("playgrounds", True),
+            ("look", False),
+            ("Adam", False),
+            ("", True),
+            (" ", True),
+            ("  ", False),
+        ],
+        ids=[
+            "should return True if each letter in phrase occurs the same number of times",
+            "should return False if phrase have more then 1 same letter",
+            "should return False if phrase have more then 1 same letter (big or small)",
+            "should return True if phrase is empty",
+            "should return True if phrase contain 1 space",
+            "should return False if phrase contain more then 1 space",
+        ]
+    )
+    def test_correct_combination(
+            self,
+            phrase: str,
+            expected_result: bool
+    ) -> None:
+        assert is_isogram(phrase) == expected_result
