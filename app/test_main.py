51de72e5a9589aef6feab8107f22ebe26c7305bf
Anton Hryhorenko <grygorenko23@gmail.com>
2023-06-20 21:11:16 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,30 @@
+import pytest
+
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, expected_result",
+    [
+        pytest.param(
+            "playgrounds",
+            True,
+            id="it is not an isogram."),
+        pytest.param(
+            "",
+            True,
+            id="Empty string is an isogram."),
+        pytest.param(
+            "Adam",
+            False,
+            id="Not only consecutive letters are not an isogram."),
+        pytest.param(
+            "look",
+            False,
+            id="Not only non-consecutive letters are not an isogram."),
+
+    ]
+)
+def test_is_isogram(word: str, expected_result: bool) -> None:
+    assert is_isogram(word) == expected_result
