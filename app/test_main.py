671d19818107e608127ee6140b84b3ed32cfaa43
arturiermolenko <arturiermolenko@gmail.com>
2023-06-23 08:56:07 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,32 @@
 from app.main import is_isogram
 
-# write your code here
+import pytest
+
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "word,b_ool",
+        [
+            pytest.param(
+                "",
+                True,
+                id="should return true for empty string"
+            ),
+            pytest.param(
+                "Mamont",
+                False,
+                id="String with different cases of same later return False"
+            ),
+            pytest.param(
+                "mmont",
+                False,
+                id="String with consecutive same later return False"
+            )
+        ]
+    )
+    def test_correct_return(
+            self,
+            word: str,
+            b_ool: bool
+    ) -> None:
+        assert is_isogram(word) == b_ool
