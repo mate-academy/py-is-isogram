2c28c49a93183015fc5fe942f6cec1047f8d5672
Yaroslav Lysiuk <quiet.klirik@gmail.com>
2023-07-05 22:20:55 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,41 @@
+from typing import Any
+
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,expected_result",
+    [
+        ("word", True),
+        ("wword", False),
+        ("wowrd", False),
+        ("Wword", False),
+        ("", True),
+        ("Hi\nworld\n!", False)
+    ]
+)
+def test_values(
+        word: str,
+        expected_result: bool
+) -> None:
+    assert is_isogram(word) == expected_result
+
+
+@pytest.mark.parametrize(
+    "word,expected_error",
+    [
+        (int(), AttributeError),
+        (set(), AttributeError),
+        (dict(), AttributeError),
+        (tuple(), AttributeError),
+        (complex(), AttributeError),
+    ]
+)
+def test_exceptions(
+        word: Any,
+        expected_error: Exception
+) -> None:
+    with pytest.raises(expected_error):
+        is_isogram(word)
