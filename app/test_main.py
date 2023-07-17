0dc889f6626e471e7d9b9063123f084c798e274a
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2023-07-17 20:00:40 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,34 @@
-from app.main import is_isogram
+from typing import Type
 
-# write your code here
+from app.main import is_isogram
+import pytest
+
+
+@pytest.mark.parametrize(
+    "word,expected_result",
+    [("playground", True),
+     ("", True),
+     ("look", False),
+     ("Adam", False),
+     ("develop", False),
+     ("documentary", True),
+     ("Filmography", True)]
+)
+def test_if_return_is_correct(
+        word: str,
+        expected_result: bool) -> None:
+    assert expected_result == is_isogram(word)
+
+
+@pytest.mark.parametrize(
+    "word,expected_error",
+    [(2, AttributeError),
+     ({"look"}, AttributeError),
+     ((14, 23), AttributeError),
+     (["15, 15"], AttributeError)]
+)
+def test_raise_errors_correctly(
+        word: str,
+        expected_error: Type[BaseException]) -> None:
+    with pytest.raises(expected_error):
+        is_isogram(word)
