5d91417470740ccc64aae6efb2b574dfbfc3c7cd
lubart2517 <lubomirvihvatniuk@gmail.com>
2023-07-19 19:22:00 +0300

    implemented test

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        pytest.param("", True, id="check_empty"),
+        pytest.param("playgrounds", True, id="check_true"),
+        pytest.param("Adam", False, id="check_case_sensitivity"),
+        pytest.param("look", False, id="check_false"),
+
+    ]
+)
+def test_get_coins(word: str, result: bool) -> None:
+    assert (
+        is_isogram(word) == result
+    ), f"Is isogram from {word} should be equal to {result}"
