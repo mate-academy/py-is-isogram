01d2c7ea4167f04c8c7345473f4af73bc78bc1a8
Artem Putsenko <artempucenko3@gmail.com>
2023-07-26 11:15:47 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,38 +1,33 @@
-import pytest
 from app.main import is_isogram
 
 
-def test_an_array() -> bool:
-    assert type(is_isogram("wer")) == bool
-
-
-def test_empty_array() -> bool:
+def test_empty_str() -> None:
     assert is_isogram("") is True
 
 
-def test_different_cases_array() -> bool:
-    assert is_isogram("Vv") is False
+def test_1_letter() -> None:
+    assert is_isogram("a") is True
 
 
-def test_different_cases_array1() -> bool:
-    assert is_isogram("343") is False
+def test_2_same_letters() -> None:
+    assert is_isogram("aa") is False
 
 
-@pytest.mark.parametrize(
-    "word, expected",
-    [
-        ("fgh", True),
-        ("ddff", False),
-        ("fgds", True),
-        ("ghhyyy", False),
-    ],
-    ids=[
-        "test_word_fgh",
-        "test_word_ddff",
-        "test_word_fgds",
-        "test_word_ghhyyy",
-    ]
-)
-def test_main_get(word: str, expected: bool) -> bool:
-    result = is_isogram(word)
-    assert result == expected
+def test_str_with_different_cases() -> None:
+    assert is_isogram("Aa") is False
+
+
+def test_2_different_letters() -> None:
+    assert is_isogram("ab") is True
+
+
+def test_long_word() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_str_with_space() -> None:
+    assert is_isogram("a b") is True
+
+
+def test_2_words() -> None:
+    assert is_isogram("one two") is False
