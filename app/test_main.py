f8ec7e3c42c4db4291f9f4a780040ebb84283d7c
Marina <marishka28023@gmail.com>
2023-07-28 16:07:27 +0200

    'solution2'

--- app/test_main.py
+++ app/test_main.py
@@ -11,5 +11,8 @@ from app.main import is_isogram
         ("", True)
     ]
 )
-def test_checks_whether_a_word_is_an_isogram(word: str, expected_result: bool):
+def test_checks_whether_a_word_is_an_isogram(
+        word: str,
+        expected_result: bool
+) -> None:
     assert is_isogram(word) == expected_result
