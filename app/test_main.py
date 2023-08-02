5313808b46976ba80ae4da63e45abfedc1b71d19
Vladimir Byshevets <texnadzor.info@gmail.com>
2023-08-02 14:05:44 +0300

    Solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -1,12 +1,21 @@
 from app.main import is_isogram
 import pytest
 
+
 @pytest.mark.parametrize(
     "word, expected_result",
     [
         pytest.param("playgrounds", True, id="should return True for isogram"),
-        pytest.param("look", False, id="should return False when same letter is lowercase"),
-        pytest.param("Adam", False, id="should return False when same letter in different cases"),
+        pytest.param(
+            "look",
+            False,
+            id="should return False when same letter is lowercase"
+        ),
+        pytest.param(
+            "Adam",
+            False,
+            id="should return False when same letter in different cases"
+        ),
         pytest.param("", True, id="should return True for an empty line")
     ]
 )
