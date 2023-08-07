5ac1af9ec24581b165e6bd056a3f6b110b135f38
niksgawrilenko <90038040+niksgawrilenko@users.noreply.github.com>
2023-08-07 14:31:25 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_should_return_true_when_word_is_isogram() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_when_word_with_lower_letters_and_is_not_isogram() -> None:
+    assert is_isogram("look") is False
+
+
+def test__when_one_letter_is_lower_and_another_is_upper() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_with_empty_word() -> None:
+    assert is_isogram("") is True
