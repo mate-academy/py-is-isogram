5dc6554f7a70b85a90489fa3447bdd3f6c49a708
jinjo <vitaly.tyagunov@gmail.com>
2023-08-12 23:44:01 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,9 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_should_return_true_when_string_empty() -> None:
+    assert is_isogram("") == True
+
+
+def test_should_return_false_with_different_cases_same_letters() -> None:
+    assert is_isogram("Aloha") == False
