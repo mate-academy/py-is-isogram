0bbed0babe850d3d29ae2a32e58c05bf17e8b584
Yeva <yevabrookman@gmail.com>
2023-08-22 15:18:35 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,27 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "word, expected_result",
+        [
+            pytest.param(
+                "playgrounds", True, id="check if count occurrences"
+            ),
+            pytest.param(
+                "look", False, id="check with two occurrences"
+            ),
+            pytest.param(
+                "Adam", False, id="check if upper and lower the same letter"
+            ),
+            pytest.param(
+                "", True, id="check if empty string is isogram"
+            )
+        ]
+    )
+    def test_is_isogram(self, word: str, expected_result: bool) -> None:
+        result = is_isogram(word)
+
+        assert expected_result == result
