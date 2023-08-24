ff74496e4a98ae8c45ddd9421f33ded74dba48df
OlegatorLE <oleh.travin@gmail.com>
2023-08-24 16:09:21 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -32,5 +32,8 @@ import pytest
         ),
     ]
 )
-def test_word_should_be_isogram(word: str, expected: bool) -> None:
+def test_word_should_be_isogram(
+        word: str,
+        expected: bool
+) -> None:
     assert is_isogram(word) == expected
