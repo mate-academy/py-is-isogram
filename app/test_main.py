43ee4d5ed6ee18dfc7cb1cf8337c2c2263885610
Max Katkalov <makskatkalov@gmail.com>
2023-08-24 21:01:49 +0300

    tests added for main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,27 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        pytest.param(
+            "playgrounds",
+            True,
+            id="there are no repeating letters in the word",
+        ),
+        pytest.param("", True, id="empty string is isogram"),
+        pytest.param(
+            "look",
+            False,
+            id="in a word in a word we have a repetition of letters",
+        ),
+        pytest.param(
+            "Adam", False, id="different cases of one letter is a repetition"
+        ),
+        pytest.param("UP", True, id="word is upper"),
+    ],
+)
+def test_is_isogram_return_correct_true(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
