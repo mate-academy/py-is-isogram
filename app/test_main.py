2fb7532f30434ad393da4eb9d97950451a96c631
oreny <orenycdota@gmail.com>
2023-08-25 12:51:28 +0300

    implemented required tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,30 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "string,result",
+    [
+        pytest.param(
+            "playgrounds",
+            True,
+            id="should return true if the "
+               "word don't have any repeating letters",
+        ),
+        pytest.param(
+            "look",
+            False,
+            id="should return false if the word has two same letters",
+        ),
+        pytest.param(
+            "Adam",
+            False,
+            id="should return false if there "
+               "is two same letters despite case difference",
+        ),
+        pytest.param("", True, id="should return true if an empty string"),
+    ],
+)
+def test_is_isogram(string, result) -> None:
+    assert is_isogram(string) == result
