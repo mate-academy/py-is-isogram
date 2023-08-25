0bc1fc5ffd0e49fc47fd4352f84abf61bbd8c2a0
CaCtUs1k <denischernish19012004@gmail.com>
2023-08-25 16:11:27 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string():
+    assert is_isogram("") == True
+
+
+def test_case_insensitive():
+    assert is_isogram("Lola") == False
+
+
+def test_different_letters():
+    assert is_isogram("abcdeghijkl") == True
+
+
+def test_non_isogram():
+    assert is_isogram("abba") == False
