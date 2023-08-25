0af32e64a7329c531f9232ec7c88cd5303a79b2e
Kateryna Zadorozhna <136272476+katryana@users.noreply.github.com>
2023-08-25 16:20:38 +0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -14,7 +14,7 @@ from app.main import is_isogram
     ]
 )
 def test_empty_string_should_be_isogram(
-        initial_string,
-        expected_output
+        initial_string: str,
+        expected_output: bool
 ) -> None:
     assert is_isogram(initial_string) == expected_output
