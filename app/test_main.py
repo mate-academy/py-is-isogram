5ab66f8207e9914a8aa144eebc917f664a5b9b09
WoyDiam <freedom.channel2003@gmail.com>
2023-08-29 16:15:15 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,33 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestFunctionWorksCorrectly:
+    @pytest.mark.parametrize(
+        "data,returned",
+        [
+            pytest.param(
+                "Adam",
+                False,
+                id="Function should be case-insensitive"
+            ),
+            pytest.param(
+                "",
+                True,
+                id="Function should be return `True` when string empty"
+            ),
+            pytest.param(
+                "playground",
+                True,
+                id="Function should be return `True` for `playground`"
+            ),
+            pytest.param(
+                "look",
+                False,
+                id="Function should be return `False` for `look`"
+            )
+        ]
+    )
+    def test_func(self, data: str, returned: bool) -> None:
+        assert is_isogram(data) is returned
