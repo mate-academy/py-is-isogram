afbee511328c1fa325b7a45dec468e17e08caa83
Mikhail <tiunovmixs@gmail.com>
2023-09-02 12:34:25 +0300

    Implemented Is isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram_be_empty() -> None:
+    assert is_isogram("")
+
+
+def test_is_isogram_be_repeating_letters() -> None:
+    assert not is_isogram("apple")
+
+
+def test_is_isogram_no_repeating_letters() -> None:
+    assert is_isogram("machine")
+
+
+def test_is_isogram_be_case_insensitive() -> None:
+    assert not is_isogram("Adam")
