209b6cf8240dcf69daee04d49acc86dbfc0040f1
Mikhail <tiunovmixs@gmail.com>
2023-09-02 13:28:34 +0300

    Solution with a decorator

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,57 @@
+from typing import Any
+import pytest
+
 from app.main import is_isogram
 
 
-def test_is_isogram_be_empty() -> None:
-    assert is_isogram("")
+class TestIsogram:
+    @pytest.mark.parametrize(
+        "word, expected",
+        [
+            pytest.param(
+                "",
+                True,
+                id="test is_isogram be empty"
+            ),
+            pytest.param(
+                "apple",
+                False,
+                id="test is_isogram be repeating letters"
+            ),
+            pytest.param(
+                "machine",
+                True,
+                id="test is_isogram no repeating letters"
+            ),
+            pytest.param(
+                "Adam",
+                False,
+                id="test is_isogram be case insensitive"
+            )
+        ]
+    )
+    def test_(self, word: str, expected: bool) -> None:
+        assert is_isogram(word) == expected
 
-
-def test_is_isogram_be_repeating_letters() -> None:
-    assert not is_isogram("apple")
-
-
-def test_is_isogram_no_repeating_letters() -> None:
-    assert is_isogram("machine")
-
-
-def test_is_isogram_be_case_insensitive() -> None:
-    assert not is_isogram("Adam")
+    @pytest.mark.parametrize(
+        "string, exception_err",
+        [
+            pytest.param(
+                {},
+                AttributeError,
+                id="checks for the input string"
+            ),
+            pytest.param(
+                1,
+                AttributeError,
+                id="expected error when entering a number"
+            )
+        ]
+    )
+    def test_raising_error_correctly(
+            self,
+            string: Any,
+            exception_err: Any
+    ) -> None:
+        with pytest.raises(exception_err):
+            is_isogram(string)
