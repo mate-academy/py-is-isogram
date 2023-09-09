e2683843c7577d7141f1bb6d1d7b997d333446f9
Artem Manko <Kruto.teny@gmail.com>
2023-09-09 16:14:17 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,23 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_lower_and_upper_case_characters():
+    assert is_isogram("Ana") is False
+
+
+def test_duplicated_characters():
+    assert is_isogram("Anaana") is False
+
+
+def test_should_return_true_for_empty_string():
+    assert is_isogram("") is True
+
+
+def test_should_return_true_for_isogram():
+    assert is_isogram("Ane") is True
+
+
+def test_for_non_letter_characters():
+    assert is_isogram("An00") is False
+
+
