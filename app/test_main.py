b98c88c0281d6b099a3ce0945f636fd3c8e79608
Artem <hartem472@gmail.com>
2023-09-10 22:44:28 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_non_consecutive_letters_are_not_isogram():
+    assert is_isogram("non") is False
+
+
+def test_empty_string_is_isogram():
+    assert is_isogram("") is True
+
+
+def test_caps_and_lower():
+    assert is_isogram("boOk") is False
