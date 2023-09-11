ef07f466bc60333a5b6a9af417922a6bc691b924
Ruslan <rusipbox@gmail.com>
2023-09-11 17:15:54 +0300

    Test(py-is-isogram)

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,29 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram_true():
+    assert is_isogram("playgrounds") is True
+
+
+def test_is_isogram_false() -> None:
+    assert is_isogram("look") is False
+
+
+def test_is_isogram_case_insensitive() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_empty_string_is_isogram() -> None:
+    assert is_isogram("") is True
+
+
+def test_is_isogram_mixed_case() -> None:
+    assert is_isogram("MixED") is True
+
+
+def test_consecutive_letters_are_not_isogram() -> None:
+    assert is_isogram("abba") is False
+
+
+def test_non_consecutive_letters_are_not_isogram() -> None:
+    assert is_isogram("hello") is False
