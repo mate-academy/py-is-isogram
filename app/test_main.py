ea4dda5ca50ae5dd6657d27c352bc29ac067e9e5
Maksim Stahovskij <maksss196@gmail.com>
2023-09-18 09:30:37 +0300

    py-is-isogram

--- app/test_main.py
+++ app/test_main.py
@@ -2,22 +2,13 @@ import pytest
 from app.main import is_isogram
 
 
-def test_check_type_return_is_bool() -> None:
-    assert isinstance(is_isogram("Maksim"), bool)
-
-
-def test_check_should_be_case_insensitive_up_register() -> None:
-    assert is_isogram(word="MAKSI") == True
-
-
-def test_check_is_empty_argument_return_true() -> None:
-    assert is_isogram(word="") == True
-
-
-def test_check_arguments_with_different_cases() -> None:
-    assert is_isogram(word="Maksim") == False
-
-
-def test_check_argument_with_same_latter_is_false() -> None:
-    assert is_isogram(word="abrakadabra") == False
-
+@pytest.mark.parametrize("word, expected_result", [
+    ("MAKSI", True),
+    ("", True),
+    ("Maksim", False),
+])
+def test_check_different_cases_word_is_isogram(
+        word: str,
+        expected_result: bool
+) -> None:
+    assert is_isogram(word) == expected_result
