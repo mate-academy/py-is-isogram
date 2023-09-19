7264ceb914f6b611f963b264e5873f6fe005ec5a
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-19 18:50:00 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -3,29 +3,29 @@ from app.main import is_isogram
 
 def test_empty_string_is_isogram() -> None:
     word = ""
-    assert is_isogram(word) == True
+    assert is_isogram(word)
 
 
 def test_isogram_lowercase() -> None:
     word = "isogram"
-    assert is_isogram(word) == True
+    assert is_isogram(word)
 
 
 def test_isogram_uppercase() -> None:
     word = "ISOGram"
-    assert is_isogram(word) == True
+    assert is_isogram(word)
 
 
 def test_non_isogram() -> None:
     word = "hello"
-    assert is_isogram(word) == False  # Виправлено порівняння
+    assert not is_isogram(word)
 
 
 def test_mixed_case_isogram() -> None:
     word = "AbCdEfG"
-    assert is_isogram(word) == True
+    assert is_isogram(word)
 
 
 def test_non_alpha_characters() -> None:
     word = "12345"
-    assert is_isogram(word) == True
+    assert is_isogram(word)
