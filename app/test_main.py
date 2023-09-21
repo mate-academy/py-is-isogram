84073c3c40b400947a32541740b96ff118970cfa
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-21 18:45:09 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -2,45 +2,45 @@ from app.main import is_isogram
 
 
 def test_empty_string_is_isogram() -> None:
-    if word == "":
-        assert is_isogram(word) == True
+    word = ""
+    assert is_isogram(word) == True
 
 
 def test_isogram_lowercase() -> None:
-    if word == "isogram":
-        assert is_isogram(word) == True
+    word = "isogram"
+    assert is_isogram(word) == True
 
 
 def test_isogram_uppercase() -> None:
-    if word == "ISOGram":
-        assert is_isogram(word) == True
+    word = "ISOGram"
+    assert is_isogram(word) == True
 
 
 def test_non_isogram() -> None:
-    if word == "hello":
-        assert is_isogram(word) == False
+    word = "hello"
+    assert is_isogram(word) == False
 
 
 def test_mixed_case_isogram() -> None:
-    if word == "AbCdEfG":
-        assert is_isogram(word) == True
+    word = "AbCdEfG"
+    assert is_isogram(word) == True
 
 
 def test_non_alpha_characters() -> None:
-    if word == "12345":
-        assert is_isogram(word) == True
+    word = "12345"
+    assert is_isogram(word) == True
 
 
 def test_isogram_with_whitespace() -> None:
-    if word == "this is an isogram":
-        assert is_isogram(word) == False
+    word = "this is an isogram"
+    assert is_isogram(word) == False
 
 
 def test_isogram_with_hyphen() -> None:
-    if word == "twenty-one":
-        assert  is_isogram(word) == True
+    word = "twenty-one"
+    assert is_isogram(word) == False
 
 
 def test_isogram_with_apostrophe() -> None:
-    if word == "I'm":
-        assert is_isogram(word) == True
+    word = "I'm"
+    assert is_isogram(word) == True
