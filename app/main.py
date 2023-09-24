033e37417d4719dee0ab6af1cff9945f6e74cc7a
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-24 17:03:19 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,11 +1,10 @@
 def is_isogram(word: str) -> bool:
-    word = word.lower()  # Переведемо всі букви у нижній регістр
-    letters_seen = set()
-
-    for letter in word:
-        if letter.isalpha():
-            if letter in letters_seen:
+    word_lower = word.lower()
+    # Check if it's an isogram
+    seen_chars = set()
+    for char in word_lower:
+        if char.isalpha():
+            if char in seen_chars:
                 return False
-            letters_seen.add(letter)
-
+            seen_chars.add(char)
     return True
