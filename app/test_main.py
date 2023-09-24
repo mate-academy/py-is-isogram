8f6f38bd62e4de4d2eb33bf0d2b2cecb5d799960
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-24 17:34:50 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,47 +1,20 @@
+import pytest
 from app.main import is_isogram
 
+def test_is_isogram_with_empty_string() -> None:
+    assert is_isogram("") is True
 
-def test_empty_string_is_isogram() -> None:
-    word = ""
-    assert is_isogram(word) is True
+def test_is_isogram_with_unique_characters() -> None:
+    assert is_isogram("playgrounds") is True
 
+def test_is_isogram_with_repeated_characters() -> None:
+    assert is_isogram("look") is False
 
-def test_isogram_lowercase() -> None:
-    word = "isogram"
-    assert is_isogram(word) is True
+def test_is_isogram_with_mixed_case() -> None:
+    assert is_isogram("Adam") is False
 
+def test_is_isogram_with_mixed_case_unique_characters() -> None:
+    assert is_isogram("Mma") is False
 
-def test_non_isogram() -> None:
-    word = "hello"
-    assert is_isogram(word) is False
-
-
-def test_mixed_case_isogram() -> None:
-    word = "AbCdEfG"
-    assert is_isogram(word) is True
-
-
-def test_non_alpha_characters() -> None:
-    word = "12345"
-    assert is_isogram(word) is True
-
-
-def test_isogram_with_whitespace() -> None:
-    word = "this is an isogram"
-    assert is_isogram(word) is False
-
-
-def test_isogram_with_hyphen() -> None:
-    word = "twenty-one"
-    assert is_isogram(word) is False
-
-
-def test_isogram_with_apostrophe() -> None:
-    word = "I'm"
-    assert is_isogram(word) is True
-
-
-def test_non_consecutive_letters_are_not_isogram() -> None:
-    word = "hello"
-    assert not is_isogram(word), "Word with non-consecutive " \
-                                 "letters should not be an isogram"
+if __name__ == "__main__":
+    pytest.main()
