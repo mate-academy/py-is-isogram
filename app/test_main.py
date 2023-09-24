1d7d7acbff94b31a425fe901cfc23d1c178c3370
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-24 16:58:40 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -11,10 +11,6 @@ def test_isogram_lowercase() -> None:
     assert is_isogram(word) is True
 
 
-def test_isogram_uppercase() -> None:
-    word = "ISOGRAM"
-    assert is_isogram(word) is True
-
 
 def test_non_isogram() -> None:
     word = "hello"
