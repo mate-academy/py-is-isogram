736e1858bfeffd567db37949885113e10dd69507
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-24 17:10:56 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,10 +1,6 @@
 def is_isogram(word: str) -> bool:
     word_lower = word.lower()
-    # Check if it's an isogram
-    seen_chars = set()
-    for char in word_lower:
-        if char.isalpha():
-            if char in seen_chars:
-                return False
-            seen_chars.add(char)
+    for letter in word_lower:
+        if word_lower.count(letter) > 1:
+            return False
     return True
