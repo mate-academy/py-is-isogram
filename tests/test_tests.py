32e8065f44aed2a82f2d031579ce3f64d4a73805
Pobedilda <playfog3@gmail.com>
2023-10-03 11:46:48 +0300

    Solution

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -13,9 +13,9 @@ def test_isogram_is_case_insensitive(monkeypatch):
     monkeypatch.setattr(main, "is_isogram", case_sensitive_isogram)
 
     test_result = pytest.main(["app/test_main.py"])
-    assert test_result.value == 1, (
-        "String with different cases of the same letter is not an isogram."
-    )
+    assert (
+        test_result.value == 1
+    ), "String with different cases of the same letter is not an isogram."
 
 
 def test_empty_string_is_isogram(monkeypatch):
@@ -31,9 +31,7 @@ def test_empty_string_is_isogram(monkeypatch):
     monkeypatch.setattr(main, "is_isogram", non_empty_string_isogram)
 
     test_result = pytest.main(["app/test_main.py"])
-    assert test_result.value == 1, (
-        "Empty string is an isogram."
-    )
+    assert test_result.value == 1, "Empty string is an isogram."
 
 
 def test_non_consecutive_letters_are_not_isogram(monkeypatch):
@@ -44,23 +42,20 @@ def test_non_consecutive_letters_are_not_isogram(monkeypatch):
                 return False
         return True
 
-    monkeypatch.setattr(
-        main, "is_isogram", only_consecutive_letters_are_not_isogram
-    )
+    monkeypatch.setattr(main, "is_isogram", only_consecutive_letters_are_not_isogram)
 
     test_result = pytest.main(["app/test_main.py"])
-    assert test_result.value == 1, (
-        "Not only consecutive letters are not an isogram."
-    )
+    assert test_result.value == 1, "Not only consecutive letters are not an isogram."
 
 
 def test_consecutive_letters_are_not_isogram(monkeypatch):
     def only_non_consecutive_letters_are_not_isogram(word):
         word_lower = word.lower()
         for ind in range(1, len(word_lower) - 1):
-            if (word_lower.count(word_lower[ind]) >= 2 and
-                    not (word_lower[ind] == word_lower[ind + 1] or
-                         word_lower[ind] == word_lower[ind - 1])):
+            if word_lower.count(word_lower[ind]) >= 2 and not (
+                word_lower[ind] == word_lower[ind + 1]
+                or word_lower[ind] == word_lower[ind - 1]
+            ):
                 return False
         return True
 
@@ -69,6 +64,6 @@ def test_consecutive_letters_are_not_isogram(monkeypatch):
     )
 
     test_result = pytest.main(["app/test_main.py"])
-    assert test_result.value == 1, (
-        "Not only non-consecutive letters are not an isogram."
-    )
+    assert (
+        test_result.value == 1
+    ), "Not only non-consecutive letters are not an isogram."
