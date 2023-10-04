245f9cd2bd2d6674152155aaa25dc77605799d70
Alex <moleksii@gmail.com>
2023-10-04 16:21:09 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,30 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_check_empty_string() -> None:
+    assert is_isogram("")
+
+
+def test_check_string_with_same_letters() -> None:
+    result = is_isogram("look")
+    assert not result
+
+
+def test_check_string_with_diferent_letters() -> None:
+    result = is_isogram("playgrounds")
+    assert result
+
+
+def test_check_string_with_diferent_upper_letters() -> None:
+    result = is_isogram("PlayGrOunDs")
+    assert result
+
+
+def test_check_string_with_same_upper_letters() -> None:
+    result = is_isogram("lOok")
+    assert not result
+
+
+def test_check_string_with_integer() -> None:
+    result = is_isogram("10")
+    assert not result
