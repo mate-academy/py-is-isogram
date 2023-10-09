e28d2e406f755759c3f2e8fef1f88a1e83e329ed
yevpy <eugene2piskun@gmail.com>
2023-10-09 14:21:40 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_if_string_is_empty() -> None:
+    assert is_isogram("") is True
+
+
+def test_not_a_single_symbol() -> None:
+    assert is_isogram("sweet") is False
+
+
+def test_upper_case() -> None:
+    assert is_isogram("Trait") is False
+
+
+def test_different_upper_case() -> None:
+    assert is_isogram("pLaYgRoUnD") is True
+
+
+def test_same_upper_letter() -> None:
+    assert is_isogram("lOok") is False
+
+
+def test_correct_string() -> None:
+    assert is_isogram("playgrounds") is True
