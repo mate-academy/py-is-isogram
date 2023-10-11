d8de330c3e41e15008445e936509c586a8c77553
Vladyslav Sabo <lebwa1337@gmail.com>
2023-10-11 16:07:44 +0300

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -9,12 +9,10 @@ def test_should_return_false_if_word_not_case_insensitive() -> None:
     assert is_isogram("Adam") is False
 
 
-def test_should_return_false_if_word_has_more_then_one_similar_letter() -> None:
+def test_should_return_false_if_word_has_more_then_one_similar_letter(
+) -> None:
     assert is_isogram("look") is False
 
 
 def test_should_return_true_if_word_is_empy() -> None:
     assert is_isogram("") is True
-
-
-
