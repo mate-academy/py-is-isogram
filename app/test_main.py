409636d6f1997968f131903713e4eed151abe933
Serhii Berdianskyi <zeferot94@gmail.com>
2023-10-13 14:04:20 +0300

    Wrote tests for Is isogram function

--- app/test_main.py
+++ app/test_main.py
@@ -2,35 +2,25 @@ import pytest
 from app.main import is_isogram
 
 
-@pytest.mark.parametrize(
-    "word, result",
-    [
-        pytest.param(
-            "playgrounds",
-            True,
-            id="lowercase input word"
-        ),
-        pytest.param(
-            "PlayGrounds",
-            True,
-            id="input word with uppercase letters"
-        ),
-        pytest.param(
-            "",
-            True,
-            id="empty string is isogram"
-        ),
-        pytest.param(
-            "appear",
-            False,
-            id="input string with repeated letter P"
-        ),
-        pytest.param(
-            "Edward",
-            False,
-            id="input string with repeated letter D and one uppercase letter"
-        )
-    ]
-)
-def test_is_isogram(word: str, result: bool) -> None:
-    assert is_isogram(word) == result
+def test_is_isogram_with_unique_characters() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_is_isogram_with_empty_string() -> None:
+    assert is_isogram("") is True
+
+
+def test_is_isogram_with_repeated_characters() -> None:
+    assert is_isogram("wood") is False
+
+
+def test_is_isogram_with_mixed_case() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_is_isogram_with_mixed_case_unique_characters() -> None:
+    assert is_isogram("Fifa") is False
+
+
+if __name__ == "__main__":
+    pytest.main()
