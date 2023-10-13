c9bd6749ad9fe665bb86fc3b4ed48ecec73de02e
Serhii Berdianskyi <zeferot94@gmail.com>
2023-10-13 12:54:10 +0300

    Wrote tests for Is isogram function

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,36 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        pytest.param(
+            "playgrounds",
+            True,
+            id="lowercase input word"
+        ),
+        pytest.param(
+            "PlayGrounds",
+            True,
+            id="input word with uppercase letters"
+        ),
+        pytest.param(
+            "",
+            True,
+            id="empty string is isogram"
+        ),
+        pytest.param(
+            "appear",
+            False,
+            id="input string with repeated letter P"
+        ),
+        pytest.param(
+            "Edward",
+            False,
+            id="input string with repeated letter D and one uppercase letter"
+        )
+    ]
+)
+def test_is_isogram(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
