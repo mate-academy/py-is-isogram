bbeebd875529719e1c6522a59d96fb84129e6d92
Denys <den.prislipskyi@gmail.com>
2023-10-18 19:36:42 +0200

    testing

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_should_return_bool() -> None:
+    is_bool = is_isogram("playgrounds")
+    assert isinstance(is_bool, bool)
+
+
+def test_two_letters_extra() -> None:
+    empty = is_isogram("Adam")
+    assert empty is False
+
+
+def test_g() -> None:
+    result = is_isogram("Playgrounds")
+    assert result is True
+
+
+def test_empty_string() -> None:
+    empty = is_isogram("")
+    assert empty is True
+
+
+def test_two_letters_together() -> None:
+    result = is_isogram("look")
+    assert result is False
