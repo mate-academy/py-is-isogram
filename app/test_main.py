8ec40c6e3daa462d3bdd309fcd01206df77a5da8
Andrey Britov <abritov82@gmail.com>
2023-10-18 23:11:45 +0300

    Solusion_1

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        ("son", True),
+        ("boy", True),
+        ("banana", False)
+    ],
+)
+def test_is_isogram(word: str, result: bool) -> None:
+    assert (
+        is_isogram(word) == result
+    )
