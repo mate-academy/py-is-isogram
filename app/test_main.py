5ef861377c2e262b9d79452ee22f7b6c219b908a
Ivan <mozharivan17@gmail.com>
2023-10-27 13:35:09 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_word_starts_with_upper() -> None:
+    result = is_isogram("Adam")
+    assert result is False
+
+
+def test_empty_string() -> None:
+    result = is_isogram("")
+    assert result is True
+
+
+def test_word_with_same_letters() -> None:
+    result = is_isogram("look")
+    assert result is False
