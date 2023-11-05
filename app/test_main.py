808494ee664bb41523e86401a722f3d9a1cc12c3
Hryhoriy <hryhoriy.bilous@gmail.com>
2023-11-06 00:05:42 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,15 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string_is_true():
+    assert is_isogram("") is True
+
+
+def test_upper_lower_case():
+    assert is_isogram("Mm") is False
+
+
+def test_words_examples():
+    assert is_isogram('playgrounds') is True
+    assert is_isogram('look') is False
+    assert is_isogram('Adam') is False
