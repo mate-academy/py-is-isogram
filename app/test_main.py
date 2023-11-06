b52b9c0c7a18cc75eae4fa4ec10324ee151b9ae7
Hryhoriy <hryhoriy.bilous@gmail.com>
2023-11-06 21:54:08 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,15 +1,16 @@
+import pytest
 from app.main import is_isogram
 
 
-def test_empty_string_is_true():
+def test_empty_string_is_true() -> None:
     assert is_isogram("") is True
 
 
-def test_upper_lower_case():
+def test_upper_lower_case() -> None:
     assert is_isogram("Mm") is False
 
 
-def test_words_examples():
-    assert is_isogram('playgrounds') is True
-    assert is_isogram('look') is False
-    assert is_isogram('Adam') is False
+def test_words_examples() -> None:
+    assert is_isogram("playgrounds") is True
+    assert is_isogram("look") is False
+    assert is_isogram("Adam") is False
