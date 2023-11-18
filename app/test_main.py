85e625881bcedb122230f520a2202fab73efc126
ve4nick <ve4nick98@gmail.com>
2023-11-18 16:39:29 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_isogram_is_case_insensitive():
+    assert is_isogram("Adam") is False
+
+
+# def test_empty_string_is_isogram():
+#     assert is_isogram("") is True
+
+
+def test_non_consecutive_letters_are_not_isogram():
+    assert is_isogram()
