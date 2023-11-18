39f658625d0b356862880627248b225bac078118
ve4nick <ve4nick98@gmail.com>
2023-11-18 16:48:16 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,9 +5,9 @@ def test_isogram_is_case_insensitive() -> None:
     assert is_isogram("Adam") is False
 
 
-# def test_empty_string_is_isogram():
-#     assert is_isogram("") is True
+def test_empty_string_is_isogram():
+    assert is_isogram("") is True
 
 
-# def test_non_consecutive_letters_are_not_isogram() -> None:
-#     assert is_isogram()
+def test_non_consecutive_letters_are_not_isogram() -> None:
+    assert is_isogram()
