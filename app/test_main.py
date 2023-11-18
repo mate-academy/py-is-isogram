ee02612bc05cce2be0c62a568b5003d305ad62f9
ve4nick <ve4nick98@gmail.com>
2023-11-18 16:53:11 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -9,5 +9,5 @@ def test_empty_string_is_isogram() -> None:
     assert is_isogram("") is True
 
 
-def test_non_consecutive_letters_are_not_isogram() -> None:
-    assert is_isogram()
+def test_consecutive_letters_are_not_isogram() -> None:
+    assert is_isogram("look") is False
