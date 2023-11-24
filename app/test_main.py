3f2b7449587c65254ba39cf537e10a42a4999640
Serhii <serhii.domik@gmail.com>
2023-11-24 10:11:40 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,39 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "word, expected",
+        [
+            ("", True),
+            ("Adam", False),
+            ("playgrounds", True),
+            ("look", False)
+        ]
+    )
+    def test_is_isogram(
+            self,
+            word: str,
+            expected: bool
+    ) -> None:
+        assert is_isogram(word) == expected
+
+    @pytest.mark.parametrize(
+        "invalid_input",
+        [
+            25,
+            25.7534,
+            ("sgsdg",),
+            None,
+            [25],
+            {"cents": 25},
+        ]
+    )
+    def test_is_isogram_invalid_input(
+            self,
+            invalid_input: Exception
+    ) -> None:
+        with pytest.raises(Exception):
+            is_isogram(invalid_input)
