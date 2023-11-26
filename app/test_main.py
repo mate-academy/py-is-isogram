4e1cd7640a2fc7bbb51d6d917e8ebc57092ff48f
Marina <marina.ua13@gmail.com>
2023-11-26 21:28:24 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_should_return_true_during_checking() -> None:
+    assert is_isogram(word="playgrounds")
+
+
+def test_return_false_if_in_word_exist_same_upper_and_lower_letter() -> None:
+    assert not is_isogram(word="Adam")
+
+
+def test_return_false_if_in_word_exist_same_letters() -> None:
+    assert not is_isogram(word="look")
+
+
+def test_should_return_true_if_empty_string() -> None:
+    assert is_isogram(word="")
