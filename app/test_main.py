003474f5dae020f3bcf09b937ec04499d409b845
MykolaMazhula <mykm3ua@gmail.com>
2023-11-29 09:28:40 +0100

    flake8

--- app/test_main.py
+++ app/test_main.py
@@ -7,10 +7,14 @@ class TestIsIsogram:
         "enter_word,result",
         [
             pytest.param("", True, id="test empty string"),
-            pytest.param("coLizeum", True, id="test isogram with different cases"),
-            pytest.param("coLlizeum", False, id="test different cases of same letter"),
-            pytest.param("morning", False, id="test whether not isogram is not isogram"),
-            pytest.param("dragon", True, id="test whether isogram is isogram")
+            pytest.param("coLizeum", True,
+                         id="test isogram with different cases"),
+            pytest.param("coLlizeum", False,
+                         id="test different cases of same letter"),
+            pytest.param("morning", False,
+                         id="test whether not isogram is not isogram"),
+            pytest.param("dragon", True,
+                         id="test whether isogram is isogram")
         ]
     )
     def test_is_repetting_letters(
