1be676261efae9ad54541648266cefcf10d832e8
Vlad <mr.darmstadtium@gmail.com>
2023-11-30 19:10:17 +0100

    pass all tests 2

--- app/test_main.py
+++ app/test_main.py
@@ -17,6 +17,6 @@ def test_can_find_isogram(
         string_to_check: str,
         result: bool) -> None:
     assert (
-            is_isogram(string_to_check) == result
+        is_isogram(string_to_check) == result
     ), f"Function {is_isogram} should " \
        f"return {result} for {string_to_check}"
