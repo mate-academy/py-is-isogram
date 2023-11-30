8a41af77b1b5dc3e4e10313aee8741132a0bd115
lyaskov <lyaskov1@gmail.com>
2023-11-30 15:01:39 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_should_return_true_if_no_repeating_letters() -> None:
+    assert is_isogram("playgrounds")
+
+
+def test_should_return_true_if_empty() -> None:
+    assert is_isogram("")
+
+
+def test_should_return_equal_result_if_case_insensitive() -> None:
+    assert is_isogram("playgrounds") == is_isogram("PlaYgRouNds")
+
+
+def test_should_return_false_if_double_repeating_letters() -> None:
+    assert is_isogram("book") == False
+
+
+def test_should_return_false_if_repeating_letters_at_the_begin_and_end_word() -> None:
+    assert is_isogram("fimhrdlf") == False
+
+
+def test_should_return_false_if_double_repeating_letters_different_case() -> None:
+    assert is_isogram("Adam") == False
