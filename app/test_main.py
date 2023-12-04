eabe9ad6f38efb10bc48eb2ff93b18b65ff11d76
xboris <boris.tikhonov.21@gmail.com>
2023-12-04 20:07:42 +0200

    'fixed'

--- app/test_main.py
+++ app/test_main.py
@@ -14,18 +14,18 @@ class TestIsIsogram:
             ("", True)
         ]
     )
-    def test_input_word(self, word, result):
+    def test_input_word(self, word: str, result: bool) -> None:
         assert is_isogram(word) == result
 
     @pytest.mark.parametrize(
         "word",
         [
-            pytest.param(1,id="input 'int' type"),
-            pytest.param({"": "123"},id="input 'dict' type",),
+            pytest.param(1, id="input 'int' type"),
+            pytest.param({"": "123"}, id="input 'dict' type", ),
             pytest.param((1, 2, 3, 4), id="input 'tuple' type")
 
         ]
     )
-    def test_unexpected_type(self, word):
+    def test_unexpected_type(self, word: any) -> None:
         with pytest.raises(AttributeError):
             is_isogram(word)
