df1361c0df2b2ffa232bc54918b4ebf520afc376
Anton Konyk <antonkonyk.com>
2023-12-05 20:02:21 +0100

    'Solution_06122023'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,44 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        pytest.param(
+            "",
+            True,
+            id="should return the wright result: True"
+        ),
+        pytest.param(
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+            True,
+            id="should return the wright result: True"
+        ),
+        pytest.param(
+            "abcdefghijklmnopqrstuvwxyz",
+            True,
+            id="should return the wright result: True"
+        ),
+        pytest.param(
+            "abcfea",
+            False,
+            id="should return the wright result: False"
+        ),
+        pytest.param(
+            "seAbcdat",
+            False,
+            id="should return the wright result: False"
+        ),
+        pytest.param(
+            "acntrSSlo",
+            False,
+            id="should return the wright result: False"
+        ),
+    ]
+)
+def test_calculation_correctly(
+        word: int,
+        result: bool
+) -> None:
+    assert is_isogram(word) == result
