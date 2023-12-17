34fd003bf7026fbb93a535466ba9d82cbe135eca
Volodymyr Kovalenko <380974261314kv@gmail.com>
2023-12-17 11:23:38 +0200

    added return type annotation

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,17 @@
 from app.main import is_isogram
 
 
-def test_when_func_takes_empty_string():
+def test_when_func_takes_empty_string() -> None:
     assert is_isogram("") is True
 
 
-def test_when_func_takes_unique_letters_in_string():
+def test_when_func_takes_unique_letters_in_string() -> None:
     assert is_isogram("abcd") is True
 
 
-def test_when_func_takes_the_same_letters():
+def test_when_func_takes_the_same_letters() -> None:
     assert is_isogram("mama") is False
 
 
-def test_when_func_takes_upper_and_lower_the_same_letters():
+def test_when_func_takes_upper_and_lower_the_same_letters() -> None:
     assert is_isogram("Oslo") is False
