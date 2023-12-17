cd1fecf269e8073f677a94cd7fb476668a2df00a
Volodymyr Kovalenko <380974261314kv@gmail.com>
2023-12-17 11:15:22 +0200

    test is_isogram function

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_when_func_takes_empty_string():
+    assert is_isogram("") is True
+
+
+def test_when_func_takes_unique_letters_in_string():
+    assert is_isogram("abcd") is True
+
+
+def test_when_func_takes_the_same_letters():
+    assert is_isogram("mama") is False
+
+
+def test_when_func_takes_upper_and_lower_the_same_letters():
+    assert is_isogram("Oslo") is False
