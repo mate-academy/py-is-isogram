7f01b485ee6cfeebefed86c91cb3dbe0f8729ef9
BirovShandor <birovs777@gmail.com>
2023-12-27 15:00:28 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_checking_all_letters_low() -> None:
+    result_of_func = is_isogram("playgrounds")
+    assert result_of_func is True
+
+
+def test_word_contains_same_letters() -> None:
+    result_of_func = is_isogram("look")
+    assert result_of_func is False
+
+
+def test_word_with_big_letter_and_same_letters() -> None:
+    result_of_func = is_isogram("Adam")
+    assert result_of_func is False
+
+
+def test_empty_string() -> None:
+    result_of_func = is_isogram("")
+    assert result_of_func is True
