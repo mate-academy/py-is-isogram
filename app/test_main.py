4811d537f6e1ac3ac6a01656fbb2958ac6ae8628
Ivan Romanov <i.romanov.dn@gmail.com>
2024-01-08 01:29:08 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_should_string_is_empty() -> None:
+    assert is_isogram("")
+
+
+def test_should_word_is_isogram() -> None:
+    assert is_isogram("Playground")
+
+
+def test_should_word_case_insensivity() -> None:
+    assert not is_isogram("Adam")
+
+
+def test_should_word_is_repeating_letters() -> None:
+    assert not is_isogram("look")
