62fe95c4bdc9c892ee01a79cec736fee9ec52168
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-01-07 23:40:50 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,37 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+def test_should_return_true_on_empty_word() -> None:
+    assert is_isogram(""), (
+        "Function should return true on empty word"
+    )
+
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        ("boOl", False),
+        ("Adam", False),
+        ("Pip", False)
+    ]
+)
+def test_should_be_case_insensitive(word: str, result: bool) -> None:
+    assert is_isogram(word) == result, (
+        "Function should be case insensitive"
+    )
+
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        ("playgrounds", True),
+        ("keyboard", True),
+        ("listen", True)
+    ]
+)
+def test_should_check_isogram_words(word: str, result: bool) -> None:
+    assert is_isogram(word), (
+        "Function should check isogram words"
+    )
