ece3bee462cedb095fc7834dfcceb7d025713bb8
Artur Poltser <polcerartur@gmail.com>
2024-01-11 15:20:06 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,30 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "initial_input,expected_result",
+    [
+        pytest.param(
+            "playgrounds", True,
+            id="should return correct value"
+        ),
+        pytest.param(
+            "Adam", False,
+            id="should be case-insensitive"
+        ),
+        pytest.param(
+            "", True,
+            id="should return True, when initial input is empty string"
+        ),
+        pytest.param(
+            "abca", False,
+            id="should return False with non consecutive letters"
+        )
+    ]
+)
+def test_should_return_correct_value(
+        initial_input: str,
+        expected_result: bool
+) -> None:
+    assert is_isogram(initial_input) == expected_result
