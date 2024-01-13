1d2272afe71e7e31afe71c023b08b7fa0f4439b8
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2024-01-13 08:56:20 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "sentence,true_or_false",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True)
+    ]
+)
+def test_for_the_correctness_of_the_definition_of_the_isogram(
+        sentence: str,
+        true_or_false: bool
+) -> None:
+    assert is_isogram(sentence) == true_or_false
