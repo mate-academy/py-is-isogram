8e36af87615f1773cbd1d5534df6adb1f8853e27
AlexHCom <alexhcomgrit@gmail.com>
2024-01-17 00:58:38 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,32 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+class TestResultIsIsogram:
+    @pytest.mark.parametrize(
+        "given_word,expected_output",
+        [
+            pytest.param(
+                "pool",
+                False,
+                id="check lowercase not isogram"
+            ),
+            pytest.param(
+                "",
+                True,
+                id="check empty string is isogram"
+            ),
+            pytest.param(
+                "Carmen",
+                True,
+                id="check isogram on the right answer"
+            ),
+            pytest.param(
+                "Squids",
+                False,
+                id="check case-insensitive not isogram"
+            ),
+        ],
+    )
+    def test_is_isogram(self, given_word, expected_output):
+        assert is_isogram(given_word) == expected_output
