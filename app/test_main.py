de7579164250c1c6855f396046073404f1fb8679
Veronika <veronikaboeva935@gmail.com>
2024-01-16 18:43:50 +0100

    Add test error 16.01

--- app/test_main.py
+++ app/test_main.py
@@ -3,7 +3,7 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "word,boolean",
+    "word,words_boolean",
     [
         ("playgrounds", True),
         ("look", False),
@@ -19,6 +19,15 @@ import pytest
 )
 def test_if_the_word_is_isogram(
         word: str,
-        boolean: bool
+        words_boolean: bool
 ) -> None:
-    assert is_isogram(word) == boolean
+    assert is_isogram(word) == words_boolean
+
+
+@pytest.mark.parametrize(
+    "word",
+    [1]
+)
+def test_error(word: int) -> None:
+    with pytest.raises(AttributeError):
+        is_isogram(word)
