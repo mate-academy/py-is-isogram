364a69fa650629d9efcfaa72fdde3e40a6b7f25b
Ant <u123@ua.fm>
2024-01-21 19:31:47 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,23 @@
+from typing import Any
 from app.main import is_isogram
 
 # write your code here
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        ("AA", False),
+        ("Aa", False),
+        ("Test", False),
+        ("playgrounds", True),
+        ("a123B", True),
+        ("", True),
+    ]
+)
+def test_can_is_isogram(word: str, result: bool) -> None:
+    assert (
+        is_isogram(word) == result
+    ), (f"Is isogram {word} should be equal to {result}")
