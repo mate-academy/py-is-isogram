7a8ce7aacc5d85be471324c904c9df4d4877a294
barabarinov <barabarinov@gmail.com>
2024-01-24 23:50:02 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,22 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,boolean_result",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+    ],
+    ids=[
+        "'playgrounds' is isogram and should return True",
+        "'look' is not isogram and should return False",
+        "'Adam' is not isogram and should return False",
+        "Empty string should return True",
+    ]
+)
+def test_is_isogram(word: str, boolean_result: bool) -> None:
+    assert is_isogram(word) == boolean_result
