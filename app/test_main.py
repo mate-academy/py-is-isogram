85bac75b5a56ac5a681882c0fc2d20d37f92ed1f
Maksym Operchuk <operlast529@gmail.com>
2024-01-28 16:21:27 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_non_isogram_word() -> None:
+    assert is_isogram("look") is False
+
+
+def test_consecutive_word() -> None:
+    assert is_isogram("abcdefa") is False
+
+
+def test_non_consecutive_word() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_empty_string() -> None:
+    assert is_isogram("") is True
+
+
+def test_mixed_case_non_isogram() -> None:
+    assert is_isogram("Mm") is False
