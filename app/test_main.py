9987df76604913102d36f91c1c4654618ea67fa8
unknown <artem.sapiens@gmail.com>
2024-01-29 21:21:48 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word",
+    [
+        ("playgrounds"),
+        ("look"),
+        ("Adam"),
+        ("a"),
+        ("")
+    ]
+)
+def test_isogram_with_isogram_word(word: str) -> None:
+    assert is_isogram(word)
