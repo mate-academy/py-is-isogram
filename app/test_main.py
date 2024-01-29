ca4eea9c907af153a28f645f8ead096336a4634a
Ihor Bondarenko <igorb6766@gmail.com>
2024-01-29 17:46:05 +0200

    Revert "solution"
    This reverts commit 40e962a50a1be7108a9b61ce27d92642e528eb5f.

--- app/test_main.py
+++ app/test_main.py
@@ -1,26 +1,3 @@
-import pytest
-
 from app.main import is_isogram
 
-
-@pytest.mark.parametrize(
-    "initial_word,expected_bool",
-    [
-        ("playgrounds", True),
-        ("look", False),
-        ("Adam", False),
-        ("", True)
-    ],
-    ids=[
-        "word is isogram",
-        "word is not isogram",
-        "contain same symbols in dif register",
-        "word is empty"
-    ]
-)
-def test_should_check_if_word_is_isogram(
-        initial_word: str,
-        expected_bool: bool
-) -> None:
-    assert (is_isogram(initial_word) == expected_bool), \
-        "Should correctly check if word is isogram"
+# write your code here
