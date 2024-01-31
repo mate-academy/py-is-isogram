316eccead1c19db0f966a8f500293afdf4313803
angelzagniy <angelzagniy@gmail.com>
2024-01-31 19:35:05 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,15 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+@pytest.mark.parametrize(
+    "word,expected_result",
+    [
+        pytest.param("new", True, id="Check that word is isogram"),
+        pytest.param("look", False, id="Check word is not isogram"),
+        pytest.param("Adam", False, id="Check function is case-insensitive"),
+        pytest.param("", True, id="Check that empty string is isogram")
+    ]
+)
+def test_find_isogram_correctly(word: str, expected_result: bool) -> None:
+    assert is_isogram(word) == expected_result
