f22730581258c7ebbd0ce16ed61a5736d581cb20
Serhii Hrynyshyn <hrynyshyn.serhii@gmail.com>
2024-02-01 00:04:18 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_with_empty_string() -> None:
+    assert (is_isogram("") is True), "Test should return True"
+
+
+def test_uppercase_and_lowercase_do_not_matters() -> None:
+    assert (is_isogram("AbCaBc") is False), "Test should return False"
+
+
+def test_same_letters() -> None:
+    assert (is_isogram("Hello") is False), "Test should return False"
+
+
+def test_is_unique_all_letters() -> None:
+    assert (is_isogram("Ukraine") is True), "Test should return True"
