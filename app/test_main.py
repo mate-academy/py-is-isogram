f2d73ae661d802cd38bc50a3727a39a436c70c43
voronvasia <voronvasia@gmail.com>
2024-02-05 22:51:18 +0200

    task completed

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,44 @@
+from typing import Any, Type
+
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogramWords:
+    @pytest.mark.parametrize(
+        "word,result",
+        [
+            ("playgrounds", True),
+            ("look", False),
+            ("Adam", False),
+            ("", True),
+            (" playgrounds ", False),
+            ("1234567890", True)
+        ]
+    )
+    def test_is_isogram(
+            self,
+            word: str,
+            result: bool
+    ):
+        assert (is_isogram(word) == result)
+
+
+class TestIsIsogramTypes:
+    @pytest.mark.parametrize(
+        "word,error",
+        [
+            (1, AttributeError),
+            ([], AttributeError),
+            ({}, AttributeError),
+            (None, AttributeError)
+        ]
+    )
+    def test_is_isogram_types(
+            self,
+            word: Any,
+            error: Type[Exception]
+    ):
+        with pytest.raises(error):
+            is_isogram(word)
