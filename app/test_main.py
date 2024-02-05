9b39d532e879ae72a99603d516db5dfb4e97646f
sergey <38cerg12021979@jmal.com>
2024-02-05 07:35:46 +0200

    05_02_24

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_return_the_empty_string_true():
+    assert is_isogram("")
+
+
+def test_M_and_m_are_considered_the_same_letter():
+    assert not is_isogram("Adam")
+
+
+def test_ead_to_the_repetition_of_letters():
+    assert not is_isogram("look")
+
+def test_should_is_an_isogram():
+    assert is_isogram("playgrounds")
