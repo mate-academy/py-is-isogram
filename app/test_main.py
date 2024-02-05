be6765bcd15fa4321c52e76dd8ea50f56b5ed6e5
sergey <38cerg12021979@jmal.com>
2024-02-05 07:50:19 +0200

    05_02_24_01

--- app/test_main.py
+++ app/test_main.py
@@ -1,16 +1,17 @@
 from app.main import is_isogram
 
 
-def test_return_the_empty_string_true():
+def test_return_the_empty_string_true() -> None:
     assert is_isogram("")
 
 
-def test_M_and_m_are_considered_the_same_letter():
+def test_letters_must_be_in_the_same_register() -> None:
     assert not is_isogram("Adam")
 
 
-def test_ead_to_the_repetition_of_letters():
+def test_ead_to_the_repetition_of_letters() -> None:
     assert not is_isogram("look")
 
-def test_should_is_an_isogram():
+
+def test_should_is_an_isogram() -> None:
     assert is_isogram("playgrounds")
