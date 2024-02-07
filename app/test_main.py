772d43ff867d51080a1912543ee70c49000ae461
cthusq <cthusq.qusevi@gmail.com>
2024-02-07 20:49:20 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "word, expected",
+        [
+            ("look", False),
+            ("Adam", False),
+            ("playgrounds", True),
+            ("", True),
+        ],
+        ids=[
+            "word should consist only unique letters",
+            "word should consist only unique letters",
+            "should return True all letters are unique",
+            "should return True if string empty",
+        ],
+    )
+    def test_check_if_word_is_isogram(
+            self,
+            word: str,
+            expected: bool,
+    ) -> None:
+        assert is_isogram(word) == expected
