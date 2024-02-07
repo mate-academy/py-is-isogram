e12391fb1fc45fe48f27d0322acc2fa7763404dc
mkuchuman <mkuchuman1@gmail.com>
2024-02-07 21:41:55 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,38 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+
+class TestIsIsonfram:
+    @pytest.mark.parametrize(
+        "word, boolian",
+        [
+            ("", True),
+            ("look", False),
+            ("Adam", False),
+            ("playgrounds", True),
+            (" ", True),
+        ],
+        ids=[
+            "empy list is True",
+            "empy list is True",
+            "empy list is True",
+            "empy list is True",
+            "empy list is True",
+        ]
+    )
+    def test_get_coin_comb(self, word: str, boolian: bool) -> None:
+        assert is_isogram(word) is boolian
+
+    @pytest.mark.parametrize(
+        "word, error",
+        [
+            (1, AttributeError),
+        ],
+        ids=[
+            "word must be str"
+        ],
+    )
+    def test_errors_for_int_values(self, word: str, error: AttributeError) -> None:
+        with pytest.raises(error):
+            is_isogram(word)
