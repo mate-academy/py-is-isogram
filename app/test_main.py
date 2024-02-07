3c9d1704c0b14181a6b804f273f5c67e4957c01c
Mykyta Soloviov <mykyta.soloviov@gmail.com>
2024-02-07 13:06:29 +0000

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_isogram() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_case_insensitive() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_none() -> None:
+    assert is_isogram("") is True
+
+
+def test_non_consecutive() -> None:
+    assert is_isogram("look") is False
