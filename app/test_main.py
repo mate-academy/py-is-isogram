cd11ebe7d45902b8fa545486fff64351961d53eb
volry <v.v.ryzhuk@gmail.com>
2024-02-09 08:53:48 +0200

    final solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,24 +2,24 @@ from app.main import is_isogram
 
 
 def test_empty_string_is_isogram() -> None:
-    assert is_isogram("") == True
+    assert is_isogram("") is True
 
 
 def test_isogram_with_no_repeating_letters() -> None:
-    assert is_isogram("playgrounds") == True, "Expected isogram"
+    assert is_isogram("playgrounds") is True, "Expected isogram"
 
 
 def test_non_isogram_due_to_repeating_letters() -> None:
-    assert is_isogram("look") == False
+    assert is_isogram("look") is False
 
 
 def test_non_isogram_with_case_insensitivity() -> None:
-    assert is_isogram("Adam") == False
+    assert is_isogram("Adam") is False
 
 
 def test_isogram_with_mixed_case_letters() -> None:
-    assert is_isogram("Subdermatoglyphic") == True
+    assert is_isogram("Subdermatoglyphic") is True
 
 
 def test_non_isogram_with_mixed_case_repeating_letters() -> None:
-    assert is_isogram("Alphabet") == False
+    assert is_isogram("Alphabet") is False
