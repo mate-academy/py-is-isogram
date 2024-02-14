d7955478490f96ace130512a44ef37d57729d368
Molchanov Ihor <=>
2024-02-14 14:14:46 +0200

    solved task

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_word():
+    assert (is_isogram('') == True)
+
+
+def test_word_with_chars_not_repeat():
+    assert (is_isogram('playgrounds') is True)
+
+
+def test_word_with_chars_repeated():
+    assert (is_isogram('look') is False)
+
+
+def test_word_with_different_char_register():
+    assert (is_isogram('Adam') is False)
