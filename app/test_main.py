bd91d92d02cdb2666d7ac2f970908163e66ae1ec
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-02-14 21:01:11 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,24 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "word, result",
+        [
+            pytest.param("", True,
+                         id="should_return_True_for_empty_string"),
+            pytest.param("playgrounds", True,
+                         id="should_return_True_if_no_repeats"),
+            pytest.param("look", False,
+                         id="should_return_False_if_two_equal_letters"),
+            pytest.param("Adam", False,
+                         id="should_return_False_if_"
+                            + "two_equal_letters_but_different_case"),
+            pytest.param("loook", False,
+                         id="should_return_False_if_three_equal_letters"),
+        ]
+    )
+    def test_returns_correct_result(self, word: str, result: str) -> None:
+        assert is_isogram(word) == result
