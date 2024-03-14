f52ef43da127bf658f18b48d38e744449912a016
Dmytro Kaliuta <d.kaliuta.work@gmail.com>
2024-03-14 11:42:49 +0200

    Implement py-is-isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,23 @@
+import pytest
+from typing import Type
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, expected_result",
+    [
+        ("", True),
+        ("look", False),
+        ("Adam", False),
+        ("playgrounds", True)
+    ],
+    ids=[
+        "empty string should return True",
+        "if letter is repeated should return False",
+        "function should be case-insensitive ((M and m are considered the same letter)",
+        "If the letters are not repeated should return True"
+    ]
+)
+def test_is_isogram(word: str, expected_result: bool) -> None:
+    assert is_isogram(word=word) == expected_result
