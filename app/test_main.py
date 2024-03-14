21ca3f67ac0b1f4fb8d2a9907f85e25d10fa7466
Olga <olgahrytsiuk777@gmail.com>
2024-03-14 15:36:21 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_name_is_false():
+    assert is_isogram("Adam") is False
+
+
+def test_symbol_how_non_consecutive():
+    assert is_isogram("playgrounds") is True
+
+
+def test_symbol_how_consecutive():
+    assert is_isogram("look") is False
+
+
+def test_return_true_if_zero_symbal():
+    assert is_isogram("") is True
