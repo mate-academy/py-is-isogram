b7ec371bdcfa409b2df70a70677233be20b978a0
Marian Kostiuk <131563162+kostiukmkalne@users.noreply.github.com>
2024-03-16 15:21:55 -0500

    new solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,19 @@
 import pytest
+
 from app.main import is_isogram
 
 
-@pytest.mark.parametrize("word, expected", [
-    ("playgrounds", True),
-    ("look", False),
-    ("Adam", False),
-    ("NIOcean", False),  # 'N' і 'n' повинні вважатися однаковими
-    ("I Love Mate academy", False),
-    ("wrong", True),
-    ("brother", True),
-    ("Ukraine", True),
-    ("", True)
-])
-def test_is_isogram(word: str, expected: bool) -> None:
+@pytest.mark.parametrize(
+    "word,expected", [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("NIOcean", False),
+        ("wrong", True),
+        ("brother", True),
+        ("Ukraine", True),
+        ("", True)
+    ]
+)
+def test_is_program(word: str, expected: bool) -> None:
     assert is_isogram(word) == expected
