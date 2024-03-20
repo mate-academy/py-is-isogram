f21ad87d55ebc7635e973037af4e56e0bbaf5c21
Artur Komyshniuk <akomisnuk@gmail.com>
2024-03-20 18:56:40 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, extended_result",
+    [
+        ('playgrounds', True),
+        ('look', False),
+        ('Adam', False),
+        ('', True)
+    ]
+)
+def test_word_is_isogram(
+        word: str,
+        extended_result: bool
+) -> None:
+    assert is_isogram(word) == extended_result
