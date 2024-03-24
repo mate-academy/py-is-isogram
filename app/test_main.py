93d6075e538579c7b9f912ce0ddaabd9ed1ae079
SHCHERBANV <shcherbanvasyl@gmail.com>
2024-03-24 12:04:15 -0600

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
+import pytest
 from app.main import is_isogram
+from typing import List, Tuple
 
-# write your code here
+@pytest.mark.parametrize("word, expected", [
+    ("playgrounds", True),
+    ("look", False),
+    ("Adam", False),
+    ("", True),
+    ("subdermatoglyphic", True),
+    ("Alphabet", False),
+])
+def test_is_isogram(word: str, expected: bool) -> None:
+    assert is_isogram(word) == expected
+
+def test_is_isogram_case_insensitive() -> None:
+    assert is_isogram("Sponge") == True
+    assert is_isogram("sPoNgE") == True
+
+def test_is_isogram_with_empty_string() -> None:
+    assert is_isogram("") == True
