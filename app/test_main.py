375008362e27e42539f69dcffe7fedd87ade9e5c
Vyacheslav Malchyk <k2.mvo98@gmail.com>
2024-03-26 19:59:55 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,15 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+@pytest.mark.parametrize(
+    "test_input,expected_bool", [
+        ("aaa", False),
+        ("", True),
+        ("Atest", False),
+        ("ITS testing string", False),
+        ("aTest", False),
+        ("looking", False)
+    ]
+)
+def test_is_program(test_input: str, expected_bool: bool) -> None:
+    assert is_isogram(test_input) == expected_bool
