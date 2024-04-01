cd57409641a43b8cf17000db2fb9cea74d705ae1
Marharyta Marchenko <ritanika12@gmail.com>
2024-04-01 19:30:37 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,8 +1,8 @@
 from app.main import is_isogram
 
 
-def test_is_isogram():
-    assert is_isogram('playgrounds') is True
-    assert is_isogram('look') is False
-    assert is_isogram('Adam') is False
-    assert is_isogram('') is True
+def test_is_isogram() -> None:
+    assert is_isogram("playgrounds") is True
+    assert is_isogram("look") is False
+    assert is_isogram("Adam") is False
+    assert is_isogram("") is True
