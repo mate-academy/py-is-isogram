ae9e36f952cd3e1a25d77eae72949198ae5a30f2
NioviIki <charmniovi@tutanota.com>
2024-04-02 18:25:46 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,28 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_instance_is_isogram() -> None:
+    assert isinstance(is_isogram("asd"), bool)
+
+
+@pytest.mark.parametrize(
+    "word,expected",
+    [
+        ("isogram", True),
+        ("playgrounds", True),
+        ("Adam", False),
+        (" ", True),
+        ("121", False),
+        ("123", True),
+        ("look", False)
+    ]
+)
+def test_is_isogram(word: str, expected: bool) -> None:
+    assert is_isogram(word) == expected
+
+
+def test_wrong_input_case() -> None:
+    with pytest.raises(AttributeError):
+        is_isogram(14)
