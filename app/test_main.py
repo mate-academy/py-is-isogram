1c6f7e9a6313bd091845969fae44111e33c21a90
Andrii Chubenko <stalkerock@gmail.com>
2024-04-04 12:59:28 +0300

    My tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
+import pytest
+
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize("test_input, expected", [
+    ("playgrounds", True),
+    ("look", False),
+    ("Adam", False),
+    ("Moment", False),
+    ("", True)
+])
+def test_is_isogram(test_input: str, expected: bool) -> None:
+    if isinstance(test_input, str):
+        assert is_isogram(test_input) == expected
