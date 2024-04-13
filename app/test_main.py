4cc30adceb1f3e3a548c618dad1bb0dbfd589ab6
Yevheniia Sosnovska <sosnovskaevgeniya.2504@gmail.com>
2024-04-13 19:42:37 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "word,result",
+        [
+            "playground, True",
+            "look, False",
+            "Adam, False",
+            "'', True",
+        ],
+        ids=[
+            "test should return True when word is isogram",
+            "test should return False when word is not isogram",
+            "String with different cases of the same letter is not an isogram",
+            "empty string is isogram"
+        ]
+    )
+    def test_is_isogram(self,
+                        word: str,
+                        result: bool
+                        ) -> None:
+        assert is_isogram(word) == result
