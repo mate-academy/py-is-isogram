e3e1e6bb506acf0ca68865bc546515c429231162
Yevheniia Sosnovska <sosnovskaevgeniya.2504@gmail.com>
2024-04-13 19:48:59 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -10,12 +10,14 @@ class TestIsIsogram:
             "look, False",
             "Adam, False",
             "'', True",
+            "Playground, True"
         ],
         ids=[
             "test should return True when word is isogram",
             "test should return False when word is not isogram",
             "String with different cases of the same letter is not an isogram",
             "empty string is isogram"
+            "String with different cases of the different letters is an isogram"
         ]
     )
     def test_is_isogram(self,
