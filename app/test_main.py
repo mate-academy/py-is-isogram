105eea218ef24a2fca4ec4237363f6c7251e7370
Klishch Nazar <klishch65@gmail.com>
2024-04-15 12:38:41 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,24 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "test_word, result",
+    [
+        ("", True),
+        ("AalL", False),
+        ("abcda", False),
+        ("Abcdzx", True),
+        ("efg", True),
+    ],
+    ids=[
+        "test empty string",
+        "test two repeating letters with upper-case",
+        "test one repeating letter with lower-case and non-consecutive",
+        "test no repeating letters with upper-case",
+        "test no repeating letters with lower-case"
+    ]
+)
+def test_letters_for_isogram(test_word: str, result: bool):
+    assert is_isogram(test_word) == result
