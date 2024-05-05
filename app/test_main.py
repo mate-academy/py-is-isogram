4ab7d2f260d27d825e2f7868c91f9490077208a8
Vlad <begavlad068@gmail.com>
2024-05-05 10:29:26 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,22 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram_empty_string():
+    assert is_isogram('') == True
+
+
+def test_is_isogram_case_insensitive():
+    assert is_isogram('Adam') == False
+    assert is_isogram('adam') == False
+    assert is_isogram('AdamM') == False
+
+
+def test_is_isogram_isograms():
+    assert is_isogram('playgrounds') == True
+    assert is_isogram('background') == True
+    assert is_isogram('subdermatoglyphic') == True
+
+def test_is_isogram_non_isograms():
+    assert is_isogram('look') == False
+    assert is_isogram('mirror') == False
+    assert is_isogram('apple') == False
