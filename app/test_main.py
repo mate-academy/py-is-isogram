93299ad7b142cf9757a601a9dbe9a28c21e67e10
Vlad <begavlad068@gmail.com>
2024-05-05 10:32:41 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,22 +1,23 @@
 from app.main import is_isogram
 
 
-def test_is_isogram_empty_string():
-    assert is_isogram('') == True
+def test_is_isogram_empty_string() -> None:
+    assert is_isogram("") is True
 
 
-def test_is_isogram_case_insensitive():
-    assert is_isogram('Adam') == False
-    assert is_isogram('adam') == False
-    assert is_isogram('AdamM') == False
+def test_is_isogram_case_insensitive() -> None:
+    assert is_isogram("Adam") is False
+    assert is_isogram("adam") is False
+    assert is_isogram("AdamM") is False
 
 
-def test_is_isogram_isograms():
-    assert is_isogram('playgrounds') == True
-    assert is_isogram('background') == True
-    assert is_isogram('subdermatoglyphic') == True
+def test_is_isogram_isograms() -> None:
+    assert is_isogram("playgrounds") is True
+    assert is_isogram("background") is True
+    assert is_isogram("subdermatoglyphic") is True
 
-def test_is_isogram_non_isograms():
-    assert is_isogram('look') == False
-    assert is_isogram('mirror') == False
-    assert is_isogram('apple') == False
+
+def test_is_isogram_non_isograms() -> None:
+    assert is_isogram("look") is False
+    assert is_isogram("mirror") is False
+    assert is_isogram("apple") is False
