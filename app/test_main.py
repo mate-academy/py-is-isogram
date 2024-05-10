b63915270e38dd9d7f0596839afd5e9be2f07325
Nazar <legoman1858@gmail.com>
2024-05-10 11:14:55 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,17 @@
 from app.main import is_isogram
 
 
-def test_is_isogram_true():
+def test_is_isogram_true() -> None:
     assert is_isogram("playgrounds") is True
 
 
-def test_is_isogram_false():
+def test_is_isogram_false() -> None:
     assert is_isogram("look") is False
 
 
-def test_is_isogram_false_caseintensive():
+def test_is_isogram_false_caseintensive() -> None:
     assert is_isogram("Adam") is False
 
 
-def test_is_isogram_empty():
+def test_is_isogram_empty() -> None:
     assert is_isogram("") is True
