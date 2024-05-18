7843f2f2ec9b076a8073ad1f16a3bb01d91b22c3
Viktor Shcherbak <dev.psychologist@gmail.com>
2024-05-18 10:47:31 +0300

    Isogram test (After flake8)

--- app/test_main.py
+++ app/test_main.py
@@ -1,46 +1,45 @@
 from app.main import is_isogram
-import pytest
 
 
-def test_empty_str():
-    assert is_isogram("") == True
+def test_empty_str() -> None:
+    assert is_isogram("")
 
 
-def test_isogram_single_character():
-    assert is_isogram('a') == True
+def test_isogram_single_character() -> None:
+    assert is_isogram("a")
 
 
-def test_isogram_playgrounds():
-    assert is_isogram('playgrounds') == True
+def test_isogram_playgrounds() -> None:
+    assert is_isogram("playgrounds")
 
 
-def test_isogram_look():
-    assert is_isogram('look') == False
+def test_isogram_look() -> None:
+    assert not is_isogram("look")
 
 
-def test_isogram_Adam():
-    assert is_isogram('Adam') == False
+def test_isogram_adam() -> None:
+    assert not is_isogram("Adam")
 
 
-def test_isogram_uppercase_mixed():
-    assert is_isogram('Alphabet') == False
+def test_isogram_uppercase_mixed() -> None:
+    assert not is_isogram("Alphabet")
 
 
-def test_isogram_case_insensitive():
-    assert is_isogram('Dermatoglyphics') == True
+def test_isogram_case_insensitive() -> None:
+    assert is_isogram("Dermatoglyphics")
 
 
-def test_isogram_long_non_isogram():
-    assert is_isogram('subdermatoglyphic') == True
+def test_isogram_long_non_isogram() -> None:
+    assert is_isogram("subdermatoglyphic")
 
 
-def test_isogram_with_repeated_characters():
-    assert is_isogram('eleven') == False
+def test_isogram_with_repeated_characters() -> None:
+    assert not is_isogram("eleven")
 
 
-def test_isogram_with_unique_characters():
-    assert is_isogram('subway') == True
+def test_isogram_with_unique_characters() -> None:
+    assert is_isogram("subway")
 
 
-def test_isogram_with_all_alphabets():
-    assert is_isogram('abcdefghijklmnopqrstuvwxyz') == True
+def test_isogram_with_all_alphabets() -> None:
+    assert is_isogram("abcdefghijklmnopqrstuvwxyz")
