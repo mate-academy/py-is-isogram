bf0768352849feaaf8252bdb6439fd8470b5fb1c
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-06-04 22:46:08 +0200

    V2

--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,11 @@
 from app.main import is_isogram
 
 
-def test_to_check_is_word_is_isogram():
-    assert is_isogram('playgrounds') is True
-    assert is_isogram('look') is False
-    assert is_isogram('Adam') is False
-    assert is_isogram('') is True
-    assert is_isogram('a') is True
-    assert is_isogram('ab') is True
-    assert is_isogram('aa') is False
+def test_to_check_is_word_is_isogram() -> None:
+    assert is_isogram("playgrounds") is True
+    assert is_isogram("look") is False
+    assert is_isogram("Adam") is False
+    assert is_isogram("") is True
+    assert is_isogram("a") is True
+    assert is_isogram("ab") is True
+    assert is_isogram("aa") is False
