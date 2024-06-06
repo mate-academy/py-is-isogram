bfcf6105573446b12c0b2cec147a4cd7f53733e9
Oleh Deshko <kochaminny@gmail.com>
2024-06-06 17:25:01 +0300

    solution1

--- app/test_main.py
+++ app/test_main.py
@@ -4,13 +4,13 @@ from app.main import is_isogram
 @pytest.mark.parametrize(
     "input_word,res",
     [
-        ["playgrounds", True],
-        ["look", False],
-        ["", True],
-        ["grapesound", True],
-        ["Ff", False],
-        ["nice fabric", True],
-        ["nice fab ric", False],
+        ("playgrounds", True),
+        ("look", False),
+        ("", True),
+        ("grapesound", True),
+        ("Ff", False),
+        ("nice fabric", True),
+        ("nice fab ric", False),
     ]
 )
 def test_is_isogram(input_word: str, res: bool) -> None:
