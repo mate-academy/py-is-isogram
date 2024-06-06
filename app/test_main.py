a02d29809af63ae1cef2c866a108d63f6f02591f
jpdreamthug <lentorilp@gmail.com>
2024-06-06 15:44:07 +0300

    add solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,true_or_false",
+    [
+        pytest.param("", True),
+        pytest.param("look", False),
+        pytest.param("ADAM", False),
+        pytest.param("MiX", True),
+        pytest.param("Aa", False),
+        pytest.param("qwertyuiopasdfghjklzxcvbnm", True)
+    ]
+)
+def test_only_letters(word: str, true_or_false: bool) -> None:
+    assert is_isogram(word) is true_or_false
