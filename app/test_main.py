0e89a10db6d50e25a4ee5c7d862b24179a40ff4b
Stanislav Sudakov <stasiksudakov@gmail.com>
2024-06-06 17:56:39 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_word() -> None:
+    goals = is_isogram("")
+    assert goals is True
+
+
+def test_isogram_word() -> None:
+    goals = is_isogram("look")
+    assert goals is False
+
+
+def test_isogram_uppercase_word() -> None:
+    goals = is_isogram("Adam")
+    assert goals is False
+
+
+def test_not_isogram_word() -> None:
+    goals = is_isogram("playground")
+    assert goals is True
