66b5e4899382cde5f431eb4af9a81a7518dfd974
Igor Kolisnyk <moonbosszzz@gmail.com>
2024-06-13 23:32:19 +0200

    The solution of the task

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,22 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True)
+    ],
+    ids=[
+        "Should return True if word is isogram",
+        "Should return False if word isn't isogram",
+        "Should return False if word capitalise and isn't isogram",
+        "Should return True if word is empty string"
+    ]
+)
+def test_should_return_correct_value(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
