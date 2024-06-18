5e59c9c40dbf64b048877445f99cd0e2d735a755
Ira <iramladanovych@gmail.com>
2024-06-18 21:17:12 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,20 +1,21 @@
-import pytest
 from app.main import is_isogram
 
 
-def test_empty_string():
+def test_empty_string() -> None:
     word = is_isogram("")
     assert word is True
 
 
-def test_word_with_doubled_letters():
+def test_word_with_doubled_letters() -> None:
     word = is_isogram("look")
     assert word is False
 
-def test_word_without_doubled_letters():
+
+def test_word_without_doubled_letters() -> None:
     word = is_isogram("playgrounds")
     assert word is True
 
-def test_word_another_type():
+
+def test_word_another_type() -> None:
     word = is_isogram("5")
     assert word is False
