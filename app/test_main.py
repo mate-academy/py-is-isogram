eb2f596f45b45b8aa7c6fd0c144c31dc82f071b2
Ira <iramladanovych@gmail.com>
2024-06-19 07:29:47 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -7,7 +7,7 @@ def test_empty_string() -> None:
 
 
 def test_word_with_doubled_letters() -> None:
-    word = is_isogram("look")
+    word = is_isogram("Look!")
     assert word is False
 
 
@@ -16,6 +16,16 @@ def test_word_without_doubled_letters() -> None:
     assert word is True
 
 
+def test_word_with_the_different_letters() -> None:
+    word = is_isogram("aBkLm!")
+    assert word is True
+
+
 def test_word_another_type() -> None:
     word = is_isogram("5")
+    assert word is True
+
+
+def test_word_with_the_same_letters() -> None:
+    word = is_isogram("Adam!")
     assert word is False
