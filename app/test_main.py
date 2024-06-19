c0bfc4ca9bf3bfe60afd20d5616f4c68b401d127
Tymur <shmorguntimur@gmail.com>
2024-06-19 17:18:06 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,19 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        ("background", True),
+        ("Downstream", True),
+        ("lumberjacks", True),
+        ("isogram", True),
+        ("Google", False),
+        ("Community", False),
+        ("System", False),
+        ("Colony", False),
+    ]
+)
+def test_is_isogram(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
