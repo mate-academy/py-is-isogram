9fba6356350b89496c1c3fb5bbbc71ab929c5f6a
Alex93Troy <troianovskiy93alex@gmail.com>
2024-06-24 19:51:30 +0200

    done

--- app/test_main.py
+++ app/test_main.py
@@ -3,12 +3,12 @@ from app.main import is_isogram
 
 
 def test_is_isogram() -> None:
-    assert is_isogram("") == True
+    assert is_isogram("") is True
     assert is_isogram("abiogeneses") is False
     assert is_isogram("cholangiogram") is False
     assert is_isogram("background") is True
     assert is_isogram("alphabet") is False
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pytest.main()
