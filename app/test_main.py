4173dd8b76cd63c52e29a44dfaa00c0e14021f29
Alex93Troy <troianovskiy93alex@gmail.com>
2024-06-24 20:04:31 +0200

    done

--- app/test_main.py
+++ app/test_main.py
@@ -9,6 +9,13 @@ def test_is_isogram() -> None:
     assert is_isogram("background") is True
     assert is_isogram("alphabet") is False
 
+def test_edge_cases():
+    assert is_isogram("Aa") is False
+    assert is_isogram("AaAaAa") is False
+    assert is_isogram("abcdefgh") is True
+    assert is_isogram("abcdefgha") is False
+    assert is_isogram("a") is True
+
 
 if __name__ == "__main__":
     pytest.main()
