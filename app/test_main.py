f90801a40c3546784c6cf2345f54a3142cf275fe
Alex93Troy <troianovskiy93alex@gmail.com>
2024-06-24 19:44:59 +0200

    fix

--- app/test_main.py
+++ app/test_main.py
@@ -1,15 +1,14 @@
 import pytest
 from app.main import is_isogram
 
+
 def test_is_isogram() -> None:
-    assert is_isogram('') == True
-    assert is_isogram('abiogeneses') == False
-    assert is_isogram('cholangiogram') == False
-    assert is_isogram('stu') == False
-    assert is_isogram('background') == True
+    assert is_isogram("") == True
+    assert is_isogram("abiogeneses") == False
+    assert is_isogram("cholangiogram") == False
+    assert is_isogram("background") == True
+    assert is_isogram('alphabet') == False
 
 
 if __name__ == '__main__':
     pytest.main()
-
-
