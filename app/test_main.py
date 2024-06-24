ba34a6b71be7eb2545b47a8a5081fceb22552c68
Alex93Troy <troianovskiy93alex@gmail.com>
2024-06-24 19:50:11 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,10 +4,10 @@ from app.main import is_isogram
 
 def test_is_isogram() -> None:
     assert is_isogram("") == True
-    assert is_isogram("abiogeneses") == False
-    assert is_isogram("cholangiogram") == False
-    assert is_isogram("background") == True
-    assert is_isogram('alphabet') == False
+    assert is_isogram("abiogeneses") is False
+    assert is_isogram("cholangiogram") is False
+    assert is_isogram("background") is True
+    assert is_isogram("alphabet") is False
 
 
 if __name__ == '__main__':
