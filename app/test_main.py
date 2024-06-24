792d5aa3a82a025365543f52815f517646824534
Serveladik <vladyslav.sabadakh@litslink.com>
2024-06-24 19:05:07 +0300

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,17 @@
 from app.main import is_isogram
 
 
-def test_is_isogram_first() -> None:
-    assert is_isogram("playgrounds") == True, "That string is isogram"
+def test_is_histogram_first() -> None:
+    assert is_isogram("playgrounds"), "That string is histogram"
 
 
-def test_is_isogram_second() -> None:
-    assert is_isogram("look") == False, "That string is not isogram"
+def test_is_histogram_second() -> None:
+    assert not is_isogram("look"), "That string is not histogram"
 
 
-def test_is_isogram_third() -> None:
-    assert is_isogram("Adam") == False, "That string is not isogram"
+def test_is_histogram_third() -> None:
+    assert not is_isogram("Adam"), "That string is not histogram"
 
 
-def test_is_isogram_fourth() -> None:
-    assert is_isogram("") == True, "That string is isogram"
+def test_is_histogram_fourth() -> None:
+    assert is_isogram(""), "That string is histogram"
