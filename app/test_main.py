934c775ea6293c58691c04b499834b13d8603df7
limbxbx <ilya.msd2003@gmail.com>
2024-06-26 17:34:41 +0300

    Add parametrized tests to "is_isogram" function

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        ("", True),
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False)
+    ]
+)
+def test(word: str, result: bool):
+    assert is_isogram(word) == result
