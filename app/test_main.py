5852d11a3b5c4aa603b8dbc7dfdea15418a46a07
Ppllupold <kotnazar2005@gmail.com>
2024-06-28 23:06:25 +0300

    dwfgf

--- app/test_main.py
+++ app/test_main.py
@@ -15,7 +15,9 @@ from app.main import is_isogram
         ("trash", True),
         ("garbage", False),
         ("boolean", False),
-        ("info", True)
+        ("info", True),
+        ("", True),
+        ("sS", False)
     ]
 )
 def test_is_isogram(word: str, expected: bool) -> None:
