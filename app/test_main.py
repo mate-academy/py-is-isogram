7b12d9f4abb47332b8e8e96d25da70f324003842
Ppllupold <kotnazar2005@gmail.com>
2024-06-28 23:03:04 +0300

    dwfgf

--- app/test_main.py
+++ app/test_main.py
@@ -18,5 +18,5 @@ from app.main import is_isogram
         ("info", True)
     ]
 )
-def test_is_isogram(word, expected):
+def test_is_isogram(word: str, expected: bool) -> None:
     assert is_isogram(word) == expected
