37eb75a8957c1f1521cf8060c83cc161803740d3
Felix Poslushnyi <felixp4@gmail.com>
2024-07-02 04:38:40 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,23 @@
+import pytest
+
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        ("", True),
+        ("look", False),
+        ("Adam", False),
+        ("playgrounds", True)
+    ],
+    ids=[
+        "empty string is isogram",
+        "consecutive repeting letters not isogram",
+        "ensitive non consecutive repeating letters not isogram",
+        "is isogram"
+    ]
+)
+def test_is_isogram(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
