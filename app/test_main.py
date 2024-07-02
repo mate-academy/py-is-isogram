1fad6178ef8be048e53b796c27b9e946c6e9c317
Ruslan Samsonovych <arankutakepakuta@gmail.com>
2024-07-02 21:28:47 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,15 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, is_word_isogram",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+    ]
+)
+def test_is_the_word_check_correct(word: str, is_word_isogram: bool) -> None:
+    assert is_isogram(word) == is_word_isogram
