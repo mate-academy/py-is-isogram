58d75c078fa4e8e2e88aa3f204cb62598acc8312
Danylo Skepskyi <kol230305@gmail.com>
2024-07-21 21:52:59 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,31 @@
 from app.main import is_isogram
+from typing import Any
+import pytest
 
-# write your code here
+
+class TestIsogram:
+    @pytest.mark.parametrize(
+        "word,expected_result",
+        [
+            ("", True),
+            ("A", True),
+            ("Ww", False),
+            ("playgrounds", True),
+            ("look", False),
+            ("Adam", False)
+        ]
+    )
+    def test_correct_result(self, word: str, expected_result: bool) -> None:
+        assert is_isogram(word) == expected_result
+
+    @pytest.mark.parametrize(
+        "word",
+        [
+            (1),
+            ([]),
+            ({})
+        ]
+    )
+    def test_raises_error_when_wrong_data(self, word: Any) -> None:
+        with pytest.raises(AttributeError):
+            is_isogram(word)
