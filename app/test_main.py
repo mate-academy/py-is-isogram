c27957377fabfe76ff48fd24f32421e08b4f1f4d
DSahalatyi <dsahalatyi@gmail.com>
2024-07-30 18:29:59 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_correct_word_returns_true() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_incorrect_word_returns_true() -> None:
+    assert is_isogram("look") is False
+
+
+def test_function_is_case_insensitive() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_empty_string_returns_true() -> None:
+    assert is_isogram("") is True
