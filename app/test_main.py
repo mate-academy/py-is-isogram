05fbdd95077d94c772493eecadf21dd22381df99
andreykolomiec <akolomiec982@gmail.com>
2024-08-03 00:18:15 +0300

    Solution3

--- app/test_main.py
+++ app/test_main.py
@@ -12,6 +12,6 @@ def test_is_isogram() -> None:
     assert is_isogram("Adam") is False
     assert is_isogram("") is True
     assert is_isogram("Star") is True
-    assert is_isogram("thumbscrew-japingly") is False
+    assert is_isogram("thumbscrew-japingly") is True
     assert is_isogram("moOse") is False
     assert is_isogram("aba") is False
