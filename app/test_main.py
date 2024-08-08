ba40113659dc5762a801818ae10101a05af1cc4b
Alexandr Politov <apolitov83@gmail.com>
2024-08-08 14:28:31 +0300

    Solution Politov

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_normal_function_running_with_isogram() -> None:
+    assert is_isogram("isogram T_ue") is True
+
+
+def test_normal_function_running_with_not_isogram() -> None:
+    assert is_isogram("not isogrAm False") is False
+
+
+def test_empty_string_must_return_true() -> None:
+    assert is_isogram("") is True
+
+
+def test_single_space_string_must_return_true() -> None:
+    assert is_isogram(" ") is True
+
+
+def test_double_space_string_must_return_false() -> None:
+    assert is_isogram("  ") is False
+
+
+def test_function_must_be_case_insensitive() -> None:
+    assert is_isogram("aA") is False
