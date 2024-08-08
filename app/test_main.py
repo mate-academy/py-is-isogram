d170cd6463e3ce71945f9119702e3a73fbce6c25
Dmytro Poznanskyi <poznanskyi.d@gmail.com>
2024-08-08 17:53:59 +0300

    solving

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,11 @@
 from app.main import is_isogram
 
-# write your code here
+def test_is_isogram() -> None:
+    assert is_isogram("playgrounds")
+    assert not is_isogram("look")
+    assert not is_isogram("Adam")
+    assert is_isogram("")
+    assert is_isogram("isogram")
+    assert not is_isogram("GeeksforGeeks")
+    assert not is_isogram("Alphabet")
+    assert is_isogram("Dermatoglyphics")
