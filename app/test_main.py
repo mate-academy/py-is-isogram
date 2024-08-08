343f351fb454bb0112dded1f56af2c6e16c84048
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-08 20:44:32 +0300

    first commit

--- app/test_main.py
+++ app/test_main.py
@@ -3,11 +3,12 @@ import pytest
 from app.main import is_isogram
 
 
-@pytest.mark.parametrize("test_input,expected", [
-    "playgrounds", True,
-    "look", True,
-    "Adam", True,
-    "",
+@pytest.mark.parametrize("test_input,expected",
+[
+    ("playgrounds", True),
+    ("look", False),
+    ("Adam", False),
+    ("", True),
 ])
 def test_is_isogram(test_input: str, expected: str) -> None:
     assert is_isogram(test_input) == expected
