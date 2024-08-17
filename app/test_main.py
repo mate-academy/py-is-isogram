51f2899fcfd2aaf6877735cfd089489f140bedf3
FalseCoreCentury <simonov0101k@gmail.com>
2024-08-17 10:36:19 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+class TestIso:
+    @pytest.mark.parametrize(
+        "string,expected",
+        [
+            ("", True),
+            ("playground", True),
+            ("look", False),
+            ("adam", False)
+        ]
+    )
+    def test_is_isogram(self, string: str, expected: bool) -> None:
+        assert is_isogram(string) == expected
