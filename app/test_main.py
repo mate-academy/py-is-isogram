e476a40ebea6c9523d616daeaf766e35bdb57ea9
Serhii-Chubur <serhichubur15@gmail.com>
2024-08-25 17:13:24 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,17 @@
 from app.main import is_isogram
 
-def test_empty_string_is_isogram():
-    assert is_isogram("") == True
 
-def test_non_consecutive_letters_are_not_isogram():
-    assert is_isogram('habanera') == False
+def test_empty_string_is_isogram() -> None:
+    assert is_isogram("") is True
 
-def test_isogram_is_case_insensitive():
-    assert is_isogram('Adam') == False
 
-def test_consecutive_letters_are_not_isogram():
-    assert is_isogram('look') == False
+def test_non_consecutive_letters_are_not_isogram() -> None:
+    assert is_isogram("habanera") is False
+
+
+def test_isogram_is_case_insensitive() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_consecutive_letters_are_not_isogram() -> None:
+    assert is_isogram("look") is False
