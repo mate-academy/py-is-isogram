78b14f15436e5e43113c11878728c9f6a842e912
ulichka <ulichka7615@gmail.com>
2024-08-28 17:27:17 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,10 +5,10 @@ from app.main import is_isogram
 @pytest.mark.parametrize(
     "word, expected_result",
     [
-        ('playgrounds', True),
-        ('look', False),
-        ('Adam', False),
-        ('', True)
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True)
     ]
 )
 def test_is_isogram_works_correctly(word: str, expected_result: bool) -> None:
