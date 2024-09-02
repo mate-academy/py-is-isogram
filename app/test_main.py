1097a9772a2080e8778c98752aa25398706d6ba3
Arthur <avers.target@gmail.com>
2024-09-02 13:59:19 +0200

    test: add is_isogram test cases

--- app/test_main.py
+++ app/test_main.py
@@ -11,3 +11,15 @@ def test_case_sensitive() -> None:
 
 def test_non_isogram() -> None:
     assert not is_isogram("Adam")
+
+
+def test_string_with_spaces() -> None:
+    assert not is_isogram("hello world")
+
+
+def test_string_with_special_characters() -> None:
+    assert is_isogram("!@#&*")
+
+
+def test_string_with_numbers() -> None:
+    assert is_isogram("1234567890")
