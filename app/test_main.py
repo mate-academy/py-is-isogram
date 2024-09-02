0520e99aea6079e6d2d1c725c6ca186b5ea00b42
Arthur <avers.target@gmail.com>
2024-09-02 13:51:26 +0200

    test: add is_isogram test cases

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string() -> None:
+    assert is_isogram("")
+
+
+def test_case_sensitive() -> None:
+    assert is_isogram("Dermatoglyphics")
+
+
+def test_non_isogram() -> None:
+    assert not is_isogram("Adam")
