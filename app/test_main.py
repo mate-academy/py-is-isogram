73516cf8baa622b58932b4fa5e0835465195b765
Ivan <vanya.bidnenko@gmail.com>
2024-09-06 12:46:29 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,20 @@
 import pytest
-
 from app.main import is_isogram
 
 
 @pytest.mark.parametrize(
     "word, expected",
     [
-        ('playgrounds', True),  # No repeating letters
-        ('look', False),        # 'o' repeats
-        ('Adam', False),        # 'A' and 'a' are the same letter, case-insensitive
-        ('', True),             # Empty string is considered an isogram
-        ('abcde', True),        # No repeating letters, all unique
-        ('aabbcc', False),      # Letters repeat
-        ('Unique', False),      # 'U' and 'u' are the same letter
-        ('Isogram', True),      # No repeating letters
-        ('Alphabet', False),   # 'a' repeats
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+        ("abcde", True),
+        ("aabbcc", False),
+        ("Unique", False),
+        ("Isogram", True),
+        ("Alphabet", False),
     ]
 )
-def test_is_isogram(word, expected):
+def test_is_isogram(word: str, expected: bool) -> None:
     assert is_isogram(word) == expected
