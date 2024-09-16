62d17a86943891f2035617e196ac76758503150f
Gariman <garimanblack1@gmail.com>
2024-09-16 20:18:07 +0300

    Fix by isogram forgot flake8

--- app/test_main.py
+++ app/test_main.py
@@ -2,16 +2,16 @@ from app.main import is_isogram
 
 
 def test_unique_word() -> None:
-    assert is_isogram("playgrounds") == True
+    assert is_isogram("playgrounds")
 
 
 def test_repeated_letters() -> None:
-    assert is_isogram("look") == False
+    assert not is_isogram("look")
 
 
 def test_non_unique_word() -> None:
-    assert is_isogram("Adam") == False
+    assert not is_isogram("Adam")
 
 
 def test_just_a_string() -> None:
-    assert is_isogram("") == True
+    assert is_isogram("")
