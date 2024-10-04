d72bc5baa7708a52fba85fb09ca0fd97ed4c0cf2
Alona <alena.tsapenko7@gmail.com>
2024-10-04 13:55:41 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -2,16 +2,16 @@ from app.main import is_isogram
 
 
 def test_isogram_lowercase() -> None:
-    assert is_isogram('playgrounds') is True
+    assert is_isogram("playgrounds") is True
 
 
 def test_not_isogram_with_repeating_letters() -> None:
-    assert is_isogram('look') is False
+    assert is_isogram("look") is False
 
 
 def test_not_isogram_case_insensitive() -> None:
-    assert is_isogram('Adam') is False
+    assert is_isogram("Adam") is False
 
 
 def test_empty_string() -> None:
-    assert is_isogram('') is True
+    assert is_isogram("") is True
