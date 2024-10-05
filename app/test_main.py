94ec8bdd97d533b6f78359fea7de4ad51a729d70
Vladyslav Kucherenko <vlados.soc@gmail.com>
2024-10-05 13:01:06 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,41 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        pytest.param(
+            "", True, id="the empty string is an isogram"
+        ),
+        pytest.param(
+            "playgrounds", True, id="word `playgrounds` is a isogram"
+        ),
+        pytest.param(
+            "Adam", False, id="function should be case-insensitive"
+        ),
+        pytest.param(
+            "look", False, id="word should has no repeating letters"
+        )
+
+    ]
+)
+def test_is_isogram(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
+
+
+@pytest.mark.parametrize(
+    "word, expected_error",
+    [
+        pytest.param(
+            5, AttributeError, id="raise AttributeError when value not string"
+        )
+    ]
+)
+def test_is_isogram_invalid_input(
+        word: str,
+        expected_error: Exception
+) -> None:
+    with pytest.raises(expected_error):
+        is_isogram(word)
