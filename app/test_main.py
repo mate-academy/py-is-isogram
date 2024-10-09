3ade1e40a245a6ae4fc5950c5b0fd919e1751c34
Fibonacci <volokitin.vitaliy@icloud.com>
2024-10-09 13:34:29 +0300

    Complated

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+from typing import Any
+
+import pytest
+
+
+@pytest.mark.parametrize("word, expected", [
+    ("playgrounds", True),
+    ("look", False),
+    ("Adam", False),
+    ("", True),
+    ("DERBY", True),
+    ("Alphabet", False)
+])
+def test_is_isogram(word: Any, expected: float) -> None:
+    assert is_isogram(word) == expected
