83071d7607b3b01a98203944ab632730c03cf464
dkn3r <n.diachuk333@gmail.com>
2024-10-11 15:54:44 +0300

    implement py-is-isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,29 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram_empty_string() -> None:
+    assert is_isogram("")
+
+
+def test_is_isogram_single_letter() -> None:
+    assert is_isogram("A")
+
+
+def test_is_isogram_all_unique() -> None:
+    assert is_isogram("playgrounds")
+
+
+def test_is_isogram_repeated_letters() -> None:
+    assert not is_isogram("look")
+
+
+def test_is_isogram_mixed_case() -> None:
+    assert not is_isogram("Adam")
+
+
+def test_is_isogram_case_insensitive_true() -> None:
+    assert is_isogram("Dermatoglyphics")
+
+
+def test_is_isogram_case_insensitive_false() -> None:
+    assert not is_isogram("Alphabet")
