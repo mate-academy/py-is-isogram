9b62532c8510b2991d07b5712a510eb285aee0d8
Ivan Hutsuliak <ivan.guculyak@gmail.com>
2024-10-15 13:59:15 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,47 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "word, expected_result",
+        [
+            ("playgrounds", True),
+            ("PlayGrounds", True),
+            ("look", False),
+            ("Adam", False),
+            ("", True)
+        ],
+        ids=[
+            "playgrounds should be returned True",
+            "PlayGrounds should be returned True",
+            "look should be returned True",
+            "Adam should be returned True",
+            "'' should be returned True"
+        ]
+    )
+    def test_returning_is_isogram(
+        self,
+        word: str,
+        expected_result: bool,
+    ) -> None:
+        assert is_isogram(word) == expected_result
+
+    @pytest.mark.parametrize(
+        "input_data, error",
+        [
+            (1, TypeError),
+            (True, TypeError)
+        ],
+        ids=[
+            "1 should be raised TypeError",
+            "True should be raised TypeError"
+        ]
+    )
+    def test_raising_errors_for_is_isogram(
+        self,
+        input_data: any,
+        error: Exception
+    ) -> None:
+        with pytest.raises(error):
+            is_isogram(input_data)
