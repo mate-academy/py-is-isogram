23b2b40b6a38bad7a36c88804a4dca4b1147576c
Leon <leon.kushnir15@gmail.com>
2024-10-17 14:16:38 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,14 +5,45 @@ from app.main import is_isogram
 @pytest.mark.parametrize(
     "input_string,result",
     [
-        pytest.param("THE stRing", False, id="with UPPERcase and with 2 t"),
-        pytest.param(".....", False, id="with points"),
-        pytest.param("./0184=-bsdjA,#@ *", True, id="with different non-consecutive values"),
-        pytest.param(" ", True, id="with only one space"),
-        pytest.param("", True, id="with empty string"),
-        pytest.param("RUN", True, id="with upper non-consecutive word"),
-        pytest.param("people", False, id="with lower consecutive word")
+        pytest.param(
+            "THE stRing",
+            False,
+            id="with UPPERcase and with 2 t"
+        ),
+        pytest.param(
+            ".....",
+            False,
+            id="with points"
+        ),
+        pytest.param(
+            "./0184=-bsdjA,#@ *",
+            True,
+            id="with different non-consecutive values"
+        ),
+        pytest.param(
+            " ",
+            True,
+            id="with only one space"
+        ),
+        pytest.param(
+            "",
+            True,
+            id="with empty string"
+        ),
+        pytest.param(
+            "RUN",
+            True,
+            id="with upper non-consecutive word"
+        ),
+        pytest.param(
+            "people",
+            False,
+            id="with lower consecutive word"
+        )
     ]
 )
-def test_consecutive_or_non_consecutive_string(input_string, result):
+def test_consecutive_or_non_consecutive_string(
+        input_string: str,
+        result: bool
+) -> bool:
     assert is_isogram(input_string) == result
