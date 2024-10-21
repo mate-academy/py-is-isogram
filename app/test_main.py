b40305d7fe68c062fb264590f89fea9a24cd2b28
Yaroslav Kalugin <yaroslav.kalugin.07@gmail.com>
2024-10-21 09:02:04 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,8 +1,8 @@
 from app.main import is_isogram
 
-def test_some_cases() -> None:
-    assert is_isogram('playgrounds') is True
-    assert is_isogram('look') is False
-    assert is_isogram('Adam') is False
-    assert is_isogram('') is True
 
+def test_some_cases() -> None:
+    assert is_isogram("playgrounds") is True
+    assert is_isogram("look") is False
+    assert is_isogram("Adam") is False
+    assert is_isogram("") is True
