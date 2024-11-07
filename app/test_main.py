e9f6524babcead799bac6a1de1046d5749eb40db
Greena1c <shchedrykserhii@gmail.com>
2024-11-07 12:28:14 +0200

    check_2.0

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,17 @@
 from app.main import is_isogram
 
 
-def test_empty_string() -> None:
-    assert is_isogram("")
+def test_isogram_is_case_insensitive() -> None:
+    assert is_isogram("Dermatoglyphics") == True
+    assert is_isogram("moOse") == False
 
 
-def test_single_letter() -> None:
-    assert is_isogram("a")
+def test_empty_string_is_isogram() -> None:
+    assert is_isogram("") == True
 
 
-def test_isogram_word() -> None:
-    assert is_isogram("category")
+def test_non_consecutive_letters_are_not_isogram() -> None:
+    assert is_isogram("alphabet") == False
 
-
-def test_repeating_letters() -> None:
-    assert not is_isogram("look")
-
-
-def test_mixed_case_word() -> None:
-    assert is_isogram("Alex")
-
-
-def test_mixed_case_word_with_doubles_letter() -> None:
-    assert not is_isogram("Tests")
+def test_consecutive_letters_are_not_isogram() -> None:
+    assert is_isogram("hello") == False
