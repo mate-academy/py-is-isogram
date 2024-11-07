7350fa7f9eb257e1aada0aa40d85f321d8bfec80
Greena1c <shchedrykserhii@gmail.com>
2024-11-07 12:06:09 +0200

    check

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string() -> None:
+    assert is_isogram("")
+
+
+def test_single_letter() -> None:
+    assert is_isogram("a")
+
+
+def test_isogram_word() -> None:
+    assert is_isogram("category")
+
+
+def test_repeating_letters() -> None:
+    assert not is_isogram("look")
+
+
+def test_mixed_case_word() -> None:
+    assert is_isogram("Alex")
+
+
+def test_mixed_case_word_with_doubles_letter() -> None:
+    assert not is_isogram("Tests")
