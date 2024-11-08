2cf0898d12ae73c186a562d21c2f7be04e8f532d
salodoxera <DavidDubovik19@gmail.com>
2024-11-08 17:32:29 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,17 @@
 from app.main import is_isogram
 
 
-def test_words_without_repetitive_letters():
-    assert (is_isogram('playgrounds') is True)
+def test_words_without_repetitive_letters() -> None:
+    assert (is_isogram("playgrounds") is True)
 
 
-def test_words_with_repetitive_letters():
-    assert (is_isogram('look') is False)
+def test_words_with_repetitive_letters() -> None:
+    assert (is_isogram("look") is False)
 
 
-def test_words_with_big_letters():
-    assert (is_isogram('Adam') is False)
+def test_words_with_big_letters() -> None:
+    assert (is_isogram("Adam") is False)
 
 
-def test_empty_line():
-    assert (is_isogram('') is True)
+def test_empty_line() -> None:
+    assert (is_isogram("") is True)
