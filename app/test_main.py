2785e7741e584c310825ab87d8419a5f8fa49d4f
Maxim Khrypunov <khrypunovmax@gmail.com>
2024-11-12 14:51:47 +0200

    solution1

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,34 @@
+import pytest
+from typing import Any
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogramParametr:
+
+    @pytest.mark.parametrize("word, expected_result", [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("Mom", False),
+        ("asGxwdfgu", False),
+        ("", True),
+        ("isogram", True),
+        ("IsoGram", True),
+        ("12345", True),
+        ("hello-world", False),
+    ])
+    def test_is_isogram(self, word: str, expected_result: bool) -> None:
+        assert expected_result == is_isogram(word)
+
+
+class TestIsIsogramErrors:
+
+    @pytest.mark.parametrize("invalid_input", [
+        10,
+        [10],
+        None,
+    ])
+    def test_is_isogram_type_error(self, invalid_input: Any) -> None:
+        with pytest.raises(AttributeError):
+            is_isogram(invalid_input)
