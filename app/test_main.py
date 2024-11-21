205508e12f1b0fc79e93838ff83415e35782e3e9
Adam Halka <galkaadam4@gmail.com>
2024-11-21 09:14:42 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_word_is_isogram():
+    assert is_isogram("playgrounds") is True
+
+
+def test_word_missing():
+    assert is_isogram("") is True
+
+
+def test_word_is_not_isogram():
+    assert is_isogram("Adam") is False
