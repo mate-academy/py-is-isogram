f3a05e3a2fab9e7499eb093102daab1c35b3e0ea
Adam Halka <galkaadam4@gmail.com>
2024-11-21 09:18:45 +0100

    remake1

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,13 @@
 from app.main import is_isogram
 
 
-def test_word_is_isogram():
+def test_word_is_isogram() -> None:
     assert is_isogram("playgrounds") is True
 
 
-def test_word_missing():
+def test_word_missing() -> None:
     assert is_isogram("") is True
 
 
-def test_word_is_not_isogram():
+def test_word_is_not_isogram() -> None:
     assert is_isogram("Adam") is False
