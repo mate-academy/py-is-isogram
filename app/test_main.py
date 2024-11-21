5a69dff31592703c8b6dbf916b67ada479945d40
Adam Halka <galkaadam4@gmail.com>
2024-11-21 09:32:01 +0100

    remake4

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,7 @@
 from app.main import is_isogram
 
 
-def test_word_is_an_isogram() -> None:
-    assert is_isogram("playgrounds") is True
-
-
-def test_word_missing() -> None:
-    assert is_isogram("") is True
-
-
-def test_word_is_repeated() -> None:
-    assert is_isogram("Adam") is False
-
-
-def test_with_spaces() -> None:
-    assert is_isogram("playg rounds") is True
-
-
-def test_word_with_hyphens() -> None:
-    assert is_isogram("playg-rounds") is True
+def test_consecutive_letters_are_not_isogram() -> None:
+    assert is_isogram("aabbcc") == False
+    assert is_isogram("abac") == False
+    assert is_isogram("abcdef") == True
