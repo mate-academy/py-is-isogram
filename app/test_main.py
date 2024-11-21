30b06ee2a99e103872d13769428e42d555107256
Adam Halka <galkaadam4@gmail.com>
2024-11-21 09:32:56 +0100

    Typе5

--- app/test_main.py
+++ app/test_main.py
@@ -2,6 +2,6 @@ from app.main import is_isogram
 
 
 def test_consecutive_letters_are_not_isogram() -> None:
-    assert is_isogram("aabbcc") == False
-    assert is_isogram("abac") == False
-    assert is_isogram("abcdef") == True
+    assert is_isogram("aabbcc") is False
+    assert is_isogram("abac") is False
+    assert is_isogram("abcdef") is True
