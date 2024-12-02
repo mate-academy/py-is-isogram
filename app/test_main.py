1ce6172941f2578b66f2c5ea23175e0a31b1399a
Beliar24 <blackdid7@gmail.com>
2024-12-02 23:28:20 +0200

    isogram tests implemented

--- app/test_main.py
+++ app/test_main.py
@@ -14,5 +14,5 @@ from app.main import is_isogram
      ("%^&*", True)
      ]
 )
-def test_is_isogram(word, expected):
+def test_is_isogram(word: str, expected: bool) -> None:
     assert is_isogram(word) is expected
