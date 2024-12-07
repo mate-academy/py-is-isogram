7f3ed386d2c9a7cf8903e1bd7aab1b3629101a4b
Valeriy Semenik <valeriy.semenik@gmail.com>
2024-12-07 23:20:07 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,44 @@
-from app.main import is_isogram
+import pytest
 
-# write your code here
+from .main import is_isogram
+
+
+def test_add_not_str_argument() -> None:
+    with pytest.raises(TypeError):
+        is_isogram(12.52)
+
+
+def test_notonly_letters_in_arguments() -> None:
+    assert (is_isogram("Gkjl2#!@") == False), \
+        "The word should consist of Letters only"
+
+
+def test_is_isogram_should_return_bool() -> None:
+    assert isinstance(is_isogram("World"), bool)
+
+
+def test_capital_letters_same_small_in_arguments() -> None:
+    assert (is_isogram("GrilL") == False), \
+        "The capital letters same the small"
+
+
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        ('playgrounds', True),
+        ('look', False),
+        ('Adam', False),
+        ('', True),
+    ],
+    ids=[
+        "all small letters isogram = True",
+        "all small letters not isogram = False",
+        "mix letters not isogram = False",
+        "the empty string is an isogram",
+    ]
+)
+def test_functionality(word: str,
+                       result: bool
+                       ) -> None:
+    assert (is_isogram(word) == result),\
+        (f"Word: {word},should be {result} isogram status")
