d9c27621da66313c4381af7a7c538de006967507
Psychox1k <success01001011@gmail.com>
2024-12-10 20:32:26 +0100

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        pytest.param(
+            "Kirill", False,
+            id="test_with_consecutive_and_non_consecutive_repeated_letters"),
+        pytest.param(
+            "", True,
+            id="test_empty_word_is_isogram"),
+        pytest.param(
+            "Water", True,
+            id="test_word_without_repeated_letters"),
+        pytest.param(
+            "African", False,
+            id="test_word_with_non_consecutive_repeated_letters"),
+        pytest.param(
+            "Mellon", False,
+            id="test_word_with_non_consecutive_repeated_letters")
+    ]
+)
+def test_word_is_consecutive(word: str, result: bool) -> None:
+    assert (is_isogram(word) == result)
