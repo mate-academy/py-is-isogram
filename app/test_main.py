3aeed7cec21eb330b783e8d9cee164a5150e03f3
Illya Kovaliuk <kovalukilla271@gmail.com>
2024-12-14 00:58:30 +0200

    Solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,21 @@
 from app.main import is_isogram
 
 
+# write your code here
 def test_that_big_letter_eaquel_to_small_letter() -> None:
     assert is_isogram("AbC") is True
     assert is_isogram("AbCAbC") is False
+
+
+def test_is_isogram_for_empty_string() -> None:
+    assert is_isogram("") is True
+
+
+def test_is_isogram_for_consecutive_letters() -> None:
+    assert is_isogram("Abc") is True
+    assert is_isogram("Abca") is False
+
+
+def test_is_isogram_for_not_consecutive_letters_with_empty_string() -> None:
+    assert is_isogram("Abc") is True
+    assert is_isogram("AaBbCc") is False
