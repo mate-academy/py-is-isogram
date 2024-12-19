c98dcdd856be1fba612c5666aca943fcf0da4752
Admin <mr.artem450@gmail.com>
2024-12-19 19:31:53 +0200

    Task

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+def test_isogram_examples():
+    assert is_isogram('playgrounds') is True
+    assert is_isogram('look') is False
+    assert is_isogram('Adam') is False
+    assert is_isogram('') is True
+
+def test_case_insensitivity():
+    assert is_isogram('MoM') is False
+    assert is_isogram('aBcDeFg') is True
+
+def test_non_letter_characters():
+    with pytest.raises(ValueError):
+        is_isogram('hello, world')
