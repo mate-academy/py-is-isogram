7fb23727d641e5fbabaf9f2dd903e83760011ac2
Maks Vakulenko <maksimvakulenkooo@gmail.com>
2024-12-20 15:47:05 +0100

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_isogram_word() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_not_isogram_word() -> None:
+    assert is_isogram("look") is False
+
+
+def test_big_and_small_letters() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_empty_string() -> None:
+    assert is_isogram("") is True
