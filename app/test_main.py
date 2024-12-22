d31e5b22e6ae72803b7b6a89df84ad8748a883f0
Mazurkav <aleshkamazurkevich@gmail.com>
2024-12-22 13:19:21 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,19 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram() -> None:
+    assert is_isogram('playgrounds') is True, "Test case 'playgrounds' failed"
+    assert is_isogram('look') is False, "Test case 'look' failed"
+    assert is_isogram('Adam') is False, "Test case 'Adam' failed"
+    assert is_isogram('') is True, "Test case empty string failed"
+    assert is_isogram('Dermatoglyphics') is True, "Test case 'Dermatoglyphics' failed"
+    assert is_isogram('aba') is False, "Test case 'aba' failed"
+    assert is_isogram('moOse') is False, "Test case 'moOse' failed"
+    assert is_isogram('abcdefg') is True, "Test case 'abcdefg' failed"
+    assert is_isogram('isogram') is True, "Test case 'isogram' failed"
+    assert is_isogram('repeated') is False, "Test case 'repeated' failed"
+
+
+if __name__ == "__main__":
+    pytest.main()
