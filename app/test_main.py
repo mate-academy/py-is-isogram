c44b6e3a795fe33c620c420258fcba440dde329b
Arsenii Aristov <arseniyaristov07@gmail.com>
2024-12-22 13:47:48 +0200

    Solution_for_word

--- app/test_main.py
+++ app/test_main.py
@@ -1,26 +1,16 @@
 import pytest
 from app.main import is_isogram
 
-
 @pytest.mark.parametrize(
     ("word", "expected"),
     [
-        ("Mark", True),
-        ("playground", True),
+        ("playgrounds", True),
         ("look", False),
+        ("Adam", False),
         ("", True),
-        ("phone", True)
+        ("Isogram", True),
+        ("RepeaT", False),
     ],
-    ids=[
-        "all_unique_lowercase",
-        "repeated_letters",
-        "case_insensitive",
-        "empty_string",
-        "mixed_case",
-    ]
 )
-def test_for_words(
-    word: str,
-    expected: bool
-) -> None:
+def test_is_isogram(word: str, expected: bool) -> None:
     assert is_isogram(word) == expected
