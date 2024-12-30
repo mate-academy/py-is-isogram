ef0e320d86887d7d8febb0aaf6894e42c3bf0330
Dmytro.Poberezhnyi <foxymood@gmail.com>
2024-12-30 08:36:09 +0200

    sol

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_base_word() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_base_word_negative() -> None:
+    assert is_isogram("look") is False
+
+
+def test_case_if_case_insensitive() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_empty_string() -> None:
+    assert is_isogram("") is True
