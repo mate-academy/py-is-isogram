6a73bf418510d13cc27a1554c8c92202e51b86cd
Ivan Khomych <ivan.khomychh@gmail.com>
2025-01-05 00:18:44 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,expected_result",
+    [
+        pytest.param('playgrounds', True, id="Text should return True"),
+        pytest.param('look', False, id="Text should return False"),
+        pytest.param('Adam', False, id="The test checks the processing of capital letters"),
+        pytest.param('', True, id="Text check empty str")
+    ]
+)
+def test_should_check_return_isogram(word: str, expected_result: bool) -> None:
+    assert is_isogram(word) == expected_result
+
