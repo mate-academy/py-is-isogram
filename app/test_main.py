69484ed7132d870a02c52c6cc5f45dcffdc7d92f
Liza Shulika <liza.shulika2009@gmail.com>
2025-01-05 20:59:51 +0200

    isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,22 @@
+import pytest
+
+
 from app.main import is_isogram
 
-# write your code here
+
+def test_should_return_true() -> None:
+    word = ""
+    parts = is_isogram(word)
+    assert parts == True
+
+
+def test_should_return_false() -> None:
+    word = "look"
+    parts = is_isogram(word)
+    assert parts == False
+
+
+def test_should_return_is_true1() -> None:
+    word = "lads"
+    parts = is_isogram(word)
+    assert parts == True
