37e9db503a71f0ae26cf3a4fc25d9abc7a02c351
JulianskyMe <juliakamynskaya@gmail.com>
2025-01-09 23:38:16 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+def test_case_insensitive_is_isogram() -> None:
+    assert not is_isogram("AaAAAaaaa")
+
+
+def test_empty_is_isogram() -> None:
+    assert is_isogram("")
+
+
+def test_non_consecutive_is_isogram() -> None:
+    assert not is_isogram("acegikmoqsuwy")
+
+
+def test_consecutive_is_isogram() -> None:
+    assert not is_isogram("abcdefghijklmnopqrstuvwxyz")
