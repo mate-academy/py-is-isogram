79be905f602ac2f8913ce3c08654509c33754ad1
JulianskyMe <juliakamynskaya@gmail.com>
2025-01-09 23:44:14 +0200

    final upd

--- app/test_main.py
+++ app/test_main.py
@@ -5,8 +5,8 @@ from app.main import is_isogram
 @pytest.mark.parametrize("word, expected", [
     ("AaAAAaaaa", False),
     ("", True),
-    ("acegikmoqsuwy", False),
-    ("abcdefghijklmnopqrstuvwxyz", False),
+    ("acegikmoqsuwy", True),
+    ("abcdefghijklmnopqrstuvwxyz", True),
 ])
 def test_is_isogram(word: str, expected: bool) -> None:
     assert is_isogram(word) == expected
