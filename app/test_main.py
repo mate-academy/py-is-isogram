fcc3e13604d83d5a42be49518fa06ea098ef07db
JulianskyMe <juliakamynskaya@gmail.com>
2025-01-10 21:04:18 +0200

    upd

--- app/test_main.py
+++ app/test_main.py
@@ -7,7 +7,7 @@ from app.main import is_isogram
     ("playgrounds", True),  # don't repeat
     ("look", False),  # repeated and placed close
     ("abracadabra", False),  # repeated,but don"t stand close
-    ("Unique", True)  # capitalized and lowercase letters
+    ("Unique", False)  # capitalized and lowercase letters
 ])
 def test_is_isogram(word: str, expected: bool) -> None:
     assert is_isogram(word) == expected
