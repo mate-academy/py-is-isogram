55982de9d83fe9f80061e24b1082288c8ec1f762
Olha Yukhnenko <pyolh36@gmail.com>
2025-01-15 14:13:06 +0200

    solved_task

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,30 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+def test_not_repeating_letters() -> None:
+    assert is_isogram('playgrounds') == True
+
+
+def test_consecutive_letters() -> None:
+    assert is_isogram('look') == False
+
+
+def test_not_consecutive_letters() -> None:
+    assert is_isogram('Adam') == False
+
+
+def test_with_empty_list() -> None:
+    assert is_isogram('') == True
+
+
+@pytest.mark.parametrize(
+    "current_text, expected", [
+        ('playgrounds' is True),
+        ('look' is False),
+        ('Adam' is False),
+        ('' is True),
+    ]
+)
+def test_various_case(current_text: str, expected: bool) -> None:
+    assert is_isogram(current_text) == expected
