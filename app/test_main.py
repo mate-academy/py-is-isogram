8aa86a9f93ddbaf4194bf24b001ee753ce89ceb1
Olha Yukhnenko <pyolh36@gmail.com>
2025-01-15 14:19:07 +0200

    fix_double_quotes_del_equal_bool

--- app/test_main.py
+++ app/test_main.py
@@ -3,27 +3,27 @@ from app.main import is_isogram
 
 
 def test_not_repeating_letters() -> None:
-    assert is_isogram('playgrounds') == True
+    assert is_isogram("playgrounds")
 
 
 def test_consecutive_letters() -> None:
-    assert is_isogram('look') == False
+    assert not is_isogram("look")
 
 
 def test_not_consecutive_letters() -> None:
-    assert is_isogram('Adam') == False
+    assert not is_isogram("Adam")
 
 
 def test_with_empty_list() -> None:
-    assert is_isogram('') == True
+    assert is_isogram(" ")
 
 
 @pytest.mark.parametrize(
     "current_text, expected", [
-        ('playgrounds' is True),
-        ('look' is False),
-        ('Adam' is False),
-        ('' is True),
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        (" ", True),
     ]
 )
 def test_various_case(current_text: str, expected: bool) -> None:
