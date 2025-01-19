6a9733e65121ce7f5ff25e1760d2e1dd748527a3
Dmytro <dimkanividimka4@gmail.com>
2025-01-20 00:29:08 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -11,26 +11,3 @@ def test_string_with_different_cases_of_the_same_letter() -> None:
 
 def test_not_only_non_consecutive_letters_are_not_an_isogram() -> None:
     assert is_isogram("look") is False
-
-#
-# from app.main import is_isogram
-#
-# import pytest
-#
-#
-# def test_value_should_be_letters() -> None:
-#     assert (isinstance(is_isogram(""), bool))
-
-#
-#
-# @pytest.mark.parametrize(
-#     "word, expected",
-#     [
-#         ("playgrounds", True),
-#         ("look", False),
-#         ("Adam", False),
-#         ("", True)
-#     ]
-# )
-# def test_check_is_isogram(word: str, expected: bool) -> None:
-#     assert is_isogram(word) == expected
