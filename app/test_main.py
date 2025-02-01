bcd38103d71877ed5743bac45fd308f2cbb07454
Ksenia Malashchuk <malashchuk.ksenia@gmail.com>
2025-02-01 18:38:41 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,22 +1,22 @@
 from app.main import is_isogram
 
-def test_empty_string():
-    assert is_isogram('') is True
+def test_empty_string() -> None:
+    assert is_isogram("") is True
 
-def test_isogram_word():
-    assert is_isogram('playgrounds') is True
+def test_isogram_word() -> None:
+    assert is_isogram("playgrounds") is True
 
-def test_non_isogram_word():
-    assert is_isogram('look') is False
+def test_non_isogram_word() -> None:
+    assert is_isogram("look") is False
 
-def test_case_insensitivity():
-    assert is_isogram('Adam') is False
+def test_case_insensitivity() -> None:
+    assert is_isogram("Adam") is False
 
-def test_long_isogram():
-    assert is_isogram('Dermatoglyphics') is True
+def test_long_isogram() -> None:
+    assert is_isogram("Dermatoglyphics") is True
 
-def test_single_character():
-    assert is_isogram('a') is True
+def test_single_character() -> None:
+    assert is_isogram("a") is True
 
-def test_repeated_letters():
-    assert is_isogram('banana') is False
+def test_repeated_letters() -> None:
+    assert is_isogram("banana") is False
