2eb29969dd79ab96c2e63a9d169c89e03a645f1c
Sviatoslav Zabegin <zabegins777@gmail.com>
2025-02-02 17:42:11 +0100

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,19 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+def test_empty_string():
+    assert is_isogram("") is True
+
+def test_unique_letters():
+    assert is_isogram("playgrounds") is True
+
+def test_repeating_letters():
+    assert is_isogram("look") is False
+    assert is_isogram("banana") is False
+
+def test_case_insensitivity():
+    assert is_isogram("Adam") is False
+    assert is_isogram("IsOgRam") is True
+
+def test_long_isogram():
+    assert is_isogram("subdermatoglyphic") is True
