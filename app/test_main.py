4cd8715027657df306584f6332c68e88aa5c8d5a
Vinohradov Volodymyr <eXpertt13@gmail.com>
2025-02-05 17:58:25 +0200

    tests to func is_isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,27 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "word,expected_value",
+        [
+            ("playgrounds", True),
+            ("look", False),
+            ("Autor", True),
+            ("Adam", False),
+            ("a", True),
+            ("", True),
+        ],
+        ids=[
+            "correct word isogram",
+            "word with repeating letter",
+            "word with uppercase letter",
+            "word with uppercase and repeating",
+            "word with one letter",
+            "empty string"
+        ]
+    )
+    def test_is_isogram(self, word: str, expected_value: bool) -> None:
+        assert (is_isogram(word) is expected_value
+                ), "The word must have unique letter for considerate isogram"
