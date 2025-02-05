6016769f7363a7a077ca78cff332aa201ba1d144
RuslanTopal <baranotik15@gmail.com>
2025-02-05 14:13:27 +0000

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
+from xmlrpc.client import Fault
+
+import pytest
 from app.main import is_isogram
 
-# write your code here
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("", True),
+        ("Adam", False),
+        ("a", True)
+    ],
+    ids=[
+        "correct isogram 'playgrounds'",
+        "word with repetitive",
+        "an empty line is also considered an isogram",
+        "Checking for different case letters",
+        "A word made up of one letter is an isogram"
+    ]
+)
+def test_(word: str, result: bool) -> None:
+    assert (
+        is_isogram(word) is result
+    ), f"The letters must be unique for the word to be considered an isogram."
