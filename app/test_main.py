d0b9804cae8b1c975ddc1841c9e626e275bd9ffd
Pavlo Nikolaiev <workhardpolly@gmail.com>
2025-02-06 18:21:13 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("PLAY", True),
+        ("lOoK", False),
+        (" ", True),
+        ("", True)
+    ],
+    ids=[
+        "check lowercase true statement",
+        "check lowercase false statement",
+        "check Uppercase true statement",
+        "check Randomcase false statement",
+        "check whitespace true statement",
+        "check empty string true statement"
+    ]
+)
+def test_standart_queries(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
