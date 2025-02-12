777111254f1635de8b0623111609fdd2d4cad4cb
Andriy Hrudnyy <andgrudniy@gmail.com>
2025-02-12 14:22:15 +0100

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,20 +1,21 @@
 from app.main import is_isogram
 
-def test_check_the_empty_string():
+
+def test_check_the_empty_string() -> None:
     result = is_isogram("")
     assert result is True
 
 
-def test_check_isogramic_word():
+def test_check_isogramic_word() -> None:
     result = is_isogram("playgrounds")
     assert result is True
 
 
-def test_check_not_isogramic_word():
+def test_check_not_isogramic_word() -> None:
     result = is_isogram("look")
     assert result is False
 
 
-def test_check_not_isogramic_word_with_two_registers():
+def test_check_not_isogramic_word_with_two_registers() -> None:
     result = is_isogram("Adam")
     assert result is False
