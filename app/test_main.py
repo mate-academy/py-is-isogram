7ce31d6d8c2fc25a030d09955bf8721857d29375
Johnny <ivensmk@gmail.com>
2025-02-16 16:21:52 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,19 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+@pytest.mark.parametrize(
+    "word, expected_result",
+    [
+        ("", True),
+        ("   ", False),
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("Robert", False)
+    ]
+)
+def test_cent_combination_valid(
+        word: str,
+        expected_result: bool
+) -> None:
+    assert is_isogram(word) == expected_result
