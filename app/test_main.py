4b72d5181c4de991c235bfeee90f4d277058a1d8
Oleksii Churbanov <lyoshatop@gmail.com>
2025-02-17 15:09:01 +0200

    implement tests for py-is-isogram

--- app/test_main.py
+++ app/test_main.py
@@ -2,24 +2,24 @@ from app.main import is_isogram
 
 
 def test_empty_string_is_isogram() -> None:
-    assert(is_isogram("") == True)
+    assert is_isogram("") is True
 
 
 def test_playground_is_isogram() -> None:
-    assert (is_isogram("playground") == True)
+    assert (is_isogram("playground") is True)
 
 
 def test_word_with_lower_case_duplicates_is_isogram() -> None:
-    assert (is_isogram("look") == False)
+    assert (is_isogram("look") is False)
 
 
 def test_word_with_upper_case_duplicates_is_isogram() -> None:
-    assert (is_isogram("oleksII") == False)
+    assert (is_isogram("oleksII") is False)
 
 
 def test_word_with_upper__and_lower_case_duplicates_is_isogram() -> None:
-    assert (is_isogram("aNton") == False)
+    assert (is_isogram("aNton") is False)
 
 
 def test_word_with_hyphens_is_isogram() -> None:
-    assert (is_isogram("abc-def-ghi") == False)
+    assert (is_isogram("abc-def-ghi") is False)
