30c60edf24ae72c67d8e52e0011986bcbcc1e591
Taras Shevchyk <tarashevchik27@gmail.com>
2025-02-27 23:02:00 +0200

    add return type annotation

--- app/test_main.py
+++ app/test_main.py
@@ -1,7 +1,7 @@
 from app.main import is_isogram
 
 
-def test_is_isogram():
+def test_is_isogram() -> None:
     assert is_isogram("playgrounds") is True
     assert is_isogram("look") is False
     assert is_isogram("Adam") is False
