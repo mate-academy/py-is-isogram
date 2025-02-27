7310d3345cf7f747868d7f6b9d853f40d0ae5459
Taras Shevchyk <tarashevchik27@gmail.com>
2025-02-27 23:01:07 +0200

    refactor

--- app/test_main.py
+++ app/test_main.py
@@ -2,7 +2,7 @@ from app.main import is_isogram
 
 
 def test_is_isogram():
-    assert is_isogram('playgrounds') is True
-    assert is_isogram('look') is False
-    assert is_isogram('Adam') is False
-    assert is_isogram('') is True
+    assert is_isogram("playgrounds") is True
+    assert is_isogram("look") is False
+    assert is_isogram("Adam") is False
+    assert is_isogram("") is True
