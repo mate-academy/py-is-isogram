f031dda2a53f48b2da245df4366e3a57facd7c00
Nikita Korenevskyi <korenevsky37@gmail.com>
2025-02-28 22:13:52 +0200

    Done

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,38 @@
+import pytest
+
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "word, result",
+        [
+            pytest.param(
+                "",
+                True,
+                id="void string"
+            ),
+            pytest.param(
+                "dragon",
+                True,
+                id="isogram word"
+            ),
+            pytest.param(
+                "Adam",
+                False,
+                id="not isogram word"
+            ),
+            pytest.param(
+                "look",
+                False,
+                id="not isogram word"
+            )
+        ]
+    )
+    def test_is_isogram(
+            self,
+            word,
+            result
+    ):
+        assert is_isogram(word) == result
