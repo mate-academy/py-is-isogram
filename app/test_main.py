8d85078686f1002205eff82d21fb7e179bedb0d7
Marcin Djaczuk <marcindjaczuk@gmail.com>
2025-03-05 11:12:17 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,28 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "word,result",
+        [
+            pytest.param("playgrounds",
+                         True,
+                         id="test long word"),
+            pytest.param("look",
+                         False,
+                         id="test word with consecutive letters"),
+            pytest.param("chocolate",
+                         False,
+                         id="test word with nonconsecutive letters"),
+            pytest.param("",
+                         True,
+                         id="test empty word"),
+        ]
+
+    )
+    def test_correct_answer(self, word: str, result: bool) -> None:
+        assert is_isogram(word) == result
+
+    def test_only_letters(self) -> None:
+        assert is_isogram("abcde123") is False
