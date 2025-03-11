88670a412249265480d13c48c74c56a410438707
Ioann Nadtocheev <ioannnatocheev@gmail.com>
2025-03-11 12:41:03 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,43 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram_valid_word() -> None:
+    assert is_isogram("playgrounds") == True
+
+
+def test_is_isogram_invalid_word() -> None:
+    assert is_isogram("look") == False
+
+
+def test_is_isogram_invalid_word_case_sensitive() -> None:
+    assert is_isogram("Adam") == False
+
+
+def test_is_isogram_empty_string() -> None:
+    assert is_isogram(" ") == True
+
+
+def test_is_isogram_case_insensitivity() -> None:
+    assert is_isogram("Aa") == False
+    assert is_isogram("mM") == False
+    assert is_isogram("Mm") == False
+
+
+def test_is_isogram_single_character() -> None:
+    assert is_isogram("a") == True
+    assert is_isogram("Z") == True
+
+
+def test_is_isogram_two_same_characters() -> None:
+    assert is_isogram("aa") == False
+    assert is_isogram("bb") == False
+    assert is_isogram("MM") == False
+
+
+def test_is_isogram_long_word_no_repeat() -> None:
+    assert is_isogram("abcdefghijklmno") == True
+
+
+def test_is_isogram_long_word_with_repeats() -> None:
+    assert is_isogram("abcdefghijklmna") == False
+    assert is_isogram("abcdefghijklmnaa") == False
