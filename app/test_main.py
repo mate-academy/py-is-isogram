57e2170b3274ddb9448fc36397571c09bc3f27b6
Volodymyr Vitsenko <your@mail.com>
2025-03-11 19:43:50 +0100

    isogram_tester

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsogram:
+
+    @pytest.mark.parametrize(
+        "word, expected",
+        [
+            ("look", False),
+            ("playgrounds", True),
+            ("", True),
+            ("kobold", False),
+            ("kobOld", False)
+        ],
+        ids=[
+            "not isogram",
+            "isogram",
+            "empty string",
+            "non consecutive repeat",
+            "repeat with diferent cases"
+        ]
+    )
+    def test_is_isogram(self, word: str, expected: bool) -> None:
+        assert is_isogram(word) == expected
