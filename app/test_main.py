b9b0f48a608736828c1a910823f984e3d49ea54d
Roman Kostiv <rkostiv@gmail.com>
2025-03-15 21:34:15 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import is_isogram
 
-# write your code here
+def test_is_isogram():
+    assert is_isogram("John") == True
+
+def test_empty_is_isogram():
+    assert is_isogram("") == True
+
+def test_string_with_different_cases_is_not_isogram():
+    assert is_isogram("Book") == False
+
+def test_no_consecutive_cases_is_not_isogram():
+    assert is_isogram("Eagle") == False
