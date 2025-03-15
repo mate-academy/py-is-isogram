eb5186fa2a6302ca9d9b9030404bf8193b3c4482
Roman Kostiv <rkostiv@gmail.com>
2025-03-15 21:37:38 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,17 @@
 from app.main import is_isogram
 
+
 def test_is_isogram() -> None:
-    assert is_isogram("John") == True
+    assert is_isogram("John") is True
+
 
 def test_empty_is_isogram() -> None:
-    assert is_isogram("") == True
+    assert is_isogram("") is True
+
 
 def test_string_with_different_cases_is_not_isogram() -> None:
-    assert is_isogram("Book") == False
+    assert is_isogram("Book") is False
+
 
 def test_no_consecutive_cases_is_not_isogram() -> None:
-    assert is_isogram("Eagle") == False
+    assert is_isogram("Eagle") is False
