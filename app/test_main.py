2cc766333787ab502830d9f63953e4754215c869
Zahar <galushkozahar>
2025-03-16 20:12:40 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize("string, extended", [
+    ("mM", False),
+    ("Adam", False),
+    ("", True),
+    (" ", True),
+    ("  ", False),
+    ("look", False),
+    ("lake", True)
+])
+def test_is_gram(string: str, extended: bool) -> None:
+    assert is_isogram(string) == extended
