41a2851be8b4349db230ef1f9f2315d60703c8ad
Oleh Kozdryn <kozdryn.oleg@gmail.com>
2025-03-17 14:45:33 +0200

    solution v3

--- app/test_main.py
+++ app/test_main.py
@@ -4,9 +4,14 @@ from app.main import is_isogram
 def test_is_isogram() -> None:
     assert is_isogram("abcdef")
     assert is_isogram("word")
-    assert is_isogram("Word")
     assert is_isogram("")
     assert is_isogram("A")
+    assert is_isogram("Word")
     assert not is_isogram("zZ")
     assert not is_isogram("aa")
     assert not is_isogram("aabbcc")
+
+
+def test_non_consecutive_letters_are_not_isogram() -> None:
+    assert not is_isogram("hello")
+    assert not is_isogram("ball")
