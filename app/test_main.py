c369cb3fe3c4546f309bda59fe37ae00c6fcc9e8
Oleh Kozdryn <kozdryn.oleg@gmail.com>
2025-03-17 14:42:19 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram() -> None:
+    assert is_isogram("abcdef")
+    assert is_isogram("word")
+    assert is_isogram("Word")
+    assert is_isogram("")
+    assert is_isogram("A")
+    assert not is_isogram("zZ")
+    assert not is_isogram("aa")
+    assert not is_isogram("aabbcc")
+
