ef935421f16289854edb872339a2e11e78acb54c
Your Name <stas101203rakitin@gmail.com>
2025-03-20 16:52:56 +0200

    Tests written

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,30 @@
+import pytest
+
+from typing import Any
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        ("plAyGroUnds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True)
+    ]
+)
+def test_return_correct_bool(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
+
+
+@pytest.mark.parametrize(
+    "word,exception",
+    [
+        (5, AttributeError),
+        (["look", "rea"], AttributeError),
+        ((5, "perf"), AttributeError),
+    ]
+)
+def test_word_must_be_str(word: Any, exception: type[Exception]) -> None:
+    with pytest.raises(exception):
+        is_isogram(word)
