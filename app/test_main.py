0d9bec0ee7452efc39093828c76d2dc2fb2a3193
Anton Vynychenko <kinid@ukr.net>
2025-03-21 17:32:09 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,31 @@
+from pickle import FALSE
+
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "input_string,expected_result",
+    [
+        pytest.param("", True, id="Empty string should return True"),
+        ("playgrounds", True),
+        ("Adam", False),
+        ("look", False)
+    ]
+)
+def test_is_isogram(input_string, expected_result):
+    assert is_isogram(input_string) == expected_result
+
+
+@pytest.mark.parametrize(
+    "input_string,expected_error",
+    [
+        pytest.param(["str"], AttributeError,
+                     id="Should return AttributeError when input is list"),
+        pytest.param(54, AttributeError,
+                     id="Should return AttributeError when input is integer"),
+    ]
+)
+def test_is_isogram_error(input_string, expected_error):
+    with pytest.raises(expected_error):
+        is_isogram(input_string)
