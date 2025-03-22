8256397804eb2f434c098d72d9afb7956e14e40e
Dmytro Lisovyi <kenedy11@ukr.net>
2025-03-22 13:24:10 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
+from pickle import FALSE
 from app.main import is_isogram
+import pytest
+
+
+@pytest.mark.parametrize("word, definition", [
+    ("playgrounds", True),
+    ("look", FALSE),
+    ("Adam", FALSE),
+    (" ", True)
+])
+def test_word_iso(word: str, definition: bool) -> None:
+    result = is_isogram(word)
+    result == definition
 
 # write your code here
