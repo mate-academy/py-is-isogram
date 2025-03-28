0d6ae7e60ea6b028565964abe4052347b0f8707c
Mykola Nakonechnuy <teddykolia@ukr.net>
2025-03-28 11:49:06 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,20 @@
+from pytest import mark, param
+
 from app.main import is_isogram
 
-# write your code here
+
+@mark.parametrize(
+    "word,expected_result",
+    [
+        param("playgrounds", True,
+              id="should return Trues when isogram"),
+        param("look", False,
+              id="should return False when duplicate letter"),
+        param("Adam", False,
+              id="should return False when capitalized duplicate"),
+        param("", True,
+              id="should return Trues when no empty str"),
+    ]
+)
+def test_is_isogram(word: str, expected_result: bool) -> None:
+    assert is_isogram(word) == expected_result
