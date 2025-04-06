e7e5ac935a301f98a3088a624eec6987984b18f4
Timur Khvat <timurkhvat@gmail.com>
2025-04-06 11:46:41 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,17 @@
 from app.main import is_isogram
 
 
-def test_get_True_if_not_the_same_latters():
-    assert is_isogram('playgrounds') is True
+def test_get_true_if_not_the_same_latters() -> None:
+    assert is_isogram("playgrounds") is True
 
 
-def test_get_False_if_word_have_the_same_latters():
-    assert is_isogram('look') is False
+def test_get_false_if_word_have_the_same_latters() -> None:
+    assert is_isogram("look") is False
 
 
-def test_get_False_if_word_have_the_same_upper_latter():
-    assert is_isogram('Adam') is False
+def test_get_false_if_word_have_the_same_upper_latter() -> None:
+    assert is_isogram("Adam") is False
 
 
-def test_get_True_if_empty_string():
-    assert is_isogram('') is True
+def test_get_true_if_empty_string() -> None:
+    assert is_isogram("") is True
