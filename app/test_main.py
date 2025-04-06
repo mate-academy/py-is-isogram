5386e438ac204bade31b08829cbf078550c77a7a
Timur Khvat <timurkhvat@gmail.com>
2025-04-06 11:43:03 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_get_True_if_not_the_same_latters():
+    assert is_isogram('playgrounds') is True
+
+
+def test_get_False_if_word_have_the_same_latters():
+    assert is_isogram('look') is False
+
+
+def test_get_False_if_word_have_the_same_upper_latter():
+    assert is_isogram('Adam') is False
+
+
+def test_get_True_if_empty_string():
+    assert is_isogram('') is True
