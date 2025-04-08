fa625d73eb5c69b2029a6835fe557179f6f04f3c
Denys Halytskyi <denys.halytskyi17@gmail.com>
2025-04-08 22:26:55 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,24 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize("word", [
+    pytest.param(""),
+    pytest.param("playgrounds"),
+    pytest.param("Cents"),
+    pytest.param("aBwTq"),
+    pytest.param("INSTAL"),
+])
+def test_is_isogram_true(word: str) -> None:
+    assert is_isogram(word)
+
+
+@pytest.mark.parametrize("word", [
+    pytest.param("look"),
+    pytest.param("Adam"),
+    pytest.param("LIDL"),
+    pytest.param("oreo"),
+])
+def test_is_isogram_false(word: str) -> None:
+    assert not is_isogram(word)
