5625cb6a67b37844d90e2dd23b9dedb260afd1cd
Oleksandr Lytvyn <litvinalex632@gmail.com>
2025-04-14 15:08:19 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,24 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        pytest.param("", True, id="test empty string"),
+        pytest.param("Futaba", False, id="test false and upper letter"),
+        pytest.param("k", True, id="test one letter"),
+        pytest.param("someone", False, id="test false on lower word"),
+        pytest.param("phone", True, id="test simple word for true"),
+        pytest.param("MAN", True, id="test upper word"),
+        pytest.param("lkfajsdlwerqlwke", False, id="test long word"),
+        pytest.param("siuuuuu", False, id="test consecutive repeating letters")
+    ]
+)
+def test_is_isogram(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
+
+
+def test_incorrect_value() -> None:
+    with pytest.raises(AttributeError):
+        is_isogram(1)
