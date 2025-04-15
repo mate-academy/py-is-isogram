e40a2f057a88910cbd0d9308c85f67ee3eb35fcb
LassisterV16 <lassisterv16@gmail.com>
2025-04-15 18:40:32 +0200

    fixed flake8 errors

--- app/test_main.py
+++ app/test_main.py
@@ -16,11 +16,14 @@ from app.main import is_isogram
 def test_function_is_case_insensitive(word: str, result: bool) -> None:
     assert is_isogram(word) == result
 
+
 def test_function_should_return_true_when_get_empty_string() -> None:
-    assert is_isogram("") == True
+    assert is_isogram("") is True
+
 
 def test_function_get_consecutive_letter() -> None:
-    assert is_isogram("accept") == False
+    assert is_isogram("accept") is False
+
 
 def test_function_get_non_consecutive_letter() -> None:
-    assert is_isogram("natural") == False
+    assert is_isogram("natural") is False
