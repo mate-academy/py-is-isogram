9ee4c716251a480743969c32eb15e75f86340e7f
LassisterV16 <lassisterv16@gmail.com>
2025-04-15 18:34:51 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        ("telephone", False),
+        ("Adam", False),
+        ("absolute", True),
+        ("ACTIVE", True),
+        ("ACCESS", False)
+    ]
+)
+def test_function_is_case_insensitive(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
+
+def test_function_should_return_true_when_get_empty_string() -> None:
+    assert is_isogram("") == True
+
+def test_function_get_consecutive_letter() -> None:
+    assert is_isogram("accept") == False
+
+def test_function_get_non_consecutive_letter() -> None:
+    assert is_isogram("natural") == False
