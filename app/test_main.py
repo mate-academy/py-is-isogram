333c5f077b3a6bb99a0cdb844f6e9b41cb15f85d
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-04-19 21:28:54 +0100

    v1

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_true_for_playgrounds() -> None:
+    assert is_isogram("playground")
+
+
+def test_false_for_look() -> None:
+    assert not is_isogram("look")
+
+
+def test_is_case_sensitive() -> None:
+    assert not is_isogram("Adam")
+
+
+def test_true_for_empty_string() -> None:
+    assert is_isogram("")
