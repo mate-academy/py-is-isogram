ecc3d91ad6154d500f4fc057bdded8d9a85d5a45
Vitalik <lg.mobile0808@gmail.com>
2025-04-25 18:22:31 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -3,13 +3,13 @@ from app.main import is_isogram
 
 
 @pytest.mark.parametrize(
-    "word", "expected_word",
+    "word", "expected_bool",
     [
-        is_isogram('playgrounds') is True
-        is_isogram('look') is False
-        is_isogram('Adam') is False
-        is_isogram('') is True
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ('', True),
     ]
 )
-def test_is_isogram(word: str, expected_word: str) -> None:
-    assert is_isogram(word) == expected_word
+def test_is_isogram(word: str, expected_bool: str) -> None:
+    assert is_isogram(word) == expected_bool
