49fffa93873862403b533e6d136b91cc7d85fde0
Mateusz Mroz <mrozu@student.agh.edu.pl>
2025-04-27 19:09:56 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
+import pytest
+
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsogram:
+    @pytest.mark.paramitze(
+        "izo_string, result",
+        [
+            pytest.param("Hania", True),
+            pytest.param("Mateusz", True),
+            pytest.param("Mom", False),
+            pytest.param("", True),
+            pytest.param("1Mat2eusz", False),
+            pytest.param("1Mat2eusz", False),
+            pytest.param("daD", False),
+        ]
+    )
+    def test_for_isogram(
+            self,
+            izo_string: str,
+            result: bool
+    ) -> None:
+        wyn = is_isogram(izo_string)
+        assert wyn == result
