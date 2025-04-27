fbb33b4fbd10996e71f094bddb5e2cf713cdd5b0
Mateusz Mroz <mrozu@student.agh.edu.pl>
2025-04-27 19:19:35 +0200

    'Solution3'

--- app/test_main.py
+++ app/test_main.py
@@ -8,11 +8,11 @@ class TestIsogram:
     @pytest.mark.parametrize(
         "izo_string, result",
         [
-            pytest.param("Hania", True),
+            pytest.param("Hania", False),
             pytest.param("Mateusz", True),
             pytest.param("Mom", False),
             pytest.param("", True),
-            pytest.param("1Mat2eusz", False),
+            pytest.param("1Mat2eusz", True),
             pytest.param("daD", False),
         ]
     )
