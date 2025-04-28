8c2f03a6de770b80632f18bbddf7e34b940b1c30
Qellexi <99039851+Qellexi@users.noreply.github.com>
2025-04-28 14:19:48 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,20 @@
 from app.main import is_isogram
 
-# write your code here
+def test_if_empty_string_is_isogram() -> None:
+    word = ""
+    assert is_isogram(word), "The empty string is an isogram"
+
+
+def test_if_duplicate_is_isogram() -> None:
+    word = "look"
+    assert is_isogram(word), "Duplicate letters cannot be in an isogram"
+
+
+def test_if_upper_and_lower_case_included() -> None:
+    word = "Adam"
+    assert is_isogram(word), "Upper and lower case are not included"
+
+
+def test_if_string_type() -> None:
+    word = 532
+    assert is_isogram(word), "Isogram has a string type"
