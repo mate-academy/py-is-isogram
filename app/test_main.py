b74f69e6c7d4d0035f6c497df904bb5e89fa8d2b
Danylo-D87 <quaestor88@gmail.com>
2025-04-30 21:02:05 +0300

    sol

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_should_return_true_for_empty_string() -> None:
+    assert is_isogram("") == True
+
+
+def test_should_return_true_for_all_unique_letters() -> None:
+    assert is_isogram("playgrounds") == True
+
+
+def test_should_return_false_for_duplicate_letters() -> None:
+    assert is_isogram("look") == False
+
+
+def test_should_be_case_insensitive() -> None:
+    assert is_isogram("Adam") == False
+
+
+def test_should_return_true_for_single_letter() -> None:
+    assert is_isogram("a") == True
+
+
+def test_should_return_false_for_same_letter_different_case() -> None:
+    assert is_isogram("aA") == False
