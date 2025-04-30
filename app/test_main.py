6051118c14ca6be83ee3191529412c0942b7615f
Danylo-D87 <quaestor88@gmail.com>
2025-04-30 21:40:34 +0300

    sol 3

--- app/test_main.py
+++ app/test_main.py
@@ -2,28 +2,28 @@ from app.main import is_isogram
 
 
 def test_should_return_true_for_empty_string() -> None:
-    assert is_isogram("") == True
+    assert is_isogram("")
 
 
 def test_should_return_true_for_all_unique_letters() -> None:
-    assert is_isogram("playgrounds") == True
+    assert is_isogram("playgrounds")
 
 
 def test_should_return_false_for_duplicate_letters() -> None:
-    assert is_isogram("look") == False
+    assert not is_isogram("look")
 
 
 def test_should_be_case_insensitive() -> None:
-    assert is_isogram("Adam") == False
+    assert not is_isogram("Adam")
 
 
 def test_should_return_true_for_single_letter() -> None:
-    assert is_isogram("a") == True
+    assert is_isogram("a")
 
 
 def test_should_return_false_for_same_letter_different_case() -> None:
-    assert is_isogram("aA") == False
+    assert not is_isogram("aA")
+
 
 def test_should_return_false_for_duplicate_letters_2() -> None:
-    assert is_isogram("local") == False
-
+    assert not is_isogram("local")
