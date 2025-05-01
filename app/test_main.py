4e76250b87817bd2146c917a2efacf2081602dfd
Bogdan0101 <kononovb777@gmail.com>
2025-05-01 14:00:25 +0300

    Solution#1

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,34 @@
 from app.main import is_isogram
 
-# write your code here
+from pytest import mark, param
+
+
+class TestModify:
+    @mark.parametrize(
+        "word, result",
+        [
+            param(
+                "playgrounds",
+                True,
+                id="ErrorWord: playgrounds"
+            ),
+            param(
+                "look",
+                False,
+                id="ErrorWord: look"
+            ),
+            param(
+                "Adam",
+                False,
+                id="ErrorWord: Adam"
+            ),
+            param(
+                "",
+                True,
+                id="ErrorWord: empty line"
+            ),
+        ]
+    )
+    def test_modify(self, word: str, result: bool) -> None:
+        assert isinstance(word, str), "Please enter a string"
+        assert is_isogram(word) == result
