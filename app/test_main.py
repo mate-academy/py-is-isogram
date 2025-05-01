d17b378f1039169200d175531aa05615197e2864
Bohdan <bsuhij@gmail.com>
2025-05-01 18:44:22 +0300

    Task done

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram_1():
+    result = is_isogram('playgrounds')
+    assert result is True
+
+
+def test_is_isogram_2():
+    result = is_isogram('look')
+    assert result is False
+
+
+def test_is_isogram_3():
+    result = is_isogram('Adam')
+    assert result is False
+
+
+def test_is_isogram_4():
+    result = is_isogram('')
+    assert result is True
