9936a3d66b576221337b0ea3a0e2afec5a711426
Dimon <kosma4evskuj@gmail.com>
2025-05-17 17:32:00 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string_is_isogram() -> None:
+    assert is_isogram('') is True
+
+
+def test_isogram_lowercase() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_non_lowercase() -> None:
+    assert is_isogram("look") is False
+
+
+def test_non_isogram_mixedcase() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_isogram_mixedcase() -> None:
+    assert is_isogram("Word") is True
