68755e940b2a5fe078a09e1dc04214db806a0a39
Bartek Pietrzyk <bartekpietrzyk@interia.eu>
2025-05-21 19:32:59 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -3,35 +3,29 @@ from app.main import is_isogram
 
 def test_empty_string_is_isogram() -> None:
     isogram = is_isogram("")
-    if isogram is True:
-        assert True
+    assert isogram
 
 
 def test_is_isogram_false() -> None:
     isogram = is_isogram("hello world")
-    if isogram:
-        assert False
+    assert not isogram
 
 
 def test_is_isogram_true() -> None:
     isogram = is_isogram("bartek")
-    if isogram is True:
-        assert True
+    assert isogram
 
 
 def test_isogram_case_insensitive() -> None:
     isogram = is_isogram("WoRlD")
-    if isogram is True:
-        assert True
+    assert isogram
 
 
 def test_non_consecutive_letters_are_not_isogram() -> None:
     isogram = is_isogram("pytest")
-    if isogram:
-        assert False
+    assert not isogram
 
 
 def test_consecutive_letters_are_isogram() -> None:
     isogram = is_isogram("hello")
-    if isogram:
-        assert False
+    assert not isogram
