7cfab74da92b1d3b3ac42ebdd23ef6713f62ac17
Vitalii Mushta <vitaliimushta1993@gmail.com>
2025-05-22 12:59:08 +0200

    solution2

--- app/test_main.py
+++ app/test_main.py
@@ -2,27 +2,27 @@ from app.main import is_isogram
 import pytest
 
 
-def test_should_return_true_if_empty_string():
+def test_should_return_true_if_empty_string() -> None:
     word = ""
     assert is_isogram(word) is True
 
 
-def test_should_return_false_if_small_and_big_letters():
+def test_should_return_false_if_small_and_big_letters() -> None:
     word = "Mama"
     assert is_isogram(word) is False
 
 
-def test_should_return_true_if_correct():
+def test_should_return_true_if_correct() -> None:
     word = "England"
     assert is_isogram(word) is True
 
 
-def test_should_return_false_if_incorrect():
+def test_should_return_false_if_incorrect() -> None:
     word = "america"
     assert is_isogram(word) is False
 
 
-def test_should_raise_attributeerror():
+def test_should_raise_attributeerror() -> None:
     word = ["america"]
     with pytest.raises(AttributeError):
         assert is_isogram(word)
