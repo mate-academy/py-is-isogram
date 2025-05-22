bedbd1123c6614a9b8f1d6785f7fa1747db64c5c
MiKPo4eLiK <stassojcuk@gmail.com>
2025-05-22 22:57:56 +0300

    well

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string_is_isogram() -> None:
+    assert is_isogram("") is True
+
+
+def test_single_word_is_isogram() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_word_with_repeating_letters() -> None:
+    assert is_isogram("look") is False
+
+
+def test_case_insensitivity() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_mixed_case_isogram() -> None:
+    assert is_isogram("Dermatoglyphics") is True
