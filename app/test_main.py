6e05a1c8b1c7bb56e28b3383e377194c36553e27
Denis <godlike.kipish@gmail.com>
2025-06-10 21:27:01 +0300

    Solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -2,16 +2,16 @@ from app.main import is_isogram
 
 
 def test_should_check_isogram() -> None:
-    assert is_isogram("today") == True
+    assert is_isogram("today") is True
 
 
 def test_should_return_false() -> None:
-    assert is_isogram("look") == False
+    assert is_isogram("look") is False
 
 
 def test_should_be_case_insensitive() -> None:
-    assert is_isogram("Adam") == False
+    assert is_isogram("Adam") is False
 
 
 def test_should_check_empty_string() -> None:
-    assert is_isogram("") == True
+    assert is_isogram("") is True
