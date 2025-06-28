6c8015b8ac575507848574f8311ca431213f39cb
Maksym Buslenko <maksbusl@gmail.com>
2025-06-28 21:47:10 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,20 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True)
+    ]
+)
+def test_function_has_or_no_similar_letters(
+        word: str,
+        result: bool
+) -> None:
+    assert is_isogram(word) is result
+
+
