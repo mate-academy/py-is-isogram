4c7e9abfb4ffe6787ed5e6961996b5eeb5bd8de6
Vadym <kirilkovadim193@gmail.com>
2025-07-10 17:51:10 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_word_string_is_empty() -> None:
+    assert is_isogram("") is True
+
+
+def test_word_without_repeated_letters() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_word_with_repeated_letters() -> None:
+    assert is_isogram("look") is False
+
+
+def test_with_same_letters_in_different_registers() -> None:
+    assert is_isogram("Adam") is False
