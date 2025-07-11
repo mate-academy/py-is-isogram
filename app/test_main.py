9239defbd69cabd6f5b21664c7a57e145aae0670
MakovM <makovinskiy09@gmail.com>
2025-07-11 17:33:02 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,32 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "input_str, expected",
+    [
+        pytest.param(
+            "",
+            True,
+            id="test when string is empty"
+        ),
+        pytest.param(
+            "background",
+            True,
+            id="test when letters don't repeat"
+        ),
+        pytest.param(
+            "book",
+            False,
+            id="test when letters repeat"
+        ),
+        pytest.param(
+            "Test",
+            False,
+            id="test when letters in different case are repeated"
+        )
+    ]
+)
+def test_is_isogram(input_str: str, expected: bool) -> None:
+    assert is_isogram(input_str) == expected
