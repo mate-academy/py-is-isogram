32785bfb897d4dd02302d42b467828afeba54ff1
Vovas <vovaafon25@gmail.com>
2025-07-11 23:22:35 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,27 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "string,result",
+    [
+        ("", True),
+        ("playgrounds", True),
+        ("AdaM", False),
+        ("Mm", False),
+        ("M", True),
+        ("1234567891", False),
+        ("#$%^#*()", False)
+    ],
+    ids=[
+        "test empty string",
+        "test regular case",
+        "test mixed case",
+        "test mixed case",
+        "test single char",
+        "check letters",
+        "check symbols"
+    ]
+)
+def test_is_isogram(string: str, result: bool) -> None:
+    assert is_isogram(string) is result
