b4c2b2c78f496771c54a1554494d92f323d40e93
Oleksii Kostenikov <akotenikov@gmail.com>
2025-07-11 21:50:14 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,30 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,expected_result",
+    [
+        ["", True],
+        ["  ", False],
+        ["Expect", False],
+        ["AdAm", False],
+        ["abca", False],
+        ["plane", True],
+        ["@#$!^&", True],
+        ["@#$@!^&", False],
+    ],
+    ids=[
+        "empty string",
+        "two spacelines",
+        "same letter in different cases",
+        "same letter in upper case",
+        "same letter in lower case",
+        "doesnt contain repeating letters",
+        "contain special symbols",
+        "contain repeating special symbols",
+    ]
+)
+def test_is_isogram(word: str, expected_result: bool) -> None:
+    assert is_isogram(word) == expected_result
