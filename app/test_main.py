37b14cce2663580ff56800516fc36a1524af6234
Vadym Vizirov <vadimplus57@gmail.com>
2025-07-12 17:31:23 +0300

    isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+        ("MaTe", True),
+        ("%#$_!", True),
+    ],
+    ids=[
+        "returns True because word isogram",
+        "returns because 'oo'",
+        "returns False because A upper",
+        "str len zero",
+        "all str must be lower",
+        "element different"
+    ]
+)
+def test_isogram_result(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
