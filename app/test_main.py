235f05e87830359613eb2425158ec3202bc7a048
benfict <benfict@gmail.com>
2025-07-14 02:32:56 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+    ],
+    ids=[
+        "all_unique_letters",
+        "repeated_letters_look",
+        "case_insensitive_false",
+        "empty_string",
+    ]
+)
+def test_is_isogram(word: str, expected: str) -> None:
+    assert is_isogram(word) == expected
