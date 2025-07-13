794f1ab32d13fd8ff8c0a79e0b525929840e0d43
yarik <super.racing@ukr.net>
2025-07-13 23:45:28 +0300

    wrote tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True)
+    ],
+    ids=[
+        "word with unique letters",
+        "word with double letter",
+        "word with uppercase",
+        "empty string"
+    ]
+)
+def test_method(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
