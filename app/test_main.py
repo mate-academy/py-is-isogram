1e7927aa381db13e92cd159bea20dbb02e989b67
reinvvay <reinvvay@gmail.com>
2025-07-14 21:06:18 +0300

    Fix

--- app/test_main.py
+++ app/test_main.py
@@ -2,20 +2,20 @@ from app.main import is_isogram
 
 
 def test_casual_case() -> None:
-    assert is_isogram("over") == True
+    assert is_isogram("over")
 
 
 def test_false_case() -> None:
-    assert is_isogram("hello") == False
+    assert not is_isogram("hello")
 
 
 def test_empty_string() -> None:
-    assert is_isogram("") == True
+    assert is_isogram("")
 
 
 def test_case_immune() -> None:
-    assert is_isogram("helLo") == False
+    assert not is_isogram("helLo")
 
 
 def test_non_consecutive_letters() -> None:
-    assert is_isogram("aba") == False
+    assert not is_isogram("aba")
