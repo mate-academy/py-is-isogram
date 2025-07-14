901d19be5bc62a1965d59d9bd34167db3422996c
Лілія <liliia@MacBook-Air-Lilia.local>
2025-07-14 09:01:26 +0300

    fixed

--- app/test_main.py
+++ app/test_main.py
@@ -3,28 +3,29 @@ from app.main import is_isogram
 
 def test_is_isogram_should_return_true() -> None:
     assert (
-        is_isogram("world") == True
+        is_isogram("world") is True
     ), "test should check if the word is isogram"
 
 
 def test_is_isogram_should_return_false() -> None:
     assert (
-        is_isogram("assert") == False
+        is_isogram("assert") is False
     ), "test should return False if letter count is more than 1"
 
 
 def test_is_isogram_should_be_case_insensitive() -> None:
     assert (
-        is_isogram("helLo") == False
+        is_isogram("helLo") is False
     ), "test should be case insensitive"
 
 
 def test_when_string_empty() -> None:
     assert (
-        is_isogram("") == True
+        is_isogram("") is True
     ), "empty string should return True"
+
 
 def test_should_return_false_when_two_letters() -> None:
     assert (
-        is_isogram("hipopotam") == False
+        is_isogram("hipopotam") is False
     ), "should return False"
