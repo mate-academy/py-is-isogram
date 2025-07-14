56a34458c581eb2c2be66bf04d7430472adfe18a
reinvvay <reinvvay@gmail.com>
2025-07-14 21:00:12 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_casual_case() -> None:
+    assert is_isogram("over") == True
+
+
+def test_false_case() -> None:
+    assert is_isogram("hello") == False
+
+
+def test_empty_string() -> None:
+    assert is_isogram("") == True
+
+
+def test_case_immune() -> None:
+    assert is_isogram("helLo") == False
+
+
+def test_non_consecutive_letters() -> None:
+    assert is_isogram("aaaa")
