67ea30a2ba72c31c5c9c823d53411cef50a1d1ec
8ajarz <8ajarz_zaprasza@protonmail.com>
2025-07-24 13:57:45 +0700

    develop

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "string,result",
+    [
+        ("playgrounds", True,),
+        ("look", False),
+        ("Adam", False),
+        pytest.param("", True, id="empty is isogram"),
+        pytest.param(None, False,
+                     marks=pytest.mark.xfail(reason="None as an input"),
+                     id="expected fail",)
+    ]
+)
+def test_correct_n_expected(string: str, result: bool) -> bool:
+    assert is_isogram(string) == result
