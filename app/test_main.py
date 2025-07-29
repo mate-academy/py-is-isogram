2acdc38673ad929c863e1dba37b43bbe13d2594b
Nick Konkov <kharkivnick@gmail.com>
2025-07-29 17:39:08 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_if_string_is_empty() -> None:
+    assert is_isogram("")
+
+
+def test_if_string_is_isogram() -> None:
+    assert is_isogram("playgrounds")
+
+
+def test_if_string_is_not_isogram() -> None:
+    assert not is_isogram("look")
+
+
+def test_if_string_starts_from_title_letter() -> None:
+    assert not is_isogram("Adam")
