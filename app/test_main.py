4f5812ef609ccf80209dcb42bbf416a0ef268f80
Arseniy Kushnir <arseniykushnir2007@gmail.com>
2025-08-25 15:11:45 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,27 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        ("", True),
+        ("isogram", True),
+        ("hello", False),
+        ("Aba", False),
+        ("123", True),
+        ("Isogram and me", False),
+        ("QwerTyuiopasdFghjklzxcvbnm", True)
+    ],
+    ids=[
+        "Word is empty",
+        "Word is isogram in lower case",
+        "Word isn`t isogram in lower case",
+        "Word isn`t isogram in multy cases",
+        "Word is isogram value its a integer",
+        "Word isn`t isogram value, it`s a two words",
+        "Word is isogram value it`s a big unique string"
+    ]
+)
+def test_main(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
