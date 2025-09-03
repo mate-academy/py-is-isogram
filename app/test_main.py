f5067086d691411cce6cc5badc18e02616f18a1d
vasylhladyshev404 <vasyl@nextgear.nl>
2025-09-03 13:24:53 +0200

    1

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,30 @@
-from app.main import is_isogram
+import app.main as main
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, result",
+    [  
+        ("mama", False),
+        ("Mama", False),
+        ("loiut", True),
+        ("Loliu", False),
+        ("", True),
+        ("playgrounds", True),
+    ]
+)
+def test_is_isogram_examples(word: str, result: bool) -> None:
+    out = main.is_isogram(word)
+    assert isinstance(out, bool), "Return type must be boolean"
+    assert out == result, f"Output {out} should be {result}"
+
+
+@pytest.mark.parametrize("word", [
+    "aa",
+    "book",
+    "letter",
+    "BooK",
+    "committee",
+])
+def test_consecutive_letters_are_not_isogram(word):
+    assert main.is_isogram(word) is False
