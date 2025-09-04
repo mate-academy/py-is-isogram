6a3582016088a1adb81f5608c39275f37e271dfb
Yurii Palahitskyi <brebston7@gmail.com>
2025-09-04 09:53:16 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
+import pytest
+from typing import List
 
-# write your code here
+
+# Тест чи слова є isogram
+@pytest.mark.parametrize(
+    "word, excepted",
+    [
+        ("playgrounds", True),
+        ("", True),
+        ("look", False),
+        ("Adam", False),
+        ("Dermatoglyphics", True),
+    ],
+)
+def test_is_isogram(
+        word: str,
+        excepted: List[list]
+) -> None:
+    assert is_isogram(word) is excepted
