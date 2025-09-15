89d7637f13ccbe3eee282a395c5b4fbd832b7249
Maksym Kiporenko <kiporenkomaks8@gmail.com>
2025-09-15 15:16:35 +0300

    Complete

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,28 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        ('playgrounds', True),
+        ('look', False),
+        ('Adam', False),
+        ('', True)
+    ]
+)
+def test_word_is_not_repeat(word: str, result: bool) -> None:
+    result = is_isogram(word)
+    assert is_isogram('') is True
+    assert is_isogram('Adam') is False
+    assert is_isogram('aDaM') is False
+    assert is_isogram('abracadabra') is False
+    assert is_isogram('paper') is False
+    assert is_isogram('playgrounds') is True
+    assert is_isogram('lamp') is True
+    assert is_isogram('letter') is False
+    assert is_isogram('look') is False
+    assert isinstance(result, bool)
+
+
+
