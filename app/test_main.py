60bcfb0fd090fd66e210c26818464ed37fcb20c8
Eduardo Doerner <eduardodoerner@gmail.com>
2025-09-16 21:55:28 -0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,20 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+        ("Isogram", True),
+        ("Aa", False),
+        ("Ab", True),
+    ]
+)
+def test_basic_cases(word: str, expected: bool) -> None:
+    actual = is_isogram(word)
+    assert actual == expected
+    assert isinstance(actual, bool)
