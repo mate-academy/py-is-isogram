79fc1494e8e0a956a557239d352ed706629d2c94
0lex1y <kotov.oleksiy@icloud.com>
2025-09-19 00:58:44 +0300

    Test

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 def is_isogram(word: str) -> bool:
+    if not isinstance(word, str):
+        raise TypeError("word must be an string")
     word_lower = word.lower()
     for letter in word_lower:
         if word_lower.count(letter) > 1:
             return False
     return True
+
+print(is_isogram('look'))
--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,24 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize("word,expected", [
+    ("playgrounds", True),
+    ("look", False),
+    ("Adam", False),
+    ("", True)
+])
+def test_is_isogram(word: str, expected: bool) -> None:
+    assert is_isogram(word) == expected
+
+def test_is_isogram_str() -> None:
+    with pytest.raises(TypeError):
+        is_isogram(0)
+    with pytest.raises(TypeError):
+        is_isogram(None)
+    with pytest.raises(TypeError):
+        is_isogram(2.5)
+    with pytest.raises(TypeError):
+        is_isogram(['a', 'b'])
+    with pytest.raises(TypeError):
+        is_isogram(True)
