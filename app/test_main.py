0cea837d6bd2398538f9faf67677c807b454b217
gfray-castilho <gfraycastilho@gmail.com>
2025-09-25 14:59:21 -0300

    solved task

--- app/test_main.py
+++ app/test_main.py
@@ -37,7 +37,5 @@ def test_is_isogram_non_string_raises(bad_input: object):
         ("abc-", False),
     ]
 )
-
-
 def test_is_isogram_assertion_error(word, expected):
     assert is_isogram(word) is expected
