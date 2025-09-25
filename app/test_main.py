aab665d3fc37144d7b46ad362c27a183036cc923
gfray-castilho <gfraycastilho@gmail.com>
2025-09-25 14:29:32 -0300

    solved task

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,46 @@
+import pytest
 from app.main import is_isogram
+from typing import Union
 
-# write your code here
+@pytest.mark.parametrize(
+    "word,expected",
+    [("look", False),
+     ("", True),
+     ("Playground", True),
+     ("Adam", False),
+
+
+     ]
+)
+
+
+def test_is_isogram_(word, expected) -> None:
+    assert is_isogram(word) is expected
+    assert isinstance(is_isogram(word), bool)
+
+
+def test_is_isogram_true() -> None:
+    assert is_isogram("Playground") == True
+
+
+@pytest.mark.parametrize("bad_input",
+                         [
+                             3.5,
+                             10,
+                             None,
+                             [1],
+                             {"word": 10},
+                             "abc1"
+                         ]
+                         )
+def test_is_isogram_non_string_raise(
+        bad_input: Union[
+            float,
+            int,
+            list,
+            dict,
+            str,
+            None
+        ]) -> None:
+    with pytest.raises(TypeError):
+        is_isogram(word=bad_input)
