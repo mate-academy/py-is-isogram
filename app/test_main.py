129dae11d1b8073284e3f971560dd9b93a3b580d
Tomasz Herman <tomekh2015@gmail.com>
2025-09-26 13:08:11 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,37 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string() -> None:
+    assert is_isogram("") is True, "Empty string should be an isogram"
+
+
+@pytest.mark.parametrize("word", [
+    "playgrounds",
+    "Python",
+    "isogram",
+    "Quiz",
+])
+def test_valid_isograms(word: str) -> None:
+    assert is_isogram(word) is True, f"{word} should be an isogram"
+
+
+@pytest.mark.parametrize("word", [
+    "look",
+    "Adam",
+    "letter",
+    "Balloon",
+])
+def test_non_isograms(word: str) -> None:
+    assert is_isogram(word) is False, f"{word} should not be an isogram"
+
+
+def test_case_insensitivity() -> None:
+    assert is_isogram("Aa") is False, "Function should be case-insensitive"
+
+
+def test_single_letter_words() -> None:
+    for letter in "abcdefghijklmnopqrstuvwxyz":
+        assert is_isogram(letter) is True, (
+            f"Single letter {letter} should be an isogram"
+        )
