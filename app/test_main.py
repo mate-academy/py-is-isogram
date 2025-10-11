c17507006093568cd45a4914cb1963000044a5ba
Kacper Kogut <kogutkacper7@gmail.com>
2025-10-11 16:47:38 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,30 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+
+    ]
+)
+def test_is_isogram(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
+
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        1,
+        ["look", 34],
+        None,
+
+    ]
+)
+def invalid_input(word: str, result: bool) -> None:
+    with pytest.raises(TypeError):
+        is_isogram(word)
