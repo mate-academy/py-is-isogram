1e20eded6a7365120ea50b934694c966e09773f6
Dawid <kucharzjaw@gmail.com>
2025-10-14 11:03:27 +0200

    solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 def is_isogram(word: str) -> bool:
+    if word == "":
+        return True
     word_lower = word.lower()
     for letter in word_lower:
         if word_lower.count(letter) > 1:
--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,45 @@
+from typing import Any
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,expected",
+    [
+        ("", True),
+        ("a", True),
+        ("subdermatoglyphic", True),
+        ("QuIz", True),
+        ("Adam", False),
+        ("pipe", False),
+        ("Alphabet", False),
+        ("LoOk", False),
+        ("playGrounds", True),
+    ]
+)
+def test_is_isogram(word: str, expected: bool) -> None:
+    result = is_isogram(word)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "invalid_input",
+    [1234, 2.5, None, ["a", "b"], {"word": "abd"}, True]
+)
+def test_is_isogram_invalid_input(invalid_input: Any) -> None:
+    with pytest.raises((TypeError, ValueError)):
+        is_isogram(invalid_input)
+
+
+def test_is_isogram_returns_isogram_only() -> None:
+    words = ["subdermatoglyphic",
+             "quiz",
+             "playgrounds",
+             "look",
+             "",
+             "Adam",
+             "quiz"
+             ]
+    for word in words:
+        result = is_isogram(word)
+        assert isinstance(result, bool)
