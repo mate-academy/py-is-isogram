c71c20345ba2cd79dc52b8918630a2b95cc33753
Thalles Gabriel <thallesgabrieldasilvacosta@gmail.com>
2025-10-22 16:14:45 -0300

    New Solution

--- app/test_main.py
+++ app/test_main.py
@@ -15,5 +15,5 @@ from app.main import is_isogram
         ("Dermatoglyphics", True),
     ],
 )
-def test_is_isogram(word, expected):
+def test_is_isogram(word: str, expected: bool) -> None:
     assert is_isogram(word) == expected
