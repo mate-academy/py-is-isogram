07add8f6a3ea1b400e2f99d68ce670b782ed50aa
Stas Sytnykov <stassytnykov@gmail.com>
2025-11-01 19:38:50 +0200

    fix: flake8

--- app/test_main.py
+++ app/test_main.py
@@ -8,10 +8,10 @@ from app.main import is_isogram
 @pytest.mark.parametrize(
     "word, expected",
     [
-        ('playgrounds', True),
-        ('look', False),
-        ('Adam', False),
-        ('', True),
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
     ],
 )
 def test_is_isogram(word: str, expected: bool) -> None:
