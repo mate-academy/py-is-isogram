f380e97cb1874bccd037a464951b5b2d0d49812d
Stanislav Kolesnyk <skolesnyk8@gmail.com>
2025-11-03 19:02:29 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,17 +1,15 @@
 def is_isogram(word: str) -> bool:
     """
     Проверяет, является ли слово изограммой.
+
     Изограмма — это слово без повторяющихся букв
-    (без учёта регистра). Пустая строка — изограмма.
+    (без учета регистра). Пустая строка считается изограммой.
 
     Args:
-        word (str): входное слово, состоящее только из букв.
+        word (str): Слово, состоящее только из букв.
 
     Returns:
         bool: True, если слово — изограмма, иначе False.
     """
     word_lower = word.lower()
-    for letter in word_lower:
-        if word_lower.count(letter) > 1:
-            return False
-    return True
+    return len(set(word_lower)) == len(word_lower)
