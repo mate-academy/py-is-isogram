f1eb6294837ab144e6e871da8e4bf6466c9f755f
Kyrylo Kilin <killin@ukr.net>
2025-11-03 18:40:19 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,39 @@
-from app.main import is_isogram
+import importlib
+import pytest
 
-# write your code here
+
+mane = importlib.import_module("app.main")
+
+
+def test_returns_bool() -> None:
+    assert isinstance(mane.is_isogram(""), bool), \
+        "function must return a boolean"
+
+
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+
+
+        ("a", True),
+        ("aba", False),
+        ("abca", False),
+        ("Dermatoglyphics", True),
+        ("Subdermatoglyphic", True),
+        ("Alphabet", False),
+        ("Aa", False),
+    ],
+)
+def test_examples_and_various_cases(word: str, expected: bool) -> None:
+    assert mane.is_isogram(word) is expected
+
+
+def test_case_insensitivity_explicit() -> None:
+
+    assert mane.is_isogram("AbCdEfGhIjK") is True
+    assert mane.is_isogram("AabcDEFgh") is False
