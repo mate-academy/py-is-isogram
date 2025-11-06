223f4ba4305a3e09a1e46473fae5890ce731768d
Vadym Vynokur <king.krollie@gmail.com>
2025-11-06 20:20:57 +0200

    add tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Aa", False),
+        ("", True),
+        ("Background", True),
+        ("Alphabet", False),
+        ("moOse", False),
+        ("x", True),
+    ]
+)
+def test_is_isogram_cases(word, expected):
+    assert is_isogram(word) == expected
