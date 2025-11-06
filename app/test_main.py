bd905a27d6068f3553051a2c608639b578a47da5
Andrii Vasylenko <gods16garage@gmail.com>
2025-11-06 16:38:43 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,33 @@
 from app import main
 
 
-def test_is_isogram_true():
+def test_is_isogram_true() -> None:
     assert main.is_isogram("playgrounds") is True
 
 
-def test_is_isogram_false_with_repeated_letters():
+def test_is_isogram_false_with_repeated_letters() -> None:
     assert main.is_isogram("look") is False
 
 
-def test_is_isogram_case_insensitive():
+def test_is_isogram_case_insensitive() -> None:
     assert main.is_isogram("Adam") is False  # 'A' == 'a'
 
 
-def test_empty_string_is_isogram():
+def test_empty_string_is_isogram() -> None:
     assert main.is_isogram("") is True
 
 
-def test_is_isogram_with_consecutive_repeats():
+def test_is_isogram_with_consecutive_repeats() -> None:
     assert main.is_isogram("committee") is False
 
 
-def test_is_isogram_with_non_consecutive_repeats():
+def test_is_isogram_with_non_consecutive_repeats() -> None:
     assert main.is_isogram("alphabet") is False
 
 
-def test_is_isogram_single_letter():
+def test_is_isogram_single_letter() -> None:
     assert main.is_isogram("a") is True
 
 
-def test_is_isogram_mixed_case_is_same():
+def test_is_isogram_mixed_case_is_same() -> None:
     assert main.is_isogram("Dermatoglyphics") is True
