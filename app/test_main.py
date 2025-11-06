929b2b6d6f599f1f77401d4e58e76aac5385eb11
vadym-zahrebelnyi <zahrebelnyivadym@gmail.com>
2025-11-06 19:09:56 +0200

    Added rtype annotation

--- app/test_main.py
+++ app/test_main.py
@@ -15,5 +15,5 @@ from app.main import is_isogram
         ("GHOST", True)
     ]
 )
-def test_is_isogram(word: str, expected: bool):
+def test_is_isogram(word: str, expected: bool) -> None:
     assert is_isogram(word) == expected
