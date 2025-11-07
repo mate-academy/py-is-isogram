7a92f65d2e0fb3fc55235d99f6de98e4e717f4d6
artem <artemasarkisan598@gmail.com>
2025-11-07 19:21:46 +0200

    py-py-is-isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,19 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "isogram, excepts",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True)
+    ]
+)
+def test_is_isogram_text(isogram: str, excepts: bool) -> None:
+    assert is_isogram(isogram) == excepts
+
+    def test_error_is_isogram() -> None:
+        with pytest.raises(TypeError):
+            is_isogram(123)
