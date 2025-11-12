c02c9b057c1aed6a29250782a43e19bb560ea0b2
Andrii Chychkan <mywork1728@gmail>
2025-11-12 17:03:12 +0200

    added tests for isogram funtion

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result,exception",
+    [
+        ("playgrounds", True, None),
+        ("look", False, None),
+        ("Adam", False, None),
+        ("", True, None)
+    ],
+    ids=[
+        "'playgrounds is isogram'",
+        "'look' is not isogram",
+        "'Adam' with capitalize letter is not isogram",
+        "Empty string considers isogram"
+    ]
+)
+def test_is_isogram_function(word: str,
+                             result: bool,
+                             exception: Exception) -> None:
+    assert (
+        is_isogram(word) == result
+    ), f"{word} should be {result}"
