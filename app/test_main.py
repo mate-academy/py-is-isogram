ecc993f8e8c359df51d01797b9c7e1e1773e6d84
Nick <nikitosi3012@gmail.com>
2025-11-12 22:44:47 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_true() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_false() -> None:
+    assert is_isogram("test") is False
+
+
+def test_upper_case_false() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_empty_string() -> None:
+    assert is_isogram("") is True
