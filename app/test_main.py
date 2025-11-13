ff73a6b300a9249f669f3da9bd05cb664d9d2c22
Oleksii Shcherbak <qksardas001@mail.com>
2025-11-13 09:04:11 +0200

    py-is-isogram-fix-flake8

--- app/test_main.py
+++ app/test_main.py
@@ -2,16 +2,16 @@ from app.main import is_isogram
 
 
 def test_empty_string() -> None:
-    assert is_isogram('') is True
+    assert is_isogram("") is True
 
 
 def test_isogram_lowercase() -> None:
-    assert is_isogram('playgrounds') is True
+    assert is_isogram("playgrounds") is True
 
 
 def test_non_isogram_repeated_letters() -> None:
-    assert is_isogram('look') is False
+    assert is_isogram("look") is False
 
 
 def test_case_insensitive_check() -> None:
-    assert is_isogram('Adam') is False
+    assert is_isogram("Adam") is False
