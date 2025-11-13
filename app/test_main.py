dd6d6d286d6a386298a8f5dc92c950541f43a60e
Oleksii Shcherbak <qksardas001@mail.com>
2025-11-13 09:02:18 +0200

    py-is-isogram-solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string() -> None:
+    assert is_isogram('') is True
+
+
+def test_isogram_lowercase() -> None:
+    assert is_isogram('playgrounds') is True
+
+
+def test_non_isogram_repeated_letters() -> None:
+    assert is_isogram('look') is False
+
+
+def test_case_insensitive_check() -> None:
+    assert is_isogram('Adam') is False
