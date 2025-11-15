e41eee4051612ec4b09ee4bbb5962676ef587dd2
Tarasov Maksym <maksimmm45555@gmail.com>
2025-11-15 20:46:13 +0200

    solved py-is-isogram task

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,29 @@
+import pytest
+from typing import Any
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True)
+    ],
+)
+def test_is_isogram_function(word: str, expected: bool) -> None:
+    assert is_isogram(word) == expected
+
+
+@pytest.mark.parametrize(
+    "word",
+    [
+        1,
+        None,
+        (-2)
+    ],
+)
+def test_type_of_value(word: Any) -> None:
+    with pytest.raises(TypeError):
+        is_isogram(word)
