1426fe64f3b9e30a925f7fa0fb1cba0c8e6186bc
Buchak Tamara <Buchak.Tamara@gmail.com>
2025-11-19 22:59:33 +0200

    add solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+def test_should_return_bool() -> None:
+    assert (
+        isinstance(is_isogram("look"), bool) is True
+    ), "Function 'is_isogram' should return bool"
+
+
+@pytest.mark.parametrize(
+    "word, bool_result",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("isIsoGram", False),
+        ("", True)
+    ]
+)
+def test_coin_combination(word: str, bool_result: bool) -> None:
+    assert is_isogram(word) == bool_result, (
+        f"Function 'is_isogram' should return {bool_result} "
+        f"when value is {word}"
+    )
