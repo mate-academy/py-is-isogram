bcd39356dd27a8233dc8d8d0f7f88b32b16ac10b
Oleg <oleggavelya@gmail.com>
2025-11-20 17:43:03 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+class TestMain:
+    @pytest.mark.parametrize("word, result_bool", [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True)
+    ])
+    def test_should_return_word_without_repetitions(self, word: str, result_bool: bool) -> None:
+        assert is_isogram(word) == result_bool
+
+    def test_should_raise_type_error(self) -> None:
+        with pytest.raises(TypeError):
+            is_isogram(1)
