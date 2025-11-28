c3173210987990400ad56a9db2d62cb8edf769af
Ievgen German <german.kbrza@gmail.com>
2025-11-28 13:15:24 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+def get_data() -> list:
+    return [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+    ]
+
+
+def key_gen(get_data_key_gen: list) -> str:
+    word, expected = get_data_key_gen
+    return (f"If word is a {word}, "
+            f"result will {expected}")
+
+
+@pytest.mark.parametrize("get_data_test",
+                         get_data(),
+                         ids=key_gen)
+def test_app(get_data_test: list) -> None:
+    word, expected = get_data_test
+    assert is_isogram(word) == expected
