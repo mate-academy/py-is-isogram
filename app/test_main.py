56a9060bca983ef35e55b34080cbd7d9f177c664
Denys Herasymchuk <herasymchukdenyss@gmail.com>
2025-12-21 01:33:42 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,39 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+cases = [
+    ("a", True),
+    ("helo", True),
+    ("asdghjl", True),
+    ("ASDFGHJKL", True),
+    ("main", True),
+    ("ex", True),
+    ("", True),
+    ("playgrounds", True),
+    ("Aa", False),
+    ("aa", False),
+    ("look", False),
+    ("Adam", False),
+    ("txt", False),
+    ("test", False),
+    ("TesT", False),
+    ("readme", False),
+    ("MAMA", False),
+]
+
+
+@pytest.mark.parametrize(
+    "word,correct_result",
+    cases,
+    ids=[
+        f"{word} {"is" if res else "is not"} an isogram"
+        for word, res in cases
+    ]
+)
+def test_is_isogram_returns_correct_result(
+        word: str,
+        correct_result: bool
+) -> None:
+    assert is_isogram(word) == correct_result
