dbf80ce9e472fb8c83202ce7226f0c6bb57a8f9d
Danylo Moroz <danylomoroz1989@gmail.com>
2026-01-05 15:19:09 +0100

    solution_moroz_tests_is_isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,51 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+class TestIsIsogram:
+
+    @pytest.mark.parametrize(
+        "word,expected",
+        [
+            pytest.param(
+                "playgrounds",
+                True,
+                id="word is isogram",
+            ),
+            pytest.param(
+                "look",
+                False,
+                id="word is not isogram",
+            ),
+            pytest.param(
+                "Adam",
+                False,
+                id="word has bif and small the same letter is isogram",
+            ),
+            pytest.param(
+                "",
+                True,
+                id="word empty and is_isogram",
+            ),
+        ]
+    )
+    def test_is_isogram_correctly(self, word: str, expected: bool) -> None:
+        assert is_isogram(word) == expected
+
+    @pytest.mark.parametrize(
+        "word,expected_error",
+        [
+            pytest.param(
+                [],
+                AttributeError,
+                id="should raise error when arg is list",
+            )
+        ]
+    )
+    def test_should_raise_error_correctly(
+        self,
+        word: str,
+        expected_error: type[Exception]
+    ) -> None:
+        with pytest.raises(expected_error):
+            assert is_isogram(word)
