9e4dcbc2cefa6c1103796eb68c98aeeaf05653ba
Daniil Nosov <nosov281@gmail.com>
2026-01-20 18:54:33 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_empty_string() -> None:
+    assert is_isogram("") is True
+
+
+def test_non_empty_string() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_word_with_the_capital_letter() -> None:
+    assert is_isogram("Adam") is False
