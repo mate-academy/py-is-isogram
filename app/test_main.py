9ec68929ad4d1a64d9dfc0de2488e303225e7f56
Daniil Nosov <nosov281@gmail.com>
2026-01-20 18:58:50 +0200

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -11,3 +11,7 @@ def test_non_empty_string() -> None:
 
 def test_word_with_the_capital_letter() -> None:
     assert is_isogram("Adam") is False
+
+
+def test_not_consecutive_letters_on_the_isogram() -> None:
+    assert is_isogram("Word hello") is False
