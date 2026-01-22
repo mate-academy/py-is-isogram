20cbd879a798dbf60504c0a7dbe795d33153487a
Artur Bertin <arturbertin2006@gmail.com>
2026-01-21 23:25:31 -0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_is_not_isogram() -> None:
+    assert is_isogram("butterfly") is False
+
+
+def test_empty_string() -> None:
+    assert is_isogram("") is True
+
+
+def test_if_is_case_sensitive() -> None:
+    assert is_isogram("ButTerfly") is False
