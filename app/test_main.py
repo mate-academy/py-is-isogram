45ac5020e3b7f215e05ff9bf2935d66da59b3a3a
Andriy <andrijbuluk@gmail.com>
2026-01-23 20:13:07 +0100

    Is isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,32 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        pytest.param(
+            "playgrounds", True,
+            id="The test returns True because it is an isogram"
+        ),
+        pytest.param(
+            "look", False,
+            id="The test returns False because the text is not an isogram"
+        ),
+        pytest.param(
+            "Aa", False,
+            id="The test returns False because the text is not an isogram"
+        ),
+        pytest.param(
+            "aA", False,
+            id="The test returns False because the text is not an isogram"
+        ),
+        pytest.param(
+            "", True,
+            id="Test returns True because is an isogram (empty string)"
+        )
+    ]
+
+)
+def test_is_isogram(word, result):
+    assert is_isogram(word) is result
