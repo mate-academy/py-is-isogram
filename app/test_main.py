9ddf358eb6dc2797a311a81ffbc72440ede16bcc
Andriy <andrijbuluk@gmail.com>
2026-01-23 20:34:38 +0100

    Is isogram Fix mentor test

--- app/test_main.py
+++ app/test_main.py
@@ -3,30 +3,31 @@ import pytest
 
 
 @pytest.mark.parametrize(
-    "word,result",
+    "word_lower,result",
     [
         pytest.param(
             "playgrounds", True,
             id="The test returns True because it is an isogram"
         ),
         pytest.param(
+            "", True,
+            id="The test returns True because it is an isogram"
+        ),
+        pytest.param(
+            "MM", False,
+            id="The test returns False because the text is not an isogram"
+        ),
+        pytest.param(
             "look", False,
             id="The test returns False because the text is not an isogram"
         ),
         pytest.param(
-            "Aa", False,
+            "test", False,
             id="The test returns False because the text is not an isogram"
-        ),
-        pytest.param(
-            "aA", False,
-            id="The test returns False because the text is not an isogram"
-        ),
-        pytest.param(
-            "", True,
-            id="Test returns True because is an isogram (empty string)"
         )
+
     ]
 
 )
-def test_is_isogram(word: str, result: bool) -> None:
-    assert is_isogram(word) is result
+def test_is_isogram(word_lower: str, result: bool) -> None:
+    assert is_isogram(word_lower) == result
