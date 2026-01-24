3160e261c8f2ac5ab8a04481b7537eb47e1cd5ac
dima.yeromin <dima.yeromin@gmail.com>
2026-01-24 16:53:31 +0100

    dome

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,24 @@
-from app.main import is_isogram
+import pytest
 
-# write your code here
+from app import main
+
+
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        ("playgrounds", True),
+        ("isogram", True),
+        ("look", False),
+        ("aba", False),
+    ],
+)
+def test_is_isogram(word: str, expected: bool) -> None:
+    assert main.is_isogram(word) is expected
+
+
+def test_is_isogram_is_case_insensitive() -> None:
+    assert main.is_isogram("Adam") is False
+
+
+def test_empty_string_is_isogram() -> None:
+    assert main.is_isogram("") is True
