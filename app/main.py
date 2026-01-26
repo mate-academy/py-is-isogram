95718a9d0e2fd7b3773e8bec3058f06caf470626
titoiriepal <titoiriepal@hotmail.com>
2026-01-26 12:23:31 +0100

    First solution with tests

--- app/main.py
+++ app/main.py
@@ -1,4 +1,12 @@
 def is_isogram(word: str) -> bool:
+    if word == "":
+        return True
+    if type(word) is not str:
+        raise TypeError
+
+    if not word.isalpha():
+        raise ValueError
+
     word_lower = word.lower()
     for letter in word_lower:
         if word_lower.count(letter) > 1:
--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,69 @@
-from app.main import is_isogram
+import pytest
+from app import main
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word",
+    [
+        (6),
+        (["Hello"]),
+        (("word", "Hello")),
+        (3.45),
+        (True),
+        ({"word": "Hello"}),
+    ],
+)
+def test_data_input_is_a_string(
+    word: str,
+) -> None:
+    with pytest.raises(TypeError):
+        main.is_isogram(word)
+
+
+@pytest.mark.parametrize(
+    "word",
+    [
+        "345",
+        "h3llo",
+        "Hello World",
+        "patet¡c",
+        "$urprise",
+    ],
+)
+def test_word_must_to_be_alpha(
+    word: str,
+) -> None:
+    with pytest.raises(ValueError):
+        main.is_isogram(word)
+
+
+@pytest.mark.parametrize(
+    "word",
+    [
+        "Thirty",
+        "hello",
+        "HelloWorld",
+        "patetic",
+        "surprise",
+    ],
+)
+def test_function_return_must_to_be_bool(word: str) -> None:
+    response = main.is_isogram(word)
+    assert type(response) is bool, f"Return {response} must to be bool"
+
+
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        ("Thirty", False),
+        ("hello", False),
+        ("open", True),
+        ("close", True),
+        ("", True),
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+    ],
+)
+def test_function_is_isogram(word: str, result: bool) -> None:
+    assert main.is_isogram(word) == result, f"Word {word} must return {result}"
