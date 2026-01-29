9ffa55faf1dd076fa4b98c696c3380793ee8de42
Redum9 <dumaneckijdenis@gmail.com>
2026-01-29 18:13:41 +0200

    Resolve it again

--- app/test_main.py
+++ app/test_main.py
@@ -2,9 +2,6 @@ import pytest
 from app.main import is_isogram
 
 
-@pytest.mark.parametrize("line, expected_answer",
-        [
-            ["", True]
-        ])
-def test_line_len(line: str, expected_answer: True) -> None:
-    assert is_isogram(line) == True
+@pytest.mark.parametrize("line", [""])
+def test_line_len(line: str) -> None:
+    assert is_isogram(line)
