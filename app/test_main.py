21635d361e31d0b76d3fa3860063a0678a9eac1e
Mafialate <mafialate@ukr.net>
2026-02-02 10:16:52 +0100

    add tests for is_isogram function

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,28 @@
 from app.main import is_isogram
 
-# write your code here
+import pytest
+
+
+@pytest.mark.parametrize(
+    "word,expected",
+    [
+        ("12345", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+        ("Andrey", True),
+        ("f-!d", True),
+        (" ", True)
+    ],
+    ids=[
+        "test_returns_true_when_word_is_12345",
+        "test_returns_false_when_word_is_look",
+        "test_returns_false_when_word_is_Adam",
+        "test_returns_true_when_word_is_empty",
+        "test_returns_true_when_word_is_Andrey",
+        "test_returns_true_when_word_is_f-!d",
+        "test_returns_true_when_word_is_space",
+    ]
+)
+def test_is_isogram(word: str, expected: bool) -> None:
+    assert is_isogram(word) == expected
