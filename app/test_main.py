8dd887ef3715dcc205d9991d354ee91dc6950af9
Karolina Latko <caroline15@poczta.fm>
2026-02-03 20:17:45 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,33 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_isogram_true() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_isogram_false_with_repeats() -> None:
+    assert is_isogram("look") is False
+
+
+def test_isogram_false_case_insensitive() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_isogram_empty_string() -> None:
+    assert is_isogram("") is True
+
+
+def test_isogram_single_letter() -> None:
+    assert is_isogram("A") is True
+
+
+def test_isogram_long_valid_word() -> None:
+    assert is_isogram("background") is True
+
+
+def test_isogram_with_multiple_repeats() -> None:
+    assert is_isogram("banana") is False
+
+
+def test_isogram_all_unique_letters() -> None:
+    assert is_isogram("subdermatoglyphic") is True
