e4aa6feaf91e5707667e877c4ca14011a5ac8987
Danylo <danil.bogomazov1356@gmail.com>
2026-02-12 14:16:59 +0200

    Create tests fo is_isogram function

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
+# flake8: noqa: *
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,expected", [
+        pytest.param("playgrounds", True, id="Default word in lowercase"),
+        pytest.param("look", False, id="Word in lowercase with repeating letters"),
+        pytest.param("Adam", False, id="Word in different cases with repeating letters"),
+        pytest.param("", True, id="Blank word"),
+    ]
+)
+def test_logic_is_isogram(word, expected):
+    assert is_isogram(word) == expected
