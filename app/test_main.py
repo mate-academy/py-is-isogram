565ae646e3eb0da120d6693b4731f4d7ecad0ce9
noshenko98 <noshenko98@gmail.com>
2026-02-18 17:24:28 +0200

    Soluton

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,20 @@
 from app.main import is_isogram
 
-# write your code here
+import pytest
+
+
+@pytest.mark.parametrize(
+    "word,expected",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True)
+    ]
+)
+def test_is_isogram_from_date(word: str, expected: bool) -> None:
+    assert is_isogram(word) == expected
+
+
+def test_is_isogram_type() -> None:
+    assert type(is_isogram("test")) == bool
