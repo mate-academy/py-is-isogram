cbb72b845eebcff1933d8d62eaf0eebd9a5d25b3
Ivan <ivanqqq@ukr.net>
2026-02-18 14:40:11 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,40 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+class TestIsIsogram:
+    @pytest.mark.parametrize(
+        "word,expected",
+        [
+            ("", True),
+            ("a", True),
+            ("ab", True),
+            ("aa", False),
+            ("A", True),
+            ("Aa", False),
+            ("aA", False),
+            ("aaaa", False),
+            ("playgrounds", True),
+            ("look", False),
+            ("Adam", False),
+            ("1", True),
+            ("11", False)
+        ],
+        ids=[
+            "empty string",
+            "a letter",
+            "two different letters",
+            "two same letters",
+            "a high register letter",
+            "the same letter in high and low register",
+            "the same letter in low and high register",
+            "string of 4 same letters",
+            "string of 4 different letters",
+            "word with 2 same letters",
+            "word with 2 same letters in high and low register",
+            "number",
+            "digit with 2 same numbers"
+        ]
+    )
+    def test_is_isogram(self, word: str, expected: bool) -> None:
+        assert is_isogram(word) == expected
