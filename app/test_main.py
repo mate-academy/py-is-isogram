b6394f67e11860dad6f9801d53a032c213cdeb6d
unknown <kgoncharenko83@gmail.com>
2026-02-19 13:09:16 +0100

    Added tests for func is_isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,29 @@
-from app.main import is_isogram
+import pytest
+from app import main
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,expected",
+    [
+        ("playgrounds", True),
+        ("adam", False),
+        ("", True),
+        ("a", True),
+        ("isogram", True),
+        ("Cheese", False),
+        ("Shelf", True),
+        ("Aa", False),
+    ],
+    ids=[
+        "uniq",
+        "repeat",
+        "empty string",
+        "single letter",
+        "classic isogram",
+        "case insensitive, repeat",
+        "case insensitive, uniq",
+        "case insensitive, two letters, repeat",
+    ]
+)
+def test_is_isogram(word: str, expected: bool) -> None:
+    assert main.is_isogram(word) == expected
