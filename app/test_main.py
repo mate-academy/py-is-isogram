d841f61864667f341d610d1932401ca6ff09736a
gelexxi <schew4uk98@gmail.com>
2026-03-01 12:19:13 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, expected_result",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+
+
+    ]
+)
+def test_is_isogram_various_thresholds(
+        word: int,
+        expected_result: bool) -> None:
+    assert (
+           is_isogram(word) == expected_result)\
+        , f"Should return {expected_result} for word {word})"
