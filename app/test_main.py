36f02cb93ad946da6bfc2428dbb16564c8eaf941
Serhii Hryniuk <serhiibhryniuk@gmail.com>
2026-03-02 12:06:12 +0200

    Written tests for function

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,expected",
+    [
+        ("", True),
+        ("TrUe", True),
+        ("lOok", False),
+        ("Adam", False),
+    ], ids=[
+        "empty string should be True",
+        "should be true if contains upper not repeated letters",
+        "should be false if contains the same consecutive letters",
+        "should be false if contains the same non-consecutive letters"
+    ]
+)
+def test_should_return_proper_isogram_check(word: str, expected: bool) -> None:
+    assert is_isogram(word) == expected
