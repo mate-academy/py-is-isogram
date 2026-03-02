b6e4d6df24f449d30ae1f0a4a23bcebb2d23c9a6
Your NameMykola <mykola.osolinskyi@gmail.com>
2026-03-02 15:39:17 +0200

    all tests passed

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,19 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        ("", True),
+        ("Car", True),
+        ("DEep", False)
+    ], ids=[
+        "Empty string should be an isogram",
+        "Isogram word should return true",
+        "Should be case insensitive"
+    ]
+)
+def test_is_isogram(word: str, expected: bool) -> None:
+    assert is_isogram(word) == expected
