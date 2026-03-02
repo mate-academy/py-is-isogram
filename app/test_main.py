6f996743ce5d8978c1ea1a5f6e2940c1948f895a
Your NameMykola <mykola.osolinskyi@gmail.com>
2026-03-02 15:43:19 +0200

    add test case for non consecutive letters

--- app/test_main.py
+++ app/test_main.py
@@ -8,11 +8,13 @@ from app.main import is_isogram
     [
         ("", True),
         ("Car", True),
-        ("DEep", False)
+        ("DEep", False),
+        ("Zhytomyr", False)
     ], ids=[
         "Empty string should be an isogram",
         "Isogram word should return true",
-        "Should be case insensitive"
+        "Should be case insensitive",
+        "test non_consecutive_letters_are_not_isogram"
     ]
 )
 def test_is_isogram(word: str, expected: bool) -> None:
