d49b72c6c15155a6596ed4e44d2255c3fd39ac56
Eduardo Atae <e.atae05@gmail.com>
2026-03-07 21:11:12 -0300

    add parametrized tests for is_isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,22 @@
-from app.main import is_isogram
+import pytest
+from app import main
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,expected",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("Dermatoglyphics", True),
+        ("isogram", True),
+        ("moose", False),
+        ("aba", False),
+        ("abc", True),
+        ("", True),
+        ("a", True),
+        ("Aa", False),
+    ],
+)
+def test_is_isogram(word: str, expected: bool) -> None:
+    assert main.is_isogram(word) is expected
