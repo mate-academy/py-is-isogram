ddb826ce9286f94f7b3f4921f25e5889713810d3
Anton Bublykov <mw.anton2000@gmail.com>
2026-03-09 17:30:52 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,35 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "initial, expected",
+    [
+        pytest.param(
+            "", True,
+            id="return True because empty string is isogram",
+        ),
+        pytest.param(
+            "background", True,
+            id="return True because background is isogram",
+        ),
+        pytest.param(
+            "Memory", False,
+            id="return False because function is case-insensitive",
+        ),
+        pytest.param(
+            "abcab", False,
+            id="return False because abcab in not isogram"
+        ),
+        pytest.param(
+            "look", False,
+            id="return False because look in not isogram",
+        ),
+        pytest.param(
+            "radar", False,
+            id="return False because radar in not isogram",
+        )
+    ]
+)
+def test_return_is_isogram(initial: str, expected: bool) -> None:
+    assert is_isogram(initial) == expected
