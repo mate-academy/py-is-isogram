303c7d1dc36bf40d89a9ca0c05abc22a303a26bb
Lumos <tsykalanovdima@gmail.com>
2026-03-11 17:55:57 +0000

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_isogram_returns_true_for_unique_letters():
+    assert is_isogram("playgrounds") is True
+
+
+def test_isogram_returns_false_for_consecutive_duplicates():
+    assert is_isogram("look") is False
+
+
+def test_isogram_returns_false_for_non_consecutive_duplicates():
+    assert is_isogram("alphabet") is False
+
+
+def test_isogram_is_case_insensitive():
+    assert is_isogram("Adam") is False
+
+
+def test_empty_string_is_isogram():
+    assert is_isogram("") is True
