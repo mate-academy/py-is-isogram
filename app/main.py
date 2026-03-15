5b920f93ff88d3ac2decc1907580848fb8bd170b
Anastasiia Velikaya <a.v.zaychenko@gmail.com>
2026-03-15 22:00:55 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
 def is_isogram(word: str) -> bool:
-    word_lower = word.lower()
-    for letter in word_lower:
-        if word_lower.count(letter) > 1:
-            return False
-    return True
+    if not isinstance(word, str):
+        raise TypeError("Input must be a string")
+    letters = [char.lower() for char in word if char.isalpha()]
+    return len(letters) == len(set(letters))
