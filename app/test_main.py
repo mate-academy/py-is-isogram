4a1491f2136e3bdd0bc65bb04f14748f04842043
Nikita <nikita.polischukov@gmail.com>
2026-03-26 20:58:58 +0100

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -5,10 +5,10 @@ from app.main import is_isogram
 @pytest.mark.parametrize(
     "word, expected",
     [
-        ('playgrounds', True),
-        ('look', False),
-        ('Adam', False),
-        ('', True),
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
         ("abcABC", False)
     ],
 )
