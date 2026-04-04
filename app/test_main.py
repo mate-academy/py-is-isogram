7529bce6e58bcbcb553ca821530128ce64b05af9
pawunder1992 <pavelkryvutsa@gmail.com>
2026-04-04 13:09:49 +0300

    solved

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize("word, result",
+                         [
+                             ("Abba", False),
+                             ("", True),
+                             (1, AttributeError),
+                             ("playgrounds", True),
+                             ("Adam", False)
+                         ],
+                         ids=[
+                             "abba must return False",
+                             "empty string must be True",
+                             "if must return TypeError",
+                             "playgrounds must return True",
+                             "non consecutive letters are not isogram"
+                         ]
+                         )
+def test_is_isogram(word: str, result: bool | TypeError) -> None:
+    if isinstance(result, type) and issubclass(result, Exception):
+        with pytest.raises(result):
+            is_isogram(word)
+    else:
+        assert is_isogram(word) == result
