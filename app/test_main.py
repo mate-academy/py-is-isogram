524373b3387dbb4a64e9bf14536c26f7aa392a32
Anton Gaievyi <anton.gaievyi@gmail.com>
2026-04-05 22:56:18 +0200

    made test for py-is-isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,expected",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
+        ("Alphabet", False),
+        ("isogram", True),
+        ("thumbscrewjapingly", True),
+        ("aba", False),
+        ("moOse", False),
+    ]
+)
+def test_is_isogram(word: str, expected: bool) -> None:
+    assert is_isogram(word) is expected
+
+
+def test_is_isogram_case_insensitivity() -> None:
+    assert is_isogram("AbC") is True
+    assert is_isogram("Aa") is False
