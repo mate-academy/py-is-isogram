4f661a48f0e1af3a6d00fbd0d3c65859e266906a
Nik <nikita.pitushkin31@mail.com>
2026-04-06 18:58:17 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,30 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        pytest.param(
+            "playgrounds",
+            True,
+            id= "test playgrounds"
+        ),
+        pytest.param(
+            "look",
+            False,
+            id="test look"
+        ),
+        pytest.param(
+            "Adam",
+            False,
+            id="test Adam"
+        ),
+        pytest.param(
+            "",
+            True,
+            id="test empty string"
+        )
+    ]
+)
+def test_on_no_repeating_letters(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
