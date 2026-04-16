88963f2c938686663b90c0d888af29c098fd7f28
elija-xp <eyelja@gmail.com>
2026-04-17 01:43:40 +0300

    implimenting test_main

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_playgrounds_is_true() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_look_is_false() -> None:
+    assert is_isogram("look") is False
+
+
+def test_adam_is_false() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_empty_string() -> None:
+    assert is_isogram("") is True
+
+
+def test_isogram_long_word() -> None:
+    assert is_isogram("alphabet") is False
+
+
+def test_is_isogram_is_false() -> None:
+    assert is_isogram("is_isogram") is False
