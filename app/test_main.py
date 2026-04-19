c06116971b2bc0b2feefb8ca9c9a9ad31ef41ae4
RustamHadoiev24 <gadoeva155@gmail.com>
2026-04-19 19:21:23 +0300

    solution: implement is_isogram and add tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram_should_return_true_for_isogram_word() -> None:
+    assert is_isogram("Dermatoglyphics") is True
+    assert is_isogram("isogram") is True
+
+
+def test_is_isogram_should_return_false_for_repeating_letters() -> None:
+    assert is_isogram("aba") is False
+    assert is_isogram("moose") is False
+
+
+def test_is_isogram_should_be_case_insensitive() -> None:
+    # Важливо: 'm' та 'M' мають вважатися однаковою літерою
+    assert is_isogram("moOse") is False
+    assert is_isogram("isIsogram") is False
+
+
+def test_is_isogram_should_return_true_for_empty_string() -> None:
+    assert is_isogram("") is True
