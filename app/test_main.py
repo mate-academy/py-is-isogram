6ddca840949ce8aa589a3523b6672bab5e9dc13f
David Ivan <davidivan6900@gmail.com>
2026-04-20 12:03:13 +0700

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -16,18 +16,3 @@ def test_result_correctness(word: str,
     assert (
         is_isogram(word) == result
     ), f"Word: '{word}' should be: {result}"
-
-
-@pytest.mark.parametrize(
-    "word,result",
-    [
-        ("play2times", False),
-        ("look at you", False),
-        ("45354", False)
-    ]
-)
-def test_only_letters(word: str,
-                      result: bool) -> None:
-    assert (
-        is_isogram(word) == result
-    ), f"Word: '{word}' should contain only letters"
