aa5a68f16a170c98cb99b8bdb0ce169b00707558
Igor <zalugin.iv2@gmail.com>
2026-04-23 17:24:19 +0200

    all tasks completed

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_no_repeating_letters() -> None:
+    assert is_isogram("nightmare") == True
+
+
+def test_repeating_letters() -> None:
+    assert is_isogram("boss") == False
+
+
+def test_no_letters() -> None:
+    assert is_isogram("") == True
+
+
+def test_consecutive_word() -> None:
+    assert is_isogram("SOS") == False
+
+
+def test_lower_and_upper_case() -> None:
+    assert is_isogram("preSs") == False
