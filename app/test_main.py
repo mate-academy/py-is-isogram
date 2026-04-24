17116c1963bbf7c02810d3b89f289ff1562072e3
Daniil Baianov <bayanovdanil666@gmail>
2026-04-24 18:22:25 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_should_return_true_for_empty_string() -> None:
+    assert is_isogram("") is True
+
+
+def test_should_return_true_for_isogram_string() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_should_return_false_for_same_upper_lover_letters() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_should_return_false_for_two_same_letters() -> None:
+    assert is_isogram("look") is False
