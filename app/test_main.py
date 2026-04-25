01731eb47d59f622e73f7fdb4bc8ef06a0a4e023
Tymur <mr.kanis82@gmail.com>
2026-04-25 16:00:46 -0400

    done

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,40 @@
 from app.main import is_isogram
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,expected",
+    [
+        ("", True),
+        ("isogram", True),
+        ("sixyearold", True),
+
+        ("alphabet", False),
+        ("programming", False),
+
+        ("Dermatoglyphics", True),
+        ("abA", False),
+        ("moOse", False), ]
+)
+def test_is_isogram(word: str, expected: bool) -> None:
+    assert is_isogram(word) == expected
+
+
+@pytest.mark.parametrize(
+    "word",
+    [
+        None,
+        123,
+        ["abc"],
+        {"a": 1},
+    ],
+    ids=[
+        "none",
+        "int",
+        "list",
+        "dict",
+    ]
+)
+def test_is_isogram_invalid_types(word: str) -> None:
+    with pytest.raises(TypeError):
+        is_isogram(word)
