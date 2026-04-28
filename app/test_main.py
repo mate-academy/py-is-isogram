aaf65a035a4deb76b0e92ee7eb313fa8a1b05ea7
artemkliuienkov <artemkluenkov41@gmail.com>
2026-04-28 21:35:48 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram_for_regular_word() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_is_isogram_for_word_with_consecutive_duplicates() -> None:
+    assert is_isogram("look") is False
+
+
+def test_is_isogram_for_word_with_non_consecutive_duplicates() -> None:
+    assert is_isogram("apple") is False
+
+
+def test_is_isogram_is_case_insensitive() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_is_isogram_for_empty_string() -> None:
+    assert is_isogram("") is True
