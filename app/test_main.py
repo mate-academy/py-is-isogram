314621e73d7115b27cf57db31908e421c219a574
Nintendiuk <oleksii.rachynskyi@gmail.com>
2026-04-30 19:33:57 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,33 @@
-from app.main import is_isogram
+import pytest
+from app import main
 
-# write your code here
+
+@pytest.mark.parametrize("string,expected", [
+    ("playgrounds", True),
+    ("look", False),
+    ("Adam", False),
+    ("Aa", False),
+    ("", True),
+    ("ABCD", True),
+    ("EFGH", True),
+    ("IJKL", True),
+    ("MNOP", True),
+    ("QRSTU", True),
+    ("VWXYZ", True)
+])
+def test_is_isogram(string: str, expected: str) -> str:
+    assert main.is_isogram(string) is expected
+
+
+@pytest.mark.parametrize(
+    "invalid_input",
+    [
+        15,
+        1.5,
+        None,
+        ["a", "b", "c"],
+    ]
+)
+def test_is_isogram_invalid_types(invalid_input: str) -> None:
+    with pytest.raises((TypeError, ValueError, AttributeError)):
+        main.is_isogram(invalid_input)
