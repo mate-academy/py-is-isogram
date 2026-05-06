535c55efea43eb7af2fbd24a3edc8cfc8baa6f94
Raffaele Maccarrone <raffaele.alves.maccarrone@gmail.com>
2026-05-06 16:06:01 -0300

    Solução

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram1() -> None:
+    assert is_isogram("Hello") is False, "Should return False"
+
+
+def test_is_isogram2() -> None:
+    assert is_isogram("") is True, "Should return True"
+
+
+def test_is_isogram3() -> None:
+    assert is_isogram("Playgrounds") is True, "Should return True"
+
+
+def test_is_isogram4() -> None:
+    assert is_isogram("Amoeba") is False, "Should return False"
