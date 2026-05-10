c8235c30cb0bb87057ef0ee5e8a54cf109b7598f
Petro Kravchenko <petros5325@gmail.com>
2026-05-10 15:56:42 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,59 @@
-from app.main import is_isogram
+import pytest
+from typing import Any
+from app import main
 
-# write your code here
+class TestIsIsogramFunction:
+    @pytest.mark.parametrize(
+        "word, bool_value",
+        [
+            pytest.param(
+                "",
+                True,
+                id="test_empty_string"
+            ),
+            pytest.param(
+                "pLayGrounDs",
+                True,
+                id="test_different_regist_in_word_isogram"
+            ),
+            pytest.param(
+                "orange",
+                True,
+                id="test_word_lowercase_is_isogram"
+            ),
+            pytest.param(
+                "GOD",
+                True,
+                id="test_word_is_isogram_upper_case"
+            ),
+            pytest.param(
+                "Adam",
+                False,
+                id="test_word_is_not_isogram_lowercase"
+            ),
+            pytest.param(
+                "APPLE",
+                False,
+                id="test_word_uppercase_is_not_isogram"
+            ),
+            pytest.param(
+                "BoOk",
+                False,
+                id="test_word_is_not_isogram_different_case"
+            )
+        ]
+    )
+    def test_is_isogram_correctly(self, word: str, bool_value: bool)  -> None:
+        assert main.is_isogram(word) == bool_value
+
+    @pytest.mark.parametrize(
+        "invalid_input",
+        [
+            124,
+            ["s", "t", "r"],
+            None
+        ]
+    )
+    def test_is_isogram_invalid_types(self, invalid_input: Any) -> None:
+        with pytest.raises(Exception):
+            main.is_isogram(invalid_input)
