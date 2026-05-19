cc634a8333324256c8535aa6dec71bad450b9f53
Dima-shima <shifrin1970@gmail.com>
2026-05-19 19:24:19 +0300

    implemented tests

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
 def is_isogram(word: str) -> bool:
+    if not isinstance(word, str):
+        raise TypeError("Argument must be string")
+    if not word.isalpha() and word != "":
+        raise TypeError("Argument must be string with only letter")
     word_lower = word.lower()
     for letter in word_lower:
         if word_lower.count(letter) > 1:
