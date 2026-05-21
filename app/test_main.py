ca4100e69dfae80aab8da76b273873a9623fe825
Mykyta Titar <mykyta.titar@gmail.com>
2026-05-21 12:24:27 +0300

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,19 @@
 from app.main import is_isogram
 
+
 def test_is_isogram() -> None:
-    assert is_isogram('playgrounds') is True
-    assert is_isogram('look') is False
-    assert is_isogram('Adam') is False
+    assert is_isogram("playgrounds") is True
+    assert is_isogram("look") is False
+    assert is_isogram("Adam") is False
 
 
 def test_empty_word() -> None:
-    assert is_isogram('') is True
+    assert is_isogram("") is True
+
 
 def test_single_char() -> None:
-    assert is_isogram('a') is True
+    assert is_isogram("a") is True
 
 
 def test_all_same() -> None:
-    assert is_isogram('aaa') is False
+    assert is_isogram("aaa") is False
