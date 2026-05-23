64e9105f7fdfb64513baaabde4699bb66c1ef11d
Alex <alex@example.com>
2026-05-23 10:57:05 +0300

    finish 2

--- app/test_main.py
+++ app/test_main.py
@@ -3,13 +3,13 @@ from app.main import is_isogram
 
 
 @pytest.mark.parametrize(
-   "string,result",
-   [
-       ("playgrounds", True),
-       ("look", False),
-       ("Adam", False),
-       ("", True)
-   ]
+    "string,result",
+    [
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True)
+    ]
 )
 def test_isogram(string: str, result: bool) -> bool:
     assert is_isogram(string) == result
