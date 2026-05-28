51b601cea475babe59154b3ece4d5082da1557c2
Łukasz Kaczmarek <Lukasz.kaczmarek1993@wp.pl>
2026-05-28 13:50:21 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,8 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram() -> None:
+    assert is_isogram("playground")
+    assert not is_isogram("Look")
+    assert not is_isogram("Adam")
+    assert is_isogram("")
