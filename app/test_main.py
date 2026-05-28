5278dd09ce338198cf910a5f4f92d8527da1c7e4
Nikita Ivanichenko <importantthings1482@gmail.com>
2026-05-28 18:42:52 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,35 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,result",
+    [
+        ("a", True),
+        ("word", True),
+        ("kraken", False),
+        ("WoRd", True),
+        ("WATER", True),
+        ("", True),
+        (" ", True),
+        ("Jake", True),
+        ("Anna", False),
+        ("cupcake", False),
+        ("haPpy", False),
+    ],
+    ids=[
+        "Is word `a` an isogram",
+        "Is word `word` an isogram",
+        "Is word `kraken` an isogram",
+        "Is word `WoRd` an isogram",
+        "Is word `WATER` an isogram",
+        "Is empty string an isogram",
+        "Is space and isogram",
+        "Is word `Jake` an isogram",
+        "Is word `Anna` an isogram",
+        "Is word `cupcake` an isogram",
+        "Is word `haPpy` an isogram",
+    ]
+)
+def test_is_isogram(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
