ff5037de4bdeed64dfcf919e4fda72bf5e7e8375
GitJaouad <jlaourayed@gmail.com>
2026-05-29 22:50:16 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,10 @@
 def is_isogram(word: str) -> bool:
+
+    if any(char.isdigit() for char in word):
+        raise TypeError
+
     word_lower = word.lower()
+
     for letter in word_lower:
         if word_lower.count(letter) > 1:
             return False
