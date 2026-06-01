85d3a70fc9dc144972b20c2c43dabdc68c43962a
Arlan Neres <arlanneresdossantos@gmail.com>
2026-05-31 21:53:56 -0300

    solution 3

--- app/test_main.py
+++ app/test_main.py
@@ -1,30 +1,13 @@
 import pytest
 from app.main import is_isogram
 
+
 @pytest.mark.parametrize(
-    "word, expected",
-    [
-        ("", True),
+    "word,result", [
         ("playgrounds", True),
         ("look", False),
         ("Adam", False),
-        ("isogram", True),
-        ("Alphabet", False),
-        ("A", True),
-        ("aa", False),
-    ],
-    ids=[
-        "empty_string",
-        "valid_lowercase_isogram",
-        "consecutive_repeats",
-        "case_insensitive_repeats",
-        "valid_longer_isogram",
-        "non_consecutive_case_insensitive",
-        "single_letter",
-        "smallest_non_isogram"
-    ]
+        ("", True)]
 )
-def test_is_isogram_standard_cases(word: str | int , expected: bool) -> None:
-    result = is_isogram(word)
-    assert result is expected
-    assert isinstance(result, bool)
+def test_is_isogram(word: str, result: bool) -> None:
+    assert is_isogram(word) == result
