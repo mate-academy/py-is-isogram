e009e600e8cb3929dacbf0d50ce1a44c5ba9a46e
Petro <petropavliuk12.05@gmail.com>
2026-06-05 17:07:58 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,22 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, result",
+    [
+        ("sublime", True),
+        ("Alpha", False),
+        ("init", False),
+        ("locksmith", True),
+        ("hyperlink", True),
+        ("complainer", True),
+        ("Router", False),
+        ("look", False),
+        ("", True)
+    ]
+)
+def test_function_finds_isograms_correctly(
+    word: str, result: bool
+) -> None:
+    assert (is_isogram(word)) == result
