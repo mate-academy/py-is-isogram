450ab0e65f06cf6065e7ac7ca01e898a9a682ca8
Kostiantyn <goncharov.kostiantyn@gmail.com>
2026-06-13 17:26:14 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize("word, expected", [
+    pytest.param(
+        "playgrounds", True,
+        id="should return True when is isogram"
+    ),
+    pytest.param(
+        "look", False,
+        id="should return False because of two 'o'"
+    ),
+    pytest.param(
+        "Adam", False,
+        id="should return False because of two 'a' in different register"
+    ),
+    pytest.param(
+        "", True,
+        id="should return True for empty string"
+    ),
+])
+def test_isogram(word: str, expected: bool) -> None:
+    assert is_isogram(word) == expected
