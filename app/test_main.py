75c45404d7a14f77ba4685a75b24484649dbee8b
Dr-Kate <katia.01.01.2007d@gmail.com>
2026-06-17 17:08:58 +0300

    isogram-tests-task-done

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,27 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, bool", [
+        ("", True),
+        ("Anna", False),
+        ("Sam", True),
+        ("eveline", False),
+        ("sam", True)
+    ]
+)
+def test_words_for_an_isogram(word: str, bool: bool) -> None:
+    assert is_isogram(word) == bool
+
+
+def test_consecutive_letters_are_not_isogram() -> None:
+    assert not is_isogram("river")
+
+
+def test_non_consecutive_letters_are_not_isogram() -> None:
+    assert not is_isogram("abbc")
+
+
+def tests_isogram_is_case_insensitive() -> None:
+    assert not is_isogram("Adam")
