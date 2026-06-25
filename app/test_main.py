74c30bebdf0d4797c65dee6a594468b7733526dc
Andrii Deneha <deneha.andrii@gmail.com>
2026-06-25 15:44:54 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,14 @@
 from app.main import is_isogram
 
-# write your code here
+def test_empty_string() -> bool:
+    assert is_isogram('') is True
+
+def test_word_with_unique_letters() -> bool:
+    assert is_isogram('abcd') is True
+
+def test_word_with_repeated_letters() -> bool:
+    assert is_isogram("aba") is False
+
+
+def test_case_insensitive() -> bool:
+    assert is_isogram("Aa") is False
