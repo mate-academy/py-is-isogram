890297f81cd8c07064688e39c47b47c7f8095c28
Andrii Deneha <deneha.andrii@gmail.com>
2026-06-25 15:57:18 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -2,16 +2,21 @@ from app.main import is_isogram
 
 
 def test_isogram_is_case_insensitive() -> None:
-    assert is_isogram("Dermatoglyphics") is True, "String with different cases of the same letter is not an isogram."
+    assert is_isogram("Dermatoglyphics") is True,\
+        ("String with different cases"
+         " of the same letter is not an isogram.")
 
 
 def test_empty_string_is_isogram() -> None:
-    assert is_isogram("") is True, "Empty string is an isogram."
+    assert is_isogram("") is True,\
+        "Empty string is an isogram."
 
 
 def test_non_consecutive_letters_are_not_isogram() -> None:
-    assert is_isogram("aba") is False, "Not only consecutive letters are not an isogram."
+    assert is_isogram("aba") is False,\
+        "Not only consecutive letters are not an isogram."
 
 
 def test_consecutive_letters_are_not_isogram() -> None:
-    assert is_isogram("aa") is False, "Not only non-consecutive letters are not an isogram."
+    assert is_isogram("aa") is False,\
+        "Not only non-consecutive letters are not an isogram."
