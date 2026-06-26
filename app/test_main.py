0ca42ec2d22eb1c8588f6dfcda4e9315fbac7f66
Nazar Vovchuk <nazarvovchuc3002@gmail.com>
2026-06-26 22:31:11 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,29 @@
-from app.main import is_isogram
+import pytest
+from app import main
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        ("isogram", True),
+        ("moon", False),
+        ("background", False),
+    ],
+)
+def test_is_isogram_with_words(word, expected):
+    assert main.is_isogram(word) is expected
+
+
+def test_is_isogram_with_empty_string():
+    assert main.is_isogram("") is True
+
+
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        ("Mm", False),
+        ("ISOGRAM", True),
+    ],
+)
+def test_is_isogram_with_uppercase_letters(word, expected):
+    assert main.is_isogram(word) is expected
