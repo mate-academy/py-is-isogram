c7cf5417dacae48fb7352e3f57b298e72602922a
Oleksandr Pylypenko <sashapylypenko.pov@gmail.com>
2026-06-28 16:24:38 +0300

    fix: flake8 errors

--- app/test_main.py
+++ app/test_main.py
@@ -19,7 +19,7 @@ def test_is_isogram(word: str, expected_result: bool) -> None:
 
 def test_is_isogram_with_diff_types() -> None:
     with pytest.raises(AttributeError):
-        assert is_isogram(12) == False
+        assert is_isogram(12) is False
 
     with pytest.raises(AssertionError):
-        assert is_isogram(".,/!@#$%^&*(") == False
+        assert is_isogram(".,/!@#$%^&*(") is False
