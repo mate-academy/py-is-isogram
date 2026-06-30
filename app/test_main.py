31866a4b7d72d27bd626a8786816262c5fb25752
Dmytro <dmitryibezkrovnyi@gmail.com>
2026-06-30 22:48:13 +0200

    second day to add

--- app/test_main.py
+++ app/test_main.py
@@ -3,5 +3,10 @@ from app.main import is_isogram
 def test_should_return_true_if_str_empty():
     assert is_isogram("") == True
 
+
 def test_should_be_case_insensitive():
     assert is_isogram("aA") is False
+
+
+def test_should_return_false_when_letters_repeat():
+    assert is_isogram("look") is False
