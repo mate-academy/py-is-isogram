023fd5c5a6d35a80386fffef33a7e126897f1b56
Dmytro <dmitryibezkrovnyi@gmail.com>
2026-07-04 15:39:49 +0200

    fixed annotations

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,21 @@
 from app.main import is_isogram
 
 
-def test_should_return_true_if_str_empty():
+def test_should_return_true_if_str_empty() -> None:
     assert is_isogram("") is True
 
 
-def test_should_be_case_insensitive():
+def test_should_be_case_insensitive() -> None:
     assert is_isogram("Adam") is False
 
 
-def test_should_return_false_when_letters_repeat():
+def test_should_return_false_when_letters_repeat() -> None:
     assert is_isogram("look") is False
 
 
-def test_should_true_for_isogram():
+def test_should_true_for_isogram() -> None:
     assert is_isogram("playground") is True
 
 
-def test_should_return_non_consecutive_repeating_letters():
+def test_should_return_non_consecutive_repeating_letters() -> None:
     assert is_isogram("alphabet") is False
