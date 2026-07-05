4ffa65460cfd839b63f590d23bad7bd9f3340372
Arlecchin47 <zhurzhaartem0412@gmail.com>
2026-07-05 18:49:19 +0300

    solv

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
-from app.main import is_isogram
+from app import main
 
-# write your code here
+
+def test_empty_string() -> None:
+    assert main.is_isogram("") is True
+
+
+def test_isogram_true() -> None:
+    assert main.is_isogram("playgrounds") is True
+    assert main.is_isogram("subdermatoglyphic") is True
+
+
+def test_consecutive_letters_false() -> None:
+    assert main.is_isogram("look") is False
+    assert main.is_isogram("aa") is False
+
+
+def test_non_consecutive_letters_false() -> None:
+    assert main.is_isogram("aba") is False
+    assert main.is_isogram("alphabet") is False
+
+
+def test_case_insensitivity() -> None:
+    assert main.is_isogram("Adam") is False
+    assert main.is_isogram("Aa") is False
+    assert main.is_isogram("mMo") is False
