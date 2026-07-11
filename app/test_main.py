3bb87c88639ee558399b32907e13626da82c83ec
Anderson <anderson.medeiros3011@gmail.com>
2026-07-11 20:43:22 -0300

    Solução do Problema

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,34 @@
+from typing import Any
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word, expected",
+    [
+        "playgrounds", True,
+        "", True,
+        "", True,
+        "look", False,
+        "Adam", False,
+        "Alphabet", False,
+    ],
+)
+def test_is_isogram_valid_cases(word: str, expected: bool) -> None:
+    assert is_isogram(word) is expected
+
+
+@pytest.mark.parametrize(
+    "wrong_word",
+    [
+        123,
+        15.5,
+        None,
+        True,
+        False,
+        (["a", "b"]),
+    ],
+)
+def test_is_isogram_invalid_types(wrong_word: Any) -> None:
+    with pytest.raises(TypeError):
+        is_isogram(wrong_word)
