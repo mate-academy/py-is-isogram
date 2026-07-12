3696f6a349f40c885e83be28d2afe0e0e84a80ff
Myroslav <miroslacvbro47@gmail.com>
2026-07-12 16:14:19 +0200

    Fix test

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,21 @@
 from app.main import is_isogram
 
 
-def test_empty_string():
-    assert is_isogram('') is True
+def test_empty_string() -> None:
+    assert is_isogram("") is True
 
 
-def test_is_isogram():
-    assert is_isogram('playgrounds') is True
+def test_is_isogram() -> None:
+    assert is_isogram("playgrounds") is True
 
 
-def test_not_isogram():
-    assert is_isogram('look') is False
+def test_not_isogram() -> None:
+    assert is_isogram("look") is False
 
 
-def test_case_insensitive():
-    assert is_isogram('Adam') is False
+def test_case_insensitive() -> None:
+    assert is_isogram("Adam") is False
 
 
-def test_consecutive_letters_are_not_isogram():
-    assert is_isogram('aab') is False
+def test_consecutive_letters_are_not_isogram() -> None:
+    assert is_isogram("aab") is False
