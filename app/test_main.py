677b86bddcbd3149b4a4d21a2509936b8951376e
Illia Fedoruk <fedorukstar@gmail.com>
2026-07-14 18:33:16 +0300

    test: add tests for is_isogram and resolved flake8 issues

--- app/test_main.py
+++ app/test_main.py
@@ -1,14 +1,15 @@
 import pytest
 from app.main import is_isogram
 
+
 @pytest.mark.parametrize(
     "word, expected_result",
     [
-        ('playgrounds', True),
-        ('look', False),
-        ('Adam', False),
-        ('', True),
+        ("playgrounds", True),
+        ("look", False),
+        ("Adam", False),
+        ("", True),
     ]
 )
-def test_is_isogram(word: str, expected_result:bool) -> None:
+def test_is_isogram(word: str, expected_result: bool) -> None:
     assert is_isogram(word) == expected_result
