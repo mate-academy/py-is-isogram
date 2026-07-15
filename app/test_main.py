87f44ac1a22844bc069dfa2392034319d7759ca8
Volodymyr Mulyko <vovamulyka@gmail.com>
2026-07-15 17:23:25 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,58 @@
-from app.main import is_isogram
+from app import main
+from pytest import mark
 
-# write your code here
+
+@mark.parametrize(
+    "value, expected",
+    [
+        (" ", True),
+        ("", True),
+    ]
+)
+def test_is_isogram(value: str, expected: bool) -> None:
+    assert main.is_isogram(value) == expected
+
+
+@mark.parametrize(
+    "value, expected",
+    [
+        ("abc", True),
+        ("a", True),
+        ("playground", True),
+        ("isogram", True),
+    ]
+)
+def test_is_isogram_with_unique_characters(value: str, expected: bool) -> None:
+    assert main.is_isogram(value) == expected
+
+
+@mark.parametrize(
+    "value, expected",
+    [
+        ("hello", False),
+        ("look", False),
+        ("isogramm", False),
+        ("isograms", False),
+        ("  ", False),
+    ]
+)
+def test_is_isogram_with_repeated_characters(
+    value: str,
+    expected: bool
+) -> None:
+    assert main.is_isogram(value) == expected
+
+
+@mark.parametrize(
+    "value, expected",
+    [
+        ("ABC", True),
+        ("AA", False),
+        ("Aa", False),
+        ("aA", False),
+        ("AbC", True),
+        ("aBcD", True),
+    ]
+)
+def test_is_isogram_case_sensitivity(value: str, expected: bool) -> None:
+    assert main.is_isogram(value) == expected
