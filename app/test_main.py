d2c732e35348b65ad8aea1574e2d76ef8faac6aa
Alona Samotoi <meit109nerium@gmail.com>
2026-07-18 20:48:43 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,41 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsogram:
+    @pytest.mark.parametrize(
+        "word, bool_result",
+        [
+            pytest.param(
+                "playgrounds", True,
+                id="should check no repeating lower letters"
+            ),
+            pytest.param(
+                "PLAYGROUND", True,
+                id="should check no repeating upper letters"
+            ),
+            pytest.param(
+                "Playground", True,
+                id="should check no repeating lower and upper letters"
+            ),
+            pytest.param(
+                "look", False,
+                id="should check consecutive lower repeating letters"
+            ),
+            pytest.param(
+                "Adam", False,
+                id="should check non-consecutive upper repeating letters"
+            ),
+            pytest.param(
+                "", True,
+                id="should check empty string"
+            )
+        ]
+    )
+    def test_isogram(
+            self,
+            word: str,
+            bool_result: bool
+    ) -> None:
+        assert is_isogram(word) is bool_result
