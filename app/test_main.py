0e488f32aa790fe143498e127ed05878b7e04979
notwhitenet <genja1337@gmail.com>
2026-07-24 23:35:04 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize("word", [
+    "playgrounds",
+    "Dermatoglyphics",
+    "",
+])
+def test_is_isogram_true(word: str) -> None:
+    assert is_isogram(word) is True
+
+
+@pytest.mark.parametrize("word", [
+    "look",
+    "Adam",
+    "moOse",
+])
+def test_is_isogram_false(word: str) -> None:
+    assert is_isogram(word) is False
