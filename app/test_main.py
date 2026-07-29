414051d67cc561aadee8f84eda20526fe4c63e8f
Vasyl Perehinets <perehinets95@gmail.com>
2026-07-29 18:37:50 +0100

    fix

--- app/test_main.py
+++ app/test_main.py
@@ -8,6 +8,14 @@ from app.main import is_isogram
         ("look", False),
         ("Adam", False),
         ("playgrounds", True),
+        ("LinkinPark", False)
+    ],
+    ids=[
+        "empty string is_isogram",
+        "lower case is_isogram",
+        "upper case is_isogram",
+        "unique letters is_isogram",
+        "mixed case is_isogram"
     ]
 )
 def test_is_isogram(word: str, result: bool) -> None:
