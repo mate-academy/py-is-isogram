db8530a6a8fee3c55fe10647c659581cd46e4a15
Oleh Kuper <ok1804@ukr.net>
2026-08-04 14:10:19 +0300

    add solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,38 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsogramString:
+    @pytest.mark.parametrize(
+        "word,expected",
+        [
+            pytest.param(
+                "playgrounds",
+                True,
+                id="should return `True` for playgrounds",
+            ),
+            pytest.param(
+                "",
+                True,
+                id="should return `True` for empty string",
+            ),
+            pytest.param(
+                "Adam",
+                False,
+                id="`A` and `a` should be considered as the same words",
+            ),
+            pytest.param(
+                "abcdef",
+                True,
+                id="should return `True` for `abcdef`",
+            ),
+            pytest.param(
+                "hello",
+                False,
+                id="should return `False` for `hello`",
+            ),
+        ],
+    )
+    def test_is_isogram_string(self, word: str, expected: bool) -> None:
+        assert is_isogram(word) == expected
