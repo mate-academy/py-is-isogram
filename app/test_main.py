db69db8e157c4e626c1b5942e49a3dda0331535c
Nazar <qwertyasd0933@gmail.com>
2026-08-08 20:13:07 +0300

    fix flake 8 errors

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,7 @@
 import pytest
 from app.main import is_isogram
 
+
 @pytest.mark.parametrize(
     "input_string, expected_output",
     [
@@ -11,5 +12,5 @@ from app.main import is_isogram
         ("Adam", False)
     ]
 )
-def test_is_isogram(input_string, expected_output):
+def test_is_isogram(input_string: str, expected_output: bool) -> None:
     assert is_isogram(input_string) == expected_output
