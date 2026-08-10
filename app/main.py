3ddf37b9521a8d36ebb3f66a6a6fd5854233f0d1
yahadimon8-deep <yahadimon8@gmail.com>
2026-08-10 18:38:21 +0300

    solution: app/test_main.py and anjtacion in main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 def is_isogram(word: str) -> bool:
+    """Check if the given word is an isogram (no repeating letters)."""
     word_lower = word.lower()
     for letter in word_lower:
         if word_lower.count(letter) > 1:
