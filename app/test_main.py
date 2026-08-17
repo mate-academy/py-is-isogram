465be226de4291fcd1a7c44dbd9e864332d6a437
Tetiana Mohorian <tetiana.mohorian@student.tuke.sk>
2026-08-17 12:14:25 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,14 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram_with_isogram_word():
+    assert is_isogram('playgrounds') is True
+
+def test_is_isogram_with_repeat_word():
+    assert is_isogram('look') is False
+
+def test_is_isogram_with_insensitive():
+    assert is_isogram('Adam') is False
+
+def test_is_isogram_with_empty():
+    assert is_isogram('') is True
