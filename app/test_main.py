6ad54519d1139b49f089f0a5064df5078bb382e5
Maxe <mkasim.donchik@gmail.com>
2026-08-26 20:45:31 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,14 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram_should_return_true_if_count_equal_to_1() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_is_isogram_should_return_true_if_string_empty() -> None:
+    assert is_isogram("") is True
+
+
+def test_is_isogram_should_return_false_if_count_higher_than_1() -> None:
+    assert is_isogram("look") is False
+    assert is_isogram("Adam") is False
