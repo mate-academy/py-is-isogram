303057eaad325f8dacb10d1c1cc19befae39f2c1
Artem D <OkeNNV@gmail.com>
2026-08-27 18:27:24 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -25,5 +25,5 @@ from app.main import is_isogram
         "case_insensitive_consecutive_repeating",
     ],
 )
-def test_is_isogram(word, expected):
+def test_is_isogram(word: str, expected: bool) -> None:
     assert is_isogram(word) is expected
