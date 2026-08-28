0537de6092bdf0e392059bcf1e47804674dae3c1
mintrecc <maxdavydenkooo@gmail.com>
2026-08-29 00:11:48 +0300

    fixed assert problems

--- app/test_main.py
+++ app/test_main.py
@@ -3,13 +3,13 @@ from app.main import is_isogram
 
 class TestIsIsoGram:
     def test_string_is_empty(self) -> None:
-        assert is_isogram("") == True
+        assert is_isogram("")
 
     def test_string_letter_meet_once(self) -> None:
-        assert is_isogram("look") == False
+        assert not is_isogram("look")
 
     def test_string_letter_meet_twice(self) -> None:
-        assert is_isogram("playgrounds") == True
+        assert is_isogram("playgrounds")
 
     def test_string_letter_in_uppercase_meet_twice(self) -> None:
-        assert is_isogram("Adam") == False
+        assert not is_isogram("Adam")
