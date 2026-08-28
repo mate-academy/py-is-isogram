08773c63bdab1630854e17a26edb69e36cae4129
mintrecc <maxdavydenkooo@gmail.com>
2026-08-29 00:08:12 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,15 @@
 from app.main import is_isogram
 
-# write your code here
+
+class TestIsIsoGram:
+    def test_string_is_empty(self) -> None:
+        assert is_isogram("") == True
+
+    def test_string_letter_meet_once(self) -> None:
+        assert is_isogram("look") == False
+
+    def test_string_letter_meet_twice(self) -> None:
+        assert is_isogram("playgrounds") == True
+
+    def test_string_letter_in_uppercase_meet_twice(self) -> None:
+        assert is_isogram("Adam") == False
