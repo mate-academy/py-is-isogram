278bd34a552168948b08ecebd64f2b3dbb8e3021
milokc <alanmilok@gmail.com>
2026-08-31 13:17:23 +0300

    quotes replaced

--- app/test_main.py
+++ app/test_main.py
@@ -3,19 +3,19 @@ from app.main import is_isogram
 
 
 @pytest.mark.parametrize("word, expected", [
-    ('playgrounds', True),
-    ('look', False),
-    ('Adam', False),
-    ('Aa', False),
-    ('pp', False),
-    ('p', True),
-    ('', True)
-], ids=['has no repeating',
-        'has repeating',
-        'case insensitive',
-        'mixed case repeat',
-        'lowercase repeat',
-        'single letter',
-        'empty string'])
+    ("playgrounds", True),
+    ("look", False),
+    ("Adam", False),
+    ("Aa", False),
+    ("pp", False),
+    ("p", True),
+    ("", True)
+], ids=["has no repeating",
+        "has repeating",
+        "case insensitive",
+        "mixed case repeat",
+        "lowercase repeat",
+        "single letter",
+        "empty string"])
 def test_words(word: str, expected: bool) -> None:
     assert is_isogram(word) is expected
