101ad3342225155be1b88f0af53604361e7b303c
milokc <alanmilok@gmail.com>
2026-08-31 13:12:39 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
 def is_isogram(word: str) -> bool:
     word_lower = word.lower()
     for letter in word_lower:
+        print(word_lower.count(letter))
         if word_lower.count(letter) > 1:
             return False
     return True
+
+
+is_isogram('playgrounds') is True
+is_isogram('look') is False
+is_isogram('Adam') is False
+is_isogram('') is True
--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
+import pytest
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize("word, expected", [
+    ('playgrounds', True),
+    ('look', False),
+    ('Adam', False),
+    ('Aa', False),
+    ('pp', False),
+    ('p', True),
+    ('', True)
+], ids=['has no repeating',
+        'has repeating',
+        'case insensitive',
+        'mixed case repeat',
+        'lowercase repeat',
+        'single letter',
+        'empty string'])
+def test_words(word: str, expected: bool) -> None:
+    assert is_isogram(word) is expected
