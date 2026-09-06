14d280a280bbae4336f0adf918c31269c1ad4e11
Khrystyna <tkachristina079@gmail.com>
2026-09-06 17:01:53 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -8,8 +8,8 @@ from app.main import is_isogram
         ("playgrounds", True),
         ("look", False),
         ("Adam", False),
-        ('', True),
+        ("", True),
     ]
 )
-def test_is_isogram(word, expected):
+def test_is_isogram(word: str, expected: bool) -> None:
     assert is_isogram(word) == expected
