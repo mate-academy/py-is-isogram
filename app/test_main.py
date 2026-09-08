9f41edbedbb386a4ce81504abdca6ceb3aa67d00
Zastt <kolan6787@ukr.net>
2026-09-08 18:51:51 +0300

    test: implement unit tests for is_isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
+import pytest
+
 from app.main import is_isogram
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "word,expected",
+    [
+        ("", True),
+        ("a", True),
+        ("playgrounds", True),
+        ("subdermatoglyphic", True),
+        ("look", False),
+        ("Adam", False),
+        ("Aa", False),
+        ("alphabet", False),
+        ("isogram", True),
+    ],
+)
+def test_is_isogram(word: str, expected: bool) -> None:
+    assert is_isogram(word) is expected
