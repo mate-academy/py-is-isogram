19022e870dfc63ff3f17a5b2a529d406c1cd1215
tym <premo1313@gmail.com>
2026-09-08 14:33:35 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,29 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_common() -> None:
+    assert is_isogram("playgrounds") is True
+
+
+def test_double_letter() -> None:
+    assert is_isogram("look") is False
+
+
+def test_double_letter_different_case() -> None:
+    assert is_isogram("Adam") is False
+
+
+def test_empty_string() -> None:
+    assert is_isogram("") is True
+
+
+def test_single_letter() -> None:
+    assert is_isogram("a") is True
+
+
+def test_upper_case() -> None:
+    assert is_isogram("ABCFD") is True
+
+
+def test_only_single_letter() -> None:
+    assert is_isogram("aaaaa") is False
